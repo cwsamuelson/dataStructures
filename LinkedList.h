962bc47a0b7f188e5bcc9804ef11701f7e4a3a3f
Chris Samuelson <chris.sam55@gmail.com>
2013-03-26 12:44:21 -0400

    Most functionality included and working, generally fails on removes.

--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,8 @@
 #define LINKED_LIST_H
 
 #include<cstdlib>
+#include<iostream>
+using namespace std;
 
 template <class T>
 class LinkedList{
@@ -20,55 +22,58 @@ private:
 
 public:
     LinkedList(){
-        this.head = NULL;
-        this.tail = NULL;
-        this.size = 0;
+        this->head = NULL;
+        this->tail = NULL;
+        this->size = 0;
     }
     LinkedList(T *data){
         if(data){
-            this.head = new struct NODE<T>;
-            this.head.data = data;
-            this.tail = this.head;
-            this.size = 1;
+            this->head = new struct NODE<T>;
+            this->head->data = data;
+            this->tail = this->head;
+            this->size = 1;
         }
     }
     ~LinkedList(){
-        
+        while(this->head){
+            this->remove();
+        }
     }
     // Insert new element into list, defaults to the end.
     bool insert(T *data){
         // Critical failure, inconsistency detected.
-        if((this.head = NULL && this.tail) || (this.head && this.tail == NULL)){
+        if((this->head == NULL && this->tail != NULL) || (this->head != NULL && this->tail == NULL)){
+            cout << "failure" << endl;//debug
             return false;
         }
 
         // List is empty, create first node with new data.
-        if(this.head = NULL && this.tail = NULL){
+        if(this->head == NULL && this->tail == NULL){
             // Set up a new node and assign it to head and tail as the only existing node.
-            this.cur = new struct NODE<T>;
-            this.cur.data = data;
-            this.cur.prev = NULL;
-            this.cur.next = NULL;
+            this->cur = new struct NODE<T>;
+            this->cur->data = data;
+            this->cur->prev = NULL;
+            this->cur->next = NULL;
 
-            this.head = this.cur;
-            this.tail = this.cur;
-            this.cur = NULL;
+            this->head = this->cur;
+            this->tail = this->cur;
+            this->cur = NULL;
 
-            this.size = 1;
+            this->size = 1;
             // Success!
             return true;
         }
 
         // Normal list, insert at tail.
-        if(this.tail){
-            this.cur = new struct NODE<T>;
-            this.cur.data = data;
-            this.cur.prev = this.cur.tail;
-            this.tail.next = this.cur;
-            this.cur.next = NULL; 
-            this.cur = NULL;
+        if(this->tail){
+            this->cur = new struct NODE<T>;
+            this->cur->data = data;
+            this->cur->prev = this->tail;
+            this->tail->next = this->cur;
+            this->cur->next = NULL; 
+            this->cur = NULL;
 
-            this.size = 1;
+            this->size++;
             // Success!
             return true;
         }
@@ -87,13 +92,15 @@ public:
             return false;
         }
 
+        // Normal list.
         if(this.head){
             this.cur = this.head;
-            // Set cur determined by insertion point.
+            // Find insert point.
             for(int i = 0; i < index; i++){
                 if(this.cur.next)
                     this.cur = this.cur.next;
             }
+            // Insert new node.
             if(this.tail){
                 struct NODE<T> temp = new struct NODE<T>;
                 temp.data = data;
@@ -112,27 +119,90 @@ public:
 
     // Returns value of first element.
     T get(){
-        return *this.head.data;
+        if(this->head != NULL){
+            return *this->head->data;
+        }
+        else
+            return 0;//replace with error testing
     }
 
     // Returns value of element at <index>.
     T get(int index){
+        this->cur = this->head;
         for(int i = 0; i < index; i++){
-            if(this.cur.next)
-                this.cur = this.cur.next;
+            if(this->cur->next){
+                this->cur = this->cur->next;
+            }
         }
-        return *this.cur.data;
+        this->cur = NULL;
+        return *this->cur->data;
     }
 
     // Deletes and returns the value of head.
     T remove(){
-        
+        // If size == 1.
+        if((this->head != this->tail)&&(this->size==1)){
+            cout << "critical failure" << endl;
+            return 0;
+        }
+        if((this->head == this->tail)&&(this->size == 1)){
+            cout << "\tremove size == 1" << endl;//debug
+            this->cur = this->head;
+            T ret = *this->cur->data;
+            this->head = NULL;
+            this->tail = NULL;
+            delete this->cur;
+            this->cur = NULL;
+            this->size--;
+            return ret;
+        }
+        if(this->size > 0){
+            cout<<"test"<<endl;//debug
+            this->cur = this->head;
+            T ret = *this->cur->data;
+            this->cur->next->prev = NULL;//fails here, need to put this code in an if statement.
+            this->head = this->cur->next;
+            delete this->cur;
+            this->size--;
+            return ret;
+        }
+        //if(){
+            
+        //}
+    // Catch error.
+        return 0;//replace with error checking
     }
 
     // Deletes and returns the value of <index>.
     T remove(int index){
+        // Index is head.
         if(index == 0){
-            return this.remove();
+            cout << "check 0" << endl;//debug
+            return this->remove();
+        }else
+        // Index is tail.
+        if(index == this->size - 1){
+            cout << "tail" << endl;//debug
+            this->cur = this->tail;
+            T ret = *this->cur->data;
+            this->cur->prev->next = NULL;
+            this->tail = this->cur->prev;
+            delete this->cur;
+            this->size--;
+            return ret;
+        }else{// Index is normal.
+            cout << "normal" << endl;//debug
+            for(int i = 0; i < index; i++){
+                if(this->cur->next){
+                    this->cur = this->cur->next;
+                }
+            }
+            T ret = *this->cur->data;
+            this->cur->next->prev = this->cur->prev;
+            this->cur->prev->next = this->cur->next;
+            delete this->cur;
+            this->size--;
+            return ret;
         }
     }
 
--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include<iostream>
 
 using namespace std;
+
 int main(int argc, char **argv){
-    cout << "test\n";
+    cout << "start" << endl;
+    int foo = 1;
+    int bar = 2;
+    int fu = 3;
+    LinkedList<int> test; //= new LinkedList<int>;
+    cout << "\ttest.insert(&foo):\t"    << test.insert(&foo) << endl;
+    cout << "\ttest.get():\t\t"         << test.get()        << endl;
+    cout << "\ttest.getSize():\t\t"     << test.getSize()    << endl;
+    cout << "\ttest.insert(&bar):\t"    << test.insert(&bar) << endl;
+    cout << "\ttest.get():\t\t"         << test.get()        << endl;
+    cout << "\ttest.getSize():\t\t"     << test.getSize()    << endl;
+    cout << "\ttest.insert(&fu):\t"     << test.insert(&fu)  << endl;
+    cout << "\ttest.getSize():\t\t"     << test.getSize()    << endl;
+    cout << "\ttest.get():\t\t"         << test.get()        << endl;
+    cout << "\ttest.remove():\t\t"      << test.remove()     << endl;
+    cout << "\ttest.getSize():\t\t"     << test.getSize()    << endl;
+    cout << "\ttest.remove():\t\t"      << test.remove()     << endl;
+    cout << "end" << endl;
+    return 0;
 }
