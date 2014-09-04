43018f144924a01a0d02b7ce975e20dd6758d8d5
Chris Samuelson <chris.sam55@gmail.com>
2014-09-03 23:49:14 -0400

    Emplacing character class as part of library.

---
+++ character/src/character.hh
@@ -0,0 +1,29 @@
+#ifndef __CHARACTER_H__
+#define __CHARACTER_H__
+
+#include<Jing/Object.hh>
+#include<ostream>
+
+namespace Jing{
+
+class character:public Object{
+protected:
+  char letter;
+public:
+  character(char c);
+  virtual ~character();
+
+  bool equals(Object& obj) const;
+  bool equals(character& ch) const;
+  bool equals(char ch) const;
+  unsigned long long hash() const;
+  character* clone() const;
+
+  friend std::ostream& operator<<(std::ostream& os, const character& let);
+};
+std::ostream& operator<<(std::ostream& os, const Jing::character& let);
+
+}
+
+#endif
+
