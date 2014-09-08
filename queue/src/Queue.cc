6c49899eac88a0135f19d412f95f43856be87a00
Chris Samuelson <chris.sam55@gmail.com>
2014-09-07 21:34:15 -0400

    Added templates to queue implementation

--- queue/src/Queue.cc
+++ queue/src/Queue.cc
@@ -1,63 +1,77 @@
 //#include<Jing/list>
 
 //Constructors
+template<class T>
 Jing::Queue<T>::Queue():que(){
 }
 
+template<class T>
 Jing::Queue<T>::Queue(Collection<T>& c):que(c){
 }
 
 //Inserts
+template<class T>
 void Jing::Queue<T>::enqueue(T& obj){
   this->que.insert(obj, this->que.size());
 }
 
 //  [inherited from collection]
+template<class T>
 void Jing::Queue<T>::insert(T& obj){
   this->enqueue(obj);
 }
 
+template<class T>
 void Jing::Queue<T>::insertAll(Collection<T>& c){
 }
 
 //Removes
+template<class T>
 T& Jing::Queue<T>::dequeue(){
   return this->que.remove(0);
 }
 
+template<class T>
 T& Jing::Queue<T>::remove(){
   return this->dequeue();
 }
 
 //  [inherited from collection]
+template<class T>
 void Jing::Queue<T>::remove(T& obj){
 }
 
+template<class T>
 void Jing::Queue<T>::removeAll(Collection<T>& c){
 }
 
 //Contains
 //  [inherited from collection]
+template<class T>
 bool Jing::Queue<T>::contains(T& obj) const{
 }
 
+template<class T>
 bool Jing::Queue<T>::containsAll(Collection<T>& c) const{
 }
 
 //Reverse gets
 //Equality
 //  [inherited from Object]
+template<class T>
 bool Jing::Queue<T>::equals(Object& obj) const{
 }
 
 //Iterator
 //  [inherited from Iterable]
+template<class T>
 Iterator<T>& Jing::Queue<T>::iterator() const{
 }
 
 //States
 //  hash
 //  [inherited from Object]
+template<class T>
 unsigned long long Jing::Queue<T>::hash() const{
   return 0;
 }
@@ -66,24 +80,28 @@ unsigned long long Jing::Queue<T>::hash() const{
 //  [inherited from Object]
 //memory management may be up to the user.
 //clean up with delete
+template<class T>
 Object* Jing::Queue<T>::clone() const{
   return 0;
 }
 
 //  isempty
 //  [inherited from collection]
+template<class T>
 bool Jing::Queue<T>::isEmpty() const{
   return this->que.isEmpty();
 }
 
 //  size
 //  [inherited from collection]
+template<class T>
 size_t Jing::Queue<T>::size() const{
   return this->que.size();
 }
 
 //  clear
 //  [inherited from collection]
+template<class T>
 void Jing::Queue<T>::clear(){
   this->que.clear();
 }
