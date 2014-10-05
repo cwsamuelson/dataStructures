8023b712a51215923e9960d4226bbf1081131715
Chris Samuelson <chris.sam55@gmail.com>
2014-10-05 14:43:12 -0400

    Established use of operators for comparison in comparable

--- abstracts/Comparable.hh
+++ abstracts/Comparable.hh
@@ -11,9 +11,55 @@ class Comparable{
 //A subclass of Comparable is only comparable to itself, and between subclasses.
 //Returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
 //Specifically Compares this object to another.
-  virtual int compareTo(T& obj) const{
+public:
+  virtual int compareTo(const T& obj) const{
     can_copy<T, Jing::Object&>();
   }
+
+  bool operator==(const T& rhs) const{
+    if(this->compareTo(rhs) == 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator!=(const T& rhs) const{
+    return !(*this == rhs);
+  }
+
+  bool operator<(const T& rhs) const{
+    if(this->compareTo(rhs) < 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator>(const T& rhs) const{
+    if(this->compareTo(rhs) > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator<=(const T& rhs) const{
+    if(*this < rhs || *this == rhs){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool operator>=(const T& rhs) const{
+    if(*this > rhs || *this == rhs){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 };
 
 }
