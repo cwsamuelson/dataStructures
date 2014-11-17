a63e7d3da3fcd4e9886ba3fe9ec513afc4bade39
Chris Samuelson <chris.sam55@gmail.com>
2014-11-17 17:44:25 -0500

    Added test for split

--- string/src/test.cc
+++ string/src/test.cc
@@ -6,9 +6,14 @@ using std::endl;
 using namespace Jing;
 
 int main(int argc, char** argv){
+  string splitter("one two three");
   string foo("test");
   string bar(foo);
 
+  cout << "Split string" << endl;
+  cout << splitter << endl;
+  smart_ptr<List<string>> tmp = splitter.split(' ');
+  cout << "list size " << tmp->size() << endl;
   cout << "charat1" << endl;
   cout << foo.charAt(1) << endl;
   cout << "<<foo<<" << endl;
