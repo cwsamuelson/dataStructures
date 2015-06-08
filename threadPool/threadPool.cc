b1d819155a18d530ff6b1800bae8d0fb95f236b9
Chris <chris.sam55@gmail.com>
2015-06-08 14:42:21 -0400

    Implemented concept of pool currently working.

--- threadPool/threadPool.cc
+++ threadPool/threadPool.cc
@@ -1,6 +1,6 @@
 
 void threadPool::threadFn(){
-  while(true){
+  while(isWorking){
     if(size() > 0){
       mtxSelf.lock();
       auto it = front();
@@ -12,12 +12,14 @@ void threadPool::threadFn(){
 }
 
 threadPool::threadPool(unsigned int nThreads){
+  isWorking = true;
   for(unsigned int i = 0; i < nThreads; ++i){
     pool.push_back(std::thread(&threadPool::threadFn, this));
   }
 }
 
 threadPool::~threadPool(){
+  isWorking = false;
   for(auto it = pool.begin(); it != pool.end(); ++it){
     (*it).join();
   }
