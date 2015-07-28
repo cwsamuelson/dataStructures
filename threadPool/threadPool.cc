cb7d58318edec15d8395bb35479a212f7fc00c46
Chris <chris.sam55@gmail.com>
2015-07-28 12:13:04 -0400

    Fixed destructor to allow threads to end safely.

--- threadPool/threadPool.cc
+++ threadPool/threadPool.cc
@@ -6,13 +6,17 @@ void threadPool::threadFn(){
       auto it = front();
       pop();
       mtxSelf.unlock();
-      it();
+      //if it's a valid function, run it
+      //  this check prevents running invalid functions when mutex was unlucked during shutdown
+      if(it.target<void()>() != 0){
+        it();
+      }
     }
   }
 }
 
-threadPool::threadPool(unsigned int nThreads){
-  isWorking = true;
+threadPool::threadPool(unsigned int nThreads):
+  isWorking(true){
   for(unsigned int i = 0; i < nThreads; ++i){
     pool.push_back(std::thread(&threadPool::threadFn, this));
   }
@@ -20,6 +24,13 @@ threadPool::threadPool(unsigned int nThreads){
 
 threadPool::~threadPool(){
   isWorking = false;
+  //While already locked(try_lock failed), unlock
+  //  This frees the running threads, allowing them to exit
+  while(!mtxSelf.try_lock()){
+    mtxSelf.unlock();
+  }
+  //unlock from incidental locking
+  mtxSelf.unlock();
   for(auto it = pool.begin(); it != pool.end(); ++it){
     (*it).join();
   }
