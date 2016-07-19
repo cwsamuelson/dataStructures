bfb05ff6e1f5407f0346cb10b6527e7ee0199706
Chris Samuelson <chris.sam55@gmail.com>
2016-07-19 12:48:37 -0400

    Added some string tests

--- source/main.cc
+++ source/main.cc
@@ -185,8 +185,47 @@ bool testStruct(){
   }
 }
 
-string testString(){
-  return "test";
+TEST_CASE("Strings can be accessed like an array", "[string]"){
+  const char* cstr = "length 8";
+  string str(cstr);
+
+  REQUIRE(str.size() == 8);
+
+  for(unsigned int i = 0; i < str.size(); ++i){
+    REQUIRE(cstr[i] == str[i]);
+  }
+
+  int i = 0;
+  for(auto it:str){
+    REQUIRE(it == cstr[i++]);
+  }
+}
+
+TEST_CASE("Strings can construct and assign with strings and string literals", "[string]"){
+  const char* cstr = "cstring";
+  string str("test string");
+
+  SECTION("Strings can be compared with c-strings"){
+    REQUIRE(str == "test string");
+  }
+
+  SECTION("Strings can be assigned c-strings"){
+    str = cstr;
+
+    REQUIRE(str == cstr);
+  }
+
+  SECTION("Strings can be constructed with c-strings"){
+    string str2(cstr);
+
+    REQUIRE(str2 == cstr);
+  }
+
+  SECTION("Strings can be constructed with other strings"){
+    string str2(str);
+
+    REQUIRE(str == str2);
+  }
 }
 
 class wrapper{
@@ -219,14 +258,14 @@ TEST_CASE("Vectors will run constructors/destructors when appropriate", "[vector
   SECTION("Constructor is run on emplace_back call"){
     v.emplace_back(&test);
 
-    REQUIRE(test == false);
+    REQUIRE(!test);
   }
 
   SECTION("Destructor is run on pop_back"){
     v.emplace_back(&test);
     v.pop_back();
 
-    REQUIRE(test == true);
+    REQUIRE(test);
   }
 }
 
