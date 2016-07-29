94f1942f7fee3e0ae4be346d29146d612fe3cf9c
Chris Samuelson <chris.sam55@gmail.com>
2016-07-29 12:19:04 -0400

    Changed sort testing values to be random

--- source/main.cc
+++ source/main.cc
@@ -2,6 +2,7 @@
 #include<functional>
 #include<sstream>
 #include<vector>
+#include<random>
 
 #define CATCH_CONFIG_MAIN
 #include<catch/catch.hpp>
@@ -481,19 +482,22 @@ TEST_CASE("Maps are associative containers", "[map]"){
 }
 
 TEST_CASE("Sort algorithm", "[algorithm]"){
+  int count = 1000;
   vector<int> vec;
+  std::random_device rd;
+  std::mt19937 rate(rd());
+  std::normal_distribution<> gene(0, 10000000);
 
-  vec.push_back(3);
-  vec.push_back(2);
-  vec.push_back(4);
-  vec.push_back(1);
-  vec.push_back(6);
-  vec.push_back(5);
+  for(int i = 0; i < count; ++i){
+    vec.push_back(gene(rate));
+  }
 
   sort(vec.begin(), vec.end());
 
+  REQUIRE(vec.size() == count);
+
   for(unsigned int i = 1; i < vec.size(); ++i){
-    REQUIRE(vec[i] > vec[i - 1]);
+    REQUIRE(vec[i] >= vec[i - 1]);
   }
 }
 
