17f3d2e85c7570def59bcce3cb0f4727694c1de6
Chris Samuelson <chris.sam55@gmail.com>
2016-12-29 12:24:49 -0500

    Overhauled and simplified memory pool system, and tests
    Memory pool was using value_type array for storage.  Switched to use
    char array, and simplified storage tracking.  new Storage tracking
    uses an array of bools for each slot, and an array of sizes to track how
    many stored at each.

--- header/memory_pool.hh
+++ header/memory_pool.hh
@@ -1,9 +1,9 @@
 #ifndef __MEMORY_POOL_HH__
 #define __MEMORY_POOL_HH__
 
-#include<list>
+#include<vector>
 
-//TODO: switch from value_type to char
+//TODO: add placement new/delete
 template<class T>
 class memoryPool{
 public:
@@ -14,27 +14,13 @@ public:
   static const size_type ptrdiff = sizeof( value_type );
 
 private:
-  class slot{
-  public:
-    pointer start;
-    pointer end;
-
-    slot( pointer first, pointer second ):
-      start( first ),
-      end( second ){
-    }
-
-    bool operator<( const slot& rhs ){
-      return start < rhs.start;
-    }
-  };
- 
   size_type mSize;
   size_type mInUse;
   size_type mTotalBytes;
   char* mStorageStart;
   char* mStorageEnd;
-  std::list<slot> mSlots;
+  std::vector<bool> mSlots;
+  std::vector<size_type> mSizes;
  
 public:
   memoryPool( size_type size ):
@@ -42,62 +28,52 @@ public:
     mInUse( 0 ),
     mTotalBytes( mSize * ptrdiff ),
     mStorageStart( new char[mTotalBytes] ),
-    mStorageEnd( mStorageStart + mTotalBytes - ptrdiff ){
+    mStorageEnd( mStorageStart + mTotalBytes - ptrdiff ),
+    mSlots( mSize, false ),
+    mSizes( mSize, 0 ){
   }
   virtual ~memoryPool(){ delete[] mStorageStart; }
 
   pointer allocate( size_type amt ){
-    pointer ret = nullptr;
-    size_type allocSize = amt * ptrdiff;
-    //If the request is larger than that available, return null;
-    if( amt == 0 || ( amt > ( mSize - mInUse ) )  ){
-      ret = nullptr;
-    } else {
-      //Determine where free memory exists to return;
-      //If there's no memory in use, return beginning;
-      if( mSlots.size() == 0 ){
-        mSlots.emplace_back( ( pointer )mStorageStart, ( pointer )( mStorageStart + allocSize ) );
-        ret = ( value_type* )mStorageStart;
-      } else if( mSlots.begin()->start != ( pointer )mStorageStart ){
-        if( ( size_type )( mSlots.begin()->start - ( pointer )mStorageStart ) >= allocSize ){
-          mSlots.emplace_back( ( pointer )mStorageStart, ( pointer )( mStorageStart + allocSize ) );
-          ret = ( value_type* )mStorageStart;
+    size_type caveSize = 0;
+    bool caveFound = false;
+    size_type caveOpening = 0;
+
+    for( unsigned int i = 0; i < mSize; ++i ){
+      if( !mSlots[i] ){
+        if( ++caveSize == amt ){
+          caveFound = true;
+          break;
         }
       } else {
-        auto first = mSlots.begin();
-        for( auto second = ++mSlots.begin(); second != mSlots.end(); ++second, ++first ){
-          if( ( size_type )( second->start - first->end ) >= allocSize ){
-            ret = ( value_type* )first->end;
-            break;
-          }
-        }
-        //If we get to the end of the list and there's no free space found,
-        //  check the tail end;
-        if( ( size_type )( ( pointer )mStorageEnd - first->end ) >= amt ){
-          mSlots.emplace_back( first->end, first->end + allocSize );
-          ret = ( value_type* )first->end;
-        }
-      }
-      //Only re-sort when new things have been added;
-      if( ret ){
-        mSlots.sort();
+        caveOpening = i + 1;
       }
     }
 
-    if( ret ){
+    if( caveFound ){
+      for( size_type i = caveOpening; i < caveOpening + amt; ++i ){
+        mSlots[i] = true;
+      }
+      mSizes[caveOpening] = amt;
+
       mInUse += amt;
+      return ( pointer )&mStorageStart[caveOpening * ptrdiff];
+    } else {
+      return nullptr;
     }
-    return ret;
   }
 
   void deallocate( pointer ptr ){
-    for( auto it = mSlots.begin(); it != mSlots.end(); ++it ){
-      if( it->start == ptr ){
-        mInUse -= ( it->end - it->start ) / ptrdiff;
-        mSlots.erase( it );
-        break;
-      }
+    if( ptr < ( pointer )mStorageStart || ptr > ( pointer )mStorageEnd ){
+      return;
     }
+
+    size_type idx = ( ptr - ( pointer )mStorageStart );
+    for( size_type i = idx; i < idx + mSizes[idx]; ++i ){
+      mSlots[i] = false;
+    }
+    mInUse -= mSizes[idx];
+    mSizes[idx] = 0;
   }
 
   size_type in_use_count(){
--- source/main.cc
+++ source/main.cc
@@ -475,6 +475,16 @@ TEST_CASE( "Memory pool will allocate resources from its collection.", "[mempool
 
   REQUIRE( ( baz + 2 ) == bar );
   REQUIRE( ( foo + 2 ) == quux );
+
+  //test invalid allocation
+  REQUIRE( test.allocate( 20 ) == nullptr );
+  REQUIRE( test.in_use_count() == 7 );
+  REQUIRE( test.available_space() == 3 );
+
+  //test invalid deallocation
+  test.deallocate( baz + 1 );
+  REQUIRE( test.in_use_count() == 7 );
+  REQUIRE( test.available_space() == 3 );
 }
 
 TEST_CASE( "Maps are associative containers", "[map]" ){
