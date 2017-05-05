296e6a3d5bf0317e83235a7c399727e3a0a605f1
Chris Samuelson <chris.sam55@gmail.com>
2017-05-05 16:07:00 -0400

    Updated testing regimen
    Added a couple important tests to unit, and changed some to work with
    updates to unit

--- source/main.cc
+++ source/main.cc
@@ -299,56 +299,64 @@ TEST_CASE( "Units participate in arithmetic", "[unit]" ){
   typedef unit<1, 0, 0, 0, 0, 0> test_t;
 
   SECTION( "Units of same type can be added and subtracted" ){
-    int valX = 1;
-    int valY = 2;
+    double valX = 1;
+    double valY = 2;
 
     test_t x = valX;
     test_t y = valY;
 
     test_t z = x + y;
+    test_t w = y - x;
 
     REQUIRE( x == valX );
     REQUIRE( y == valY );
     REQUIRE( z == valX + valY );
+    REQUIRE( w == valY - valX );
   }
 
   SECTION( "Units multiplication/division result in new unit types" ){
-    voltage V = 3;
-    current I = 5;
-    resistance R = 4;
+    voltage<> V = 3.0;
+    current<> I = 5.0;
+    resistance<> R = 4.0;
 
     V = I * R;
 
     REQUIRE( V.getValue() == I.getValue() * R.getValue() );
+    I = 3.0;
 
-    test_t X = 2;
-    test_t Y = 3;
+    I = V / R;
+
+    REQUIRE( I == 5.0 );
+    REQUIRE( I.getValue() == V.getValue() / R.getValue() );
+
+    test_t X = 2.0;
+    test_t Y = 3.0;
     unit<2, 0, 0, 0, 0, 0> Z;
 
     Z = X * Y;
 
-    REQUIRE( Z.getValue() == 6 );
+    REQUIRE( Z.getValue() == 6.0 );
   }
 
   SECTION( "Units of same type can be value compared" ){
-    test_t X = 1;
-    test_t Y = 1;
-    test_t Z = 3;
+    test_t X = 1.0;
+    test_t Y = 1.0;
+    test_t Z = 3.0;
 
     REQUIRE( X == Y );
     REQUIRE( X < Z );
     REQUIRE( Z > Y );
-    REQUIRE( X < 3 );
+    REQUIRE( X < 3.0 );
   }
 
-  SECTION( "Units can use compount operators" ){
-    test_t X = 0;
-    X += 3;
+  SECTION( "Units can use compound operators" ){
+    test_t X = 0.0;
 
-    REQUIRE( X == 3 );
+    X += 3.0;
+    REQUIRE( X == 3.0 );
 
-    X -= 1;
-    REQUIRE( X == 2 );
+    X -= 1.0;
+    REQUIRE( X == 2.0 );
   }
 }
 
