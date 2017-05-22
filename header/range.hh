34dbc09d0dfec67e0c7af55bf00f6f39bff75263
Chris Samuelson <chris.sam55@gmail.com>
2017-05-22 11:56:02 -0400

    Added doxygen style doc blocks

--- header/range.hh
+++ header/range.hh
@@ -6,6 +6,16 @@
 template<class>
 class range;
 
+/*! Range filtering iterator
+ *
+ * @tparam CONTAINER  Type of container being iterated
+ *
+ * Provides a normal iterator interface over a container, however the data
+ * retrieved from said container is not the raw data stored; instead the data
+ * returned by this iterator is filtered using a provided filter callback,
+ * as well as modified by a provided modification callback.  This allows for a
+ * customized view into a storage container.
+ */
 template<class CONTAINER>
 class range_iterator{
 public:
@@ -21,60 +31,68 @@ private:
   iterator mIterator;
 
 public:
-  range_iterator(const range<container>* rang, iterator iter):
-    mRange(rang),
-    mIterator(iter){
+  range_iterator( const range<container>* rang, iterator iter ):
+    mRange( rang ),
+    mIterator( iter ){
   }
-  range_iterator(const range_iterator& iter):
-    mRange(iter.mRange),
-    mIterator(iter.mIterator){
+  range_iterator( const range_iterator& iter ):
+    mRange( iter.mRange ),
+    mIterator( iter.mIterator ){
   }
 
-  bool operator==(const range_iterator& other) const{
+  bool operator==( const range_iterator& other ) const{
     return mIterator == other.mIterator;
   }
-  bool operator==(const iterator& other) const{
+  bool operator==( const iterator& other ) const{
     return mIterator == other;
   }
-  bool operator!=(const range_iterator& other) const{
-    return !((*this) == other);
+  bool operator!=( const range_iterator& other ) const{
+    return !( ( *this ) == other );
   }
-  bool operator!=(const iterator& other) const{
-    return !(mIterator == other);
+  bool operator!=( const iterator& other ) const{
+    return !( mIterator == other );
   }
 
   value_type operator*() const{
-    return mRange->mModifier(*mIterator);
+    return mRange->mModifier( *mIterator );
   }
   pointer operator->() const{
     return mIterator.operator->();
   }
   
   range_iterator& operator++(){
-    while((!mRange->mFilter(*++mIterator)) && (mRange->end() != mIterator)){
+    while( ( !mRange->mFilter( *++mIterator ) ) && ( mRange->end() != mIterator ) ){
       ;
     }
     return *this;
   }
-  range_iterator operator++(int){
-    range_iterator ret(*this);
-    ++(*this);
+  range_iterator operator++( int ){
+    range_iterator ret( *this );
+    ++( *this );
     return ret;
   }
   
   range_iterator& operator--(){
-    while(!mRange->mFilter(*--mIterator) && (mRange->begin() != mIterator)){
+    while( !mRange->mFilter( *--mIterator ) && ( mRange->begin() != mIterator ) ){
       ;
     }
     return *this;
   }
-  range_iterator operator--(int){
-    range_iterator ret(*this);
-    --(*this);
+  range_iterator operator--( int ){
+    range_iterator ret( *this );
+    --( *this );
     return ret;
   }
 };
 
+/*! Range filter class
+ *
+ * @tparam CONTAINER  Type of container being filtered
+ *
+ * Range class takes a container, or iterators to its beginning and ending
+ * and will provide custom iterators to said container.  These custom iterators
+ * allow custom data filtering and data modification based on provided callbacks.
+ */
 template<class CONTAINER>
 class range{
 public:
@@ -83,8 +101,8 @@ public:
   typedef typename container::pointer pointer;
   typedef typename container::reference reference;
   typedef typename container::iterator contIter;
-  typedef std::function<bool(const reference)> filter;
-  typedef std::function<value_type(const reference)> modifier;
+  typedef std::function<bool( const reference )> filter;
+  typedef std::function<value_type( const reference )> modifier;
   typedef range_iterator<container> iterator;
 
 private:
@@ -96,29 +114,63 @@ private:
   friend iterator;
 
 public:
-  range(contIter begin, contIter end,
-        filter filFN = [](const reference){ return true; },
-        modifier modFN = [](const reference t){ return t; }):
-    mBegin(begin),
-    mEnd(end),
-    mFilter(filFN),
-    mModifier(modFN){
+  /*! Stores beginning and ending iterators, filtering callback, and modification callback
+   *
+   * @param begin  Iterator to beginning iterator of target container
+   *
+   * @param end  Iterator to end iterator of target container
+   *
+   * @param filFN  Filter callback
+   *
+   * @param modFN  Modification callback
+   *
+   * Takes iterator, filtering, and modification information that will be
+   * needed when continuing to use range.
+   */
+  range( contIter begin, contIter end,
+         filter filFN = []( const reference ){ return true; },
+         modifier modFN = []( const reference t ){ return t; } ):
+    mBegin( begin ),
+    mEnd( end ),
+    mFilter( filFN ),
+    mModifier( modFN ){
   }
-  range(container& cont, 
-        filter filFN = [](const reference){ return true; },
-        modifier modFN = [](const reference t){ return t; }):
-    range(cont.begin(), cont.end(), filFN, modFN){
+
+  /*! Takes a container, and stores its beginning and ending iterators, and filtering callback, and modification callback
+   *
+   * @param cont  Reference to container that begin and end iterators will pull from
+   *
+   * @param filFN  Filter callback
+   *
+   * @param modFN  Modification callback
+   *
+   * Takes container, filtering, and modification information that will be
+   * needed when continuing to use range.
+   */
+  range( container& cont, 
+         filter filFN = []( const reference ){ return true; },
+         modifier modFN = []( const reference t ){ return t; } ):
+    range( cont.begin(), cont.end(), filFN, modFN ){
   }
-  
+
+  /*! Get beginning iterator
+   *
+   * @return Iterator pointing to beginning of container
+   */
   iterator begin() const{
-    contIter iter(mBegin);
-    while(!mFilter(*iter)){
+    contIter iter( mBegin );
+    while( !mFilter( *iter ) ){
       ++iter;
     }
-    return iterator(this, iter);
+    return iterator( this, iter );
   }
+
+  /*! Get ending iterator
+   *
+   * @return Iterator pointing to past end of container
+   */
   iterator end() const{
-    return iterator(this, mEnd);
+    return iterator( this, mEnd );
   }
 };
 
--- header/ratio.hh
+++ header/ratio.hh
@@ -3,6 +3,12 @@
 
 #include<numeric>
 
+/*! Get the sign of a given number
+ *
+ * @tparam T  Type of provided argument
+ *
+ * @param t  Integral argument
+ */
 template<typename T>
 constexpr signed int sign( T t ){
   if( std::signbit( t ) ){
@@ -12,6 +18,12 @@ constexpr signed int sign( T t ){
   }
 }
 
+/*! Get the absolute value of a given number
+ *
+ * @tparam T  Type of provided argument
+ *
+ * @param t  Numeric argument
+ */
 template<typename T>
 constexpr T myAbs( T t ){
   if( t < 0 ){
@@ -21,6 +33,12 @@ constexpr T myAbs( T t ){
   }
 }
 
+/*! Get the greatest common denominator of 2 values
+ *
+ * @tparam T  Type of 'lhs'
+ *
+ * @tparam U  Type of 'rhs'
+ */
 template<typename T, typename U>
 constexpr std::common_type_t<T, U> gcd( T t, U u ){
   //return std::gcd( t, u );
@@ -34,6 +52,14 @@ constexpr std::common_type_t<T, U> gcd( T t, U u ){
   return t;
 }
 
+/*! Compile time integer ratio class
+ *
+ * @tparam N  Numerator value
+ *
+ * @tparam D  Denominator value
+ *
+ * Provides compile time ratios, including reduced fractions
+ */
 template<size_t N, size_t D>
 class ratio{
 public:
@@ -43,16 +69,46 @@ public:
   static constexpr double value = double( double( numerator ) / double( denominator ) );
   typedef ratio<numerator, denominator> type;
 
+  /*! Value of ( numerator / denominator ) or of template parameters ( N / D )
+   *
+   * This allows the number to be easily used at runtime, converting the ratio
+   * to a double value
+   */
   constexpr operator double() const{
     return value;
   }
 
+  /*! Value of 2 ratios multiplied together
+   *
+   * @tparam NUM  Numerator of parameter
+   *
+   * @tparam DEN  Denominator of parameter
+   *
+   * @param other  Ratio to multiply with
+   *
+   * @return Ratio of the result of ratio multiplication  
+   *
+   * Returns object of new ratio type, dependent on this object, and parameter
+   */
   template<size_t NUM, size_t DEN>
   constexpr auto operator*( ratio<NUM, DEN> other ){
     typedef decltype( other ) OTHER;
 
     return ratio<numerator * OTHER::numerator, denominator * OTHER::denominator>();
   }
+
+  /*! Value of 1 ratio divided by another
+   *
+   * @tparam NUM  Numerator of parameter
+   *
+   * @tparam DEN  Denominator of parameter
+   *
+   * @param other  Ratio to divide by
+   *
+   * @return Ratio of the result of ratio division
+   *
+   * Returns object of new ratio type, dependent on this object, and parameter
+   */
   template<size_t NUM, size_t DEN>
   constexpr auto operator/( ratio<NUM, DEN> other ){
     typedef decltype( other ) OTHER;
@@ -61,24 +117,94 @@ public:
   }
 };
 
+/*! Value of ratio multiplied with a number
+ *
+ * @tparam T  Type of number being multiplied with ratio
+ *
+ * @tparam N  Numerator of ratio parameter
+ *
+ * @tparam D  Denominator of ratio parameter
+ *
+ * @param t  Number to multiply with
+ *
+ * @param r  Ratio to multiply with
+ *
+ * @return Number of type T, containing multiplication result
+ */
 template<typename T, size_t N, size_t D>
-T operator*( T t, ratio<N, D> r ){
-  return t * double( r );
-}
-template<typename T, size_t N, size_t D>
-T operator*( ratio<N, D> r, T t ){
+constexpr T operator*( T t, ratio<N, D> r ){
   return t * double( r );
 }
 
+/*! Value of ratio multiplied with a number
+ *
+ * @tparam T  Type of number being multiplied with ratio
+ *
+ * @tparam N  Numerator of ratio parameter
+ *
+ * @tparam D  Denominator of ratio parameter
+ *
+ * @param r  Ratio to multiply with
+ *
+ * @param t  Number to multiply with
+ *
+ * @return Number of type T, containing multiplication result
+ */
 template<typename T, size_t N, size_t D>
-T operator/( T t, ratio<N, D> r ){
-  return t / double( r );
+constexpr T operator*( ratio<N, D> r, T t ){
+  return t * double( r );
 }
+
+/*! Value of number dividedi by ratio
+ *
+ * @tparam T  Type of number being divided
+ *
+ * @tparam N  Numerator of ratio parameter
+ *
+ * @tparam D  Denominator of ratio parameter
+ *
+ * @param t  Number being divided
+ *
+ * @param r  Ratio dividing by
+ *
+ * @return Number of type T, containing division result
+ */
 template<typename T, size_t N, size_t D>
-T operator/( ratio<N, D> r, T t ){
+constexpr T operator/( T t, ratio<N, D> r ){
   return t / double( r );
 }
 
+/*! Value of ratio divided by number
+ *
+ * @tparam T  Type of number being multiplied with ratio
+ *
+ * @tparam N  Numerator of ratio parameter
+ *
+ * @tparam D  Denominator of ratio parameter
+ *
+ * @param r  Ratio to divide
+ *
+ * @param t  Number to divide by
+ *
+ * @return Number of type T, containing division result
+ */
+template<typename T, size_t N, size_t D>
+constexpr T operator/( ratio<N, D> r, T t ){
+  return double( r ) / t;
+}
+
+/*! Check 'actual' equality of 2 ratios
+ *
+ * @tparam N1  Numerator of lhs
+ *
+ * @tparam D1  Denominator of lhs
+ *
+ * @tparam N2  Numerator of rhs
+ *
+ * @tparam D2  Denominator of rhs
+ *
+ * 'Actual' equality determined by value property
+ */
 template<size_t N1, size_t D1,
          size_t N2, size_t D2>
 constexpr bool operator==( ratio<N1, D1> r1, ratio<N2, D2> r2 ){
@@ -87,6 +213,19 @@ constexpr bool operator==( ratio<N1, D1> r1, ratio<N2, D2> r2 ){
 
   return R1::value == R2::value;
 }
+
+/*! Check 'actual' inequality of 2 ratios
+ *
+ * @tparam N1  Numerator of lhs
+ *
+ * @tparam D1  Denominator of lhs
+ *
+ * @tparam N2  Numerator of rhs
+ *
+ * @tparam D2  Denominator of rhs
+ *
+ * 'Actual' inequality determined by value property
+ */
 template<size_t N1, size_t D1,
          size_t N2, size_t D2>
 constexpr bool operator!=( ratio<N1, D1> r1, ratio<N2, D2> r2 ){
