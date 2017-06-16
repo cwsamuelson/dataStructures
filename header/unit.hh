0e4ef7e5a51c7f9a2ab48d3fd2129c0509f34879
Chris Samuelson <chris.sam55@gmail.com>
2017-06-16 11:15:50 -0400

    Added angles as a new measurement, and several derived units

--- header/unit.hh
+++ header/unit.hh
@@ -19,6 +19,10 @@ namespace gsw{
  *
  * @tparam CANDELA  Brightness
  *
+ * @tparam DEGREE  Degrees of angle
+ *
+ * @tparam PERCENTAGE  Percent
+ *
  * @tparam TICK  Custom user type.  A common usage may be tick, but can be
  *               used for anything
  *
@@ -35,7 +39,7 @@ namespace gsw{
  * appropriate type.  For instance if speed is multiplied by time, the result
  * will be of type distance.
  */
-template<int METERS, int SECONDS, int KILOGRAM, int AMPERE, int KELVIN, int CANDELA, int PERCENTAGE = 0, int TICK = 0, typename DBL = double, typename FACTOR = ratio<1, 1> >
+template<int METERS, int SECONDS, int KILOGRAM, int AMPERE, int KELVIN, int CANDELA, int DEGREE, int PERCENTAGE = 0, int TICK = 0, typename DBL = double, typename FACTOR = ratio<1, 1> >
 class unit{
 public:
   typedef DBL value_type;
@@ -444,11 +448,12 @@ public:
  *         equivalent parameters of the lhs and rhs.
  *
  */
-template<int METERS1, int SECONDS1, int KILOGRAM1, int AMPERE1, int KELVIN1, int CANDELA1, int PERCENTAGE1, int TICK1,
-         int METERS2, int SECONDS2, int KILOGRAM2, int AMPERE2, int KELVIN2, int CANDELA2, int PERCENTAGE2, int TICK2,
+template<int METERS1, int SECONDS1, int KILOGRAM1, int AMPERE1, int KELVIN1, int CANDELA1, int DEGREE1, int PERCENTAGE1, int TICK1,
+         int METERS2, int SECONDS2, int KILOGRAM2, int AMPERE2, int KELVIN2, int CANDELA2, int DEGREE2, int PERCENTAGE2, int TICK2,
          typename D_t1, typename D_t2, typename F_t1, typename F_t2>
 unit<METERS1 + METERS2, SECONDS1 + SECONDS2, KILOGRAM1 + KILOGRAM2, AMPERE1 + AMPERE2,
-     KELVIN1 + KELVIN2, CANDELA1 + CANDELA2, PERCENTAGE1 + PERCENTAGE2, TICK1 + TICK2, D_t1, F_t1>
+     KELVIN1 + KELVIN2, CANDELA1 + CANDELA2, DEGREE1 + DEGREE2, PERCENTAGE1 + PERCENTAGE2, TICK1 + TICK2,
+     D_t1, F_t1>
 operator*( const unit<METERS1, SECONDS1, KILOGRAM1, AMPERE1, KELVIN1, CANDELA1, PERCENTAGE1, TICK1, D_t1, F_t1>& lhs,
            const unit<METERS2, SECONDS2, KILOGRAM2, AMPERE2, KELVIN2, CANDELA2, PERCENTAGE2, TICK2, D_t2, F_t2>& rhs ){
   return lhs.getRaw() * rhs.getRaw();
@@ -464,71 +469,84 @@ operator*( const unit<METERS1, SECONDS1, KILOGRAM1, AMPERE1, KELVIN1, CANDELA1, 
  *         of the equivalent parameters of the lhs and rhs.
  *
  */
-template<int METERS1, int SECONDS1, int KILOGRAM1, int AMPERE1, int KELVIN1, int CANDELA1, int PERCENTAGE1, int TICK1,
-         int METERS2, int SECONDS2, int KILOGRAM2, int AMPERE2, int KELVIN2, int CANDELA2, int PERCENTAGE2, int TICK2,
+template<int METERS1, int SECONDS1, int KILOGRAM1, int AMPERE1, int KELVIN1, int CANDELA1, int DEGREE1, int PERCENTAGE1, int TICK1,
+         int METERS2, int SECONDS2, int KILOGRAM2, int AMPERE2, int KELVIN2, int CANDELA2, int DEGREE2, int PERCENTAGE2, int TICK2,
          typename D_t1, typename D_t2, typename F_t1, typename F_t2>
 unit<METERS1 - METERS2, SECONDS1 - SECONDS2, KILOGRAM1 - KILOGRAM2, AMPERE1 - AMPERE2,
-     KELVIN1 - KELVIN2, CANDELA1 - CANDELA2, PERCENTAGE1 - PERCENTAGE2, TICK1 - TICK2, D_t1, F_t1>
+     KELVIN1 - KELVIN2, CANDELA1 - CANDELA2, DEGREE1 - DEGREE2, PERCENTAGE1 - PERCENTAGE2, TICK1 - TICK2,
+     D_t1, F_t1>
 operator/( const unit<METERS1, SECONDS1, KILOGRAM1, AMPERE1, KELVIN1, CANDELA1, PERCENTAGE1, TICK1, D_t1, F_t1>& lhs,
            const unit<METERS2, SECONDS2, KILOGRAM2, AMPERE2, KELVIN2, CANDELA2, PERCENTAGE2, TICK2, D_t2, F_t2>& rhs ){
   return ( lhs.getRaw() / rhs.getRaw() ) / F_t1::value;
 }
 
-/* TODO:'thyme' should be renamed back to 'time' when a namespace is established, as
- * name collision would be disambiguated using <namespace>::time vs ( i.e. ) std::time
- */
-//                          m   s   kg  A   K  C  P  T
+//                          m   s  kg   A   K  C  D  P  T
 template<typename T = double, typename F = ratio<1,1> >
-using length        = unit< 1,  0,  0,  0,  0, 0, 0, 0, T, F>;
+using length        = unit< 1,  0,  0,  0,  0, 0, 0, 0, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using area          = unit< 2,  0,  0,  0,  0, 0, 0, 0, T, F>;
+using mass          = unit< 0,  0,  1,  0,  0, 0, 0, 0, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using volume        = unit< 3,  0,  0,  0,  0, 0, 0, 0, T, F>;
+using current       = unit< 0,  0,  0,  1,  0, 0, 0, 0, 0, T, F>;//amps
 template<typename T = double, typename F = ratio<1,1> >
-using mass          = unit< 0,  0,  1,  0,  0, 0, 0, 0, T, F>;
+using temperature   = unit< 0,  0,  0,  0,  1, 0, 0, 0, 0, T, F>;//kelvin
 template<typename T = double, typename F = ratio<1,1> >
-using current       = unit< 0,  0,  0,  1,  0, 0, 0, 0, T, F>;//amps
+using l_intensity   = unit< 0,  0,  0,  0,  0, 1, 0, 0, 0, T, F>;//luminous intensity
 template<typename T = double, typename F = ratio<1,1> >
-using temperature   = unit< 0,  0,  0,  0,  1, 0, 0, 0, T, F>;//kelvin
+using time          = unit< 0,  1,  0,  0,  0, 0, 0, 0, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using brightness    = unit< 0,  0,  0,  0,  0, 1, 0, 0, T, F>;//candela
+using tick          = unit< 0,  0,  0,  0,  0, 0, 0, 0, 1, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using thyme         = unit< 0,  1,  0,  0,  0, 0, 0, 0, T, F>;//time, renamed to avoid collision
+using percent       = unit< 0,  0,  0,  0,  0, 0, 0, 1, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using tick          = unit< 0,  0,  0,  0,  0, 0, 0, 1, T, F>;
+using frequency     = unit< 0, -1,  0,  0,  0, 0, 0, 0, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using percent       = unit< 0,  0,  0,  0,  0, 0, 1, 0, T, F>;
+using angle         = unit< 0,  0,  0,  0,  0, 0, 1, 0, 0, T, F>;
+template<typename T = double, typename F = ratio<1,1> >
+using solid_angle   = unit< 0,  0,  0,  0,  0, 0, 2, 0, 0, T, F>;
 
 template<typename T = double, typename F = ratio<1,1> >
-using speed         = unit< 1, -1,  0,  0,  0, 0, 0, 0, T, F>;
+using area          = decltype( length<T, F>() * length<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using acceleration  = unit< 1, -2,  0,  0,  0, 0, 0, 0, T, F>;
+using volume        = decltype( length<T, F>() * length<T, F>() * length<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using force         = unit< 1, -2,  1,  0,  0, 0, 0, 0, T, F>;//newton
+using l_flux        = decltype( l_intensity<T, F>() * solid_angle<T, F>() );//lumen
 template<typename T = double, typename F = ratio<1,1> >
-using momentum      = unit< 1, -1,  1,  0,  0, 0, 0, 0, T, F>;
+using l_illuminance = decltype( l_flux<T, F>() / area<T, F>() );//lux
 template<typename T = double, typename F = ratio<1,1> >
-using energy        = unit< 2, -2,  1,  0,  0, 0, 0, 0, T, F>;//joules
+using l_exposure    = decltype( l_illuminance<T, F>() * time<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using power         = unit< 2, -3,  1,  0,  0, 0, 0, 0, T, F>;//watts
+using l_energy      = decltype( l_flux<T, F>() * time<T, F>() );
 
 template<typename T = double, typename F = ratio<1,1> >
-using voltage       = unit< 2, -3,  1, -1,  0, 0, 0, 0, T, F>;
+using speed         = decltype( length<T, F>() / time<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using resistance    = unit< 2, -3,  1, -2,  0, 0, 0, 0, T, F>;//ohms
+using acceleration  = decltype( speed<T, F>() / time<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using capacitance   = unit<-2,  4, -1,  2,  0, 0, 0, 0, T, F>;//farad
+using force         = decltype( mass<T, F>() * acceleration<T, F>() );//newtons
 template<typename T = double, typename F = ratio<1,1> >
-using inductance    = unit< 2, -2,  1, -2,  0, 0, 0, 0, T, F>;//henrys
+using momentum      = decltype( mass<T, F>() * speed<T, F>() );
 template<typename T = double, typename F = ratio<1,1> >
-using charge        = unit< 0,  1,  0,  1,  0, 0, 0, 0, T, F>;//coulomb
+using energy        = unit< 2, -2,  1,  0,  0, 0, 0, 0, 0, T, F>;//joules
 template<typename T = double, typename F = ratio<1,1> >
-using coulomb = charge<T, F>;
+using power         = decltype( energy<T, F>() / time<T, F>() );//watts
 
 template<typename T = double, typename F = ratio<1,1> >
-using mag_field     = unit< 0, -2,  1,  1,  0, 0, 0, 0, T, F>;//tesla
+using voltage       = unit< 2, -3,  1, -1,  0, 0, 0, 0, 0, T, F>;
 template<typename T = double, typename F = ratio<1,1> >
-using elec_field    = unit< 1, -3,  1, -1,  0, 0, 0, 0, T, F>;
+using resistance    = unit< 2, -3,  1, -2,  0, 0, 0, 0, 0, T, F>;//ohms
+template<typename T = double, typename F = ratio<1,1> >
+using capacitance   = unit<-2,  4, -1,  2,  0, 0, 0, 0, 0, T, F>;//farad
+template<typename T = double, typename F = ratio<1,1> >
+using inductance    = unit< 2, -2,  1, -2,  0, 0, 0, 0, 0, T, F>;//henrys
+template<typename T = double, typename F = ratio<1,1> >
+using charge        = unit< 0,  1,  0,  1,  0, 0, 0, 0, 0, T, F>;//coulomb
+template<typename T = double, typename F = ratio<1,1> >
+using coulomb       = charge<T, F>;
+
+template<typename T = double, typename F = ratio<1,1> >
+using mag_field     = unit< 0, -2,  1,  1,  0, 0, 0, 0, 0, T, F>;//tesla
+template<typename T = double, typename F = ratio<1,1> >
+using elec_field    = unit< 1, -3,  1, -1,  0, 0, 0, 0, 0, T, F>;
 
 template<typename T = double, typename F = ratio<1,1> >
 using specificHeat  = decltype( energy<T, F>() / temperature<T, F>() );
