369e431d83ef7bf27541d820edbdabdb60b94a3b
Chris <chris.sam55@gmail.com>
2017-08-04 16:01:26 -0400

    Added some docs to remove_cv

--- header/remove_cv.hh
+++ header/remove_cv.hh
@@ -3,30 +3,68 @@
 
 namespace gsw{
 
+/*! Removes const qualifier
+ *
+ * @tparam
+ *
+ * This class is used when remove_const is used with a class that doesn't have
+ * a const qualifier, so the provided class is presented through type as is.
+ */
 template<typename T>
 class remove_const{
 public:
   using type = T;
 };
 
+/*! Removes const qualifier
+ *
+ * @tparam
+ *
+ * This class is  a template specialization and is used when remove_const is
+ * used with a class that does have a const qualifier, so the provided class is
+ * presented through type with the const qualifier stripped. 
+ */
 template<typename T>
 class remove_const<const T>{
 public:
   using type = T;
 };
 
+/*! Removes volatile qualifier
+ *
+ * @tparam
+ *
+ * This class is used when remove_volatile is used with a class that doesn't
+ * have a volatile qualifier, so the provided class is presented through type
+ * as is.
+ */
 template<typename T>
 class remove_volatile{
 public:
   using type = T;
 };
 
+/*! Removes volatile qualifier
+ *
+ * @tparam
+ *
+ * This class is a template specialization and is used when remove_volatile is
+ * used with a class that does have a volatile qualifier, so the provided class
+ * is presented through type with the volatile qualifier stripped. 
+ */
 template<typename T>
 class remove_volatile<volatile T>{
 public:
   using type = T;
 };
 
+/*! Removes const and volatile qualifiers
+ *
+ * @tparam
+ *
+ * Utilizes remove_const and remove_volatile to remove const and volatile
+ * qualifiers.
+ */
 template<typename T>
 class remove_cv{
 public:
