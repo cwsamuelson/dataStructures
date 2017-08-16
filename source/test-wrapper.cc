d1338ab15340a5ae3731b2434488a24fc31511a4
Chris <chris.sam55@gmail.com>
2017-08-16 15:34:11 -0400

    Augmented ref wrapper tests

--- source/test-wrapper.cc
+++ source/test-wrapper.cc
@@ -1,8 +1,10 @@
+#include<vector>
+
 #include<catch.hpp>
 
 #include<reference_wrapper.hh>
 
-TEST_CASE( "", "[reference_wrapper]"){
+TEST_CASE( "Reference wrappers can be used similar to regular references", "[reference_wrapper]" ){
   int i = 1;
   int j = 2;
 
@@ -12,5 +14,22 @@ TEST_CASE( "", "[reference_wrapper]"){
   i = 3;
   REQUIRE( ref == 3 );
   REQUIRE( refs[0] == 3 );
+  REQUIRE( refs[1] == 2 );
+  REQUIRE( ref.get() == 3 );
+
+  ref.get() = 5;
+  REQUIRE( ref == 5 );
+  REQUIRE( refs[0] == 5 );
+  REQUIRE( refs[1] == 2 );
+  REQUIRE( ref.get() == 5 );
+}
+
+TEST_CASE( "Reference wrappers can be stored in containers", "[reference_wrapper]" ){
+  std::vector<gsw::reference_wrapper<int> > vec;
+  int i = 0;
+
+  vec.emplace_back( i );
+  i = 5;
+  REQUIRE( vec[0] == 5 );
 }
 
