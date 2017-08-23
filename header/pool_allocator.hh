9343c988d8fe45e203a3f97ae23bcbaa01faf1e1
Chris Samuelson <chris.sam55@gmail.com>
2017-08-23 18:33:03 -0400

    Updated pool_allocator implementation
    Added deallocate implementation, and fixed allocate implementation.

--- header/pool_allocator.hh
+++ header/pool_allocator.hh
@@ -1,6 +1,17 @@
 #ifndef __POOL_ALLOCATOR_HH__
 #define __POOL_ALLOCATOR_HH__
 
+#include<exception>
+
+namespace gsw{
+
+class bad_alloc : public std::exception{
+public:
+  virtual const char* what() const noexcept{
+    return "Bad allocation!";
+  }
+};
+
 template<typename T>
 class pool_allocator{
 public:
@@ -16,11 +27,11 @@ private:
 
 public:
   allocator():
+    mIndicators( 0 ),
     pool( new char[sizeof( mIndicators ) * 8 * ptrdiff] ){
   }
 
-  allocator( const allocator& ){
-  }
+  allocator( const allocator& ) = delete;
 
   allocator( allocator&& other ){
     delete[] pool;
@@ -33,14 +44,38 @@ public:
   }
 
   pointer allocate( size_type number ){
-    size_type mask = 1;
+    int caveSize = 0;
+    int index = 0;
 
-    for( unsigned int i = 0; i < sizeof
+    for( unsigned int index = 0; index < sizeof( mIndicators ) * 8 ){
+      if( !( mIndicators & 1 << index ) ){
+        ++caveSize;
+
+        if( caveSize == number ){
+          for( unsigned int i = 0; i < number; ++i ){
+            mIndicators |= ( 1 << ( i + index ) );
+          }
+
+          return pool + ( index * ptrdiff );
+        }
+      }
+
+      ++index;
+    }
+
+    throw bad_alloc();
   }
 
-  void deallocate( pointer ptr ){
+  void deallocate( pointer ptr, size_type number ){
+    unsigned long start = ( ptr - pool ) / ptrdiff;
+
+    for( unsigned int i = 0; i < number; ++i ){
+      mIndicators &= ~( 1 << ( i + start ) );
+    }
   }
 };
+
+}
 
 #endif
 
