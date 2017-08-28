821ce32f01e700444d34b1526a35bda74dd4f5d6
Chris Samuelson <chris.sam55@gmail.com>
2017-08-28 14:06:52 -0400

    Moved static/compile-time strings
    Moved static/compile-time strings to a more semantically meaningful location

--- header/basic_string.hh
+++ header/basic_string.hh
@@ -279,42 +279,6 @@ OSTREAM& operator<<( OSTREAM& os, const basic_string<T>& str ){
   return os;
 }
 
-class test_ct_string{
-private:
-  const char* const mString;
-  const size_t mSize;
-
-public:
-  template<size_t N>
-  constexpr test_ct_string( const char( &arr )[N] ):
-    mString( arr ),
-    mSize( N - 1 ){
-  }
-
-  constexpr char operator[]( size_t n ) const{
-    return n < mSize ? mString[n] : throw 1;
-  }
-
-  constexpr size_t size() const{
-    return mSize;
-  }
-};
-
-template<char ...STRING>
-struct ct_string{
-  static basic_string<char> string(){
-    return basic_string<char>{ STRING... };
-  }
-};
-
-template<typename L, typename R>
-struct concatenate;
-
-template<char ...Ls, char ...Rs>
-struct concatenate<ct_string<Ls...>, ct_string<Rs...> >{
-  using result = ct_string<Ls..., Rs...>;
-};
-
 }
 
 #endif
