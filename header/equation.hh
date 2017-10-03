d6bb1896c3b84de96c147d8d17c23f542dd27532
Chris Samuelson <chris.sam55@gmail.com>
2017-10-03 15:56:54 -0400

    Fleshing out implementation of equation

--- header/equation.hh
+++ header/equation.hh
@@ -11,52 +11,54 @@ namespace gsw{
 
 class equation;
 
-equation operator""_lvar( const char* name, size_t sz );
+equation operator""_evar( const char* name, size_t sz );
 
 /*! Mathematical equation
  */
 class equation{
 public:
-  using data = std::set<std::tuple<std::string, double> >;
+  using data = std::map<std::string, double>;
 
 private:
   struct operation{
-    virtual bool evaluate( const data& variables ) const = 0;
+    virtual double evaluate( const data& variables ) const = 0;
   };
 
   using op_ptr = std::shared_ptr<operation>;
 
   struct variable : public operation{
-    bool evaluate( const data& variables ) const;
+    std::string name;
+
+    double evaluate( const data& variables ) const;
   };
   struct multiplication : public operation{
     op_ptr lhs;
     op_ptr rhs;
 
-    bool evaluate( const data& variables ) const;
+    double evaluate( const data& variables ) const;
   };
   struct division : public operation{
     op_ptr lhs;
     op_ptr rhs;
 
-    bool evaluate( const data& variables ) const;
+    double evaluate( const data& variables ) const;
   };
   struct addition : public operation{
     op_ptr lhs;
     op_ptr rhs;
 
-    bool evaluate( const data& variables ) const;
+    double evaluate( const data& variables ) const;
   };
   struct subtraction : public operation{
     op_ptr lhs;
     op_ptr rhs;
 
-    bool evaluate( const data& variables ) const;
+    double evaluate( const data& variables ) const;
   };
   struct inversion : public operation{
     op_ptr operand;
 
-    bool evaluate( const data& variables ) const;
+    double evaluate( const data& variables ) const;
   };
 
   op_ptr mValue;
--- source/equation.cc
+++ source/equation.cc
@@ -1,7 +1,31 @@
 #include<equation.hh>
 
-equation equation::operator*( const equation& multiplicand ) const{
-  auto var = make_shared<proposition::multiplication>();
+double variable::evaluate( const data& variables ) const{
+  return variables.at( name );
+}
+
+double multiplication::evaluate( const data& variables ) const{
+  return lhs->evaluate( variables ) * rhs->evaluate( variables );
+}
+
+double division::evaluate( const data& variables ) const{
+  return lhs->evaluate( variables ) / rhs->evaluate( variables );
+}
+
+double addition::evaluate( const data& variables ) const{
+  return lhs->evaluate( variables ) + rhs->evaluate( variables );
+}
+
+double subtraction::evaluate( const data& variables ) const{
+  return lhs->evaluate( variables ) - rhs->evaluate( variables );
+}
+
+double inversion::evaluate( const data& variables ) const{
+  return -operand->evaluate( variables );
+}
+
+equation equation::operator*( const equation
+  auto var = make_shared<proposition::multip
   var->lhs = mValue;
   var->rhs = multiplicand;
   return {var};
@@ -51,5 +75,8 @@ std::set<data> solve( const data& variables ) const{
 }
 
 equation operator""_evar( const char* name, size_t sz ){
+  auto var = std::make_shared<equation::variable>();
+  var->name = std::string( name, sz );
+  return {var};
 }
 
