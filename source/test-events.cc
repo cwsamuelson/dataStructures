45e9ec9216b1c8f395d341516c607cefa482e783
Chris Samuelson <chris.sam55@gmail.com>
2017-11-06 16:22:53 +0000

    Added tests for multiple event arguments

--- source/test-events.cc
+++ source/test-events.cc
@@ -14,11 +14,14 @@ TEST_CASE( "Basic event control flow", "[events]" ){
   int g_i = 0;
   int g_j = 0;
   int g_k = 0;
+  int g_l = 0;
+  int g_m = 0;
   foo g_f;
   g_f.x = 0;
 
   gsw::event_channel<int> chan0;
   gsw::event_channel<foo> chan1;
+  gsw::event_channel<int, int> chan2;
 
   gsw::event_channel<int>::handler handler0;
   gsw::event_channel<foo>::handler handler1(
@@ -33,6 +36,11 @@ TEST_CASE( "Basic event control flow", "[events]" ){
                                      [&]( gsw::event_channel<int>, int i ){
                                        g_k = i;
                                      } );
+  gsw::event_channel<int, int>::handler handler4(
+                                     [&]( gsw::event_channel<int, int>, int i, int j ){
+                                       g_l = i;
+                                       g_m = j;
+                                     } );
 
   handler0 =
     [&]( gsw::event_channel<int>, int i ){
@@ -42,20 +50,26 @@ TEST_CASE( "Basic event control flow", "[events]" ){
   chan0 += handler0;
   chan0 += handler2;
   chan1 += handler1;
+  chan2 += handler4;
   auto idx = chan0.enlist( handler3 );
   chan0.delist( idx );
 
   REQUIRE( g_i   == 0 );
   REQUIRE( g_j   == 0 );
   REQUIRE( g_k   == 0 );
+  REQUIRE( g_l   == 0 );
+  REQUIRE( g_m   == 0 );
   REQUIRE( g_f.x == 0 );
 
   chan0.fire(     42 );
   chan1.fire( foo{69} );
+  chan2.fire( 6, 9 );
 
   REQUIRE( g_i   == 42 );
   REQUIRE( g_j   == 42 );
   REQUIRE( g_k   == 0 );
+  REQUIRE( g_l   == 6 );
+  REQUIRE( g_m   == 9 );
   REQUIRE( g_f.x == 69 );
 }
 
