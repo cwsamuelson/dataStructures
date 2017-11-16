0bb1fbafc37e830fdbf39e55fc9bb92534b45e7f
Chris Samuelson <chris.sam55@gmail.com>
2017-11-16 19:46:52 +0000

    Catch now allows arguments in REQUIRE macros
    So a proxy test variable is no longer necessary

--- source/test-traits.cc
+++ source/test-traits.cc
@@ -22,43 +22,30 @@ TEST_CASE( "is_void", "[traits]" ){
 
 TEST_CASE( "remove_cv", "[traits]" ){
   gsw::remove_cv<const int>::type G = 5;
-  bool test;
 
   REQUIRE( ( G += 5 ) == 10 );
 
-  test = gsw::is_same<gsw::remove_cv<const int>::type, int>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_cv<int>::type,       int>::value;
-  REQUIRE( test );
+  REQUIRE( gsw::is_same<gsw::remove_cv<const int>::type, int>::value );
+  REQUIRE( gsw::is_same<gsw::remove_cv<int>::type,       int>::value );
 
-  test = gsw::is_same<gsw::remove_reference<int&&>::type, int>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_reference<int&>::type,  int>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_reference<int>::type,   int>::value;
-  REQUIRE( test );
+  REQUIRE( gsw::is_same<gsw::remove_reference<int&&>::type, int>::value );
+  REQUIRE( gsw::is_same<gsw::remove_reference<int&>::type,  int>::value );
+  REQUIRE( gsw::is_same<gsw::remove_reference<int>::type,   int>::value );
 
-  test = gsw::is_same<gsw::remove_extent<int[12]>::type, int*>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_extent<int>::type,     int>::value;
-  REQUIRE( test );
+  REQUIRE( gsw::is_same<gsw::remove_extent<int[12]>::type, int*>::value );
+  REQUIRE( gsw::is_same<gsw::remove_extent<int>::type,     int>::value );
 
-  test = gsw::is_same<gsw::remove_pointer<int*>::type, int>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_pointer<int>::type,  int>::value;
-  REQUIRE( test );
+  REQUIRE( gsw::is_same<gsw::remove_pointer<int*>::type, int>::value );
+  REQUIRE( gsw::is_same<gsw::remove_pointer<int>::type,  int>::value );
 
-  test = gsw::is_same<gsw::remove_pointer<int*[12]>::type, int*[12]>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_extent<int*[12]>::type, int**>::value;
-  REQUIRE( test );
-  test = gsw::is_same<gsw::remove_pointer<
-                        gsw::remove_pointer<
-                          gsw::remove_extent<
-                            int*[12]>::type>
-                          ::type>
-                        ::type,
-                      int>::value;
-  REQUIRE( test );
+  REQUIRE( gsw::is_same<gsw::remove_pointer<int*[12]>::type, int*[12]>::value );
+  REQUIRE( gsw::is_same<gsw::remove_extent<int*[12]>::type, int**>::value );
+  REQUIRE( gsw::is_same<gsw::remove_pointer<
+                          gsw::remove_pointer<
+                            gsw::remove_extent<
+                              int*[12]>::type>
+                            ::type>
+                          ::type,
+                        int>::value );
 }
 
