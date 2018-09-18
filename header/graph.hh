b630e7596e8903d5c262544e276fd29a1fc38cef
Chris <chris.sam55@gmail.com>
2018-09-18 12:42:16 -0400

    Fleshed out graph interface some

--- header/graph.hh
+++ header/graph.hh
@@ -1,33 +1,62 @@
 #ifndef __GRAPH_HH__
 #define __GRAPH_HH__
 
+#include<utility>
 #include<map>
 #include<set>
 
+//TODO: consider associating data with edges
+//associating data with a node is meaningless for this data structure; this
+//  information is more appropriately stored at the client.  However the data of
+//  existing edges is intrinsically stored and tracked in this class, and could
+//  be useful to obtain.
+/*!
+ * @tparam T
+ */
 template<typename T>
 class graph{
 public:
   using value_type = T;
   using reference = T&;
   using iterator = /**/;
+  using edge = std::pair<value_type, value_type>;
 
 private:
   std::map<value_type, std::set<value_type> > mNodes;
 
 public:
-  void add_node( const reference ref );
-  void add_edge( const reference refA, const reference refB, bool is_directed = false );
-  void remove_edge( const reference refA, const reference refB );
+  void
+  add_node( const reference ref );
+  void
+  remove_node( const reference ref );
 
-  set get_nodes();
-  set get_edges();
+  void
+  add_edge( const reference refA, const reference refB, bool is_directed = false );
+  void
+  remove_edge( const reference refA, const reference refB );
 
-  set get_out_edges() const;
-  set get_in_edges() const;
+  set<value_type>
+  get_nodes() const;
+  set<edge>
+  get_edges() const;//?
 
-  size_t size() const;
-  iterator begin();
-  iterator end();
+  set<edge>
+  get_out_edges() const;
+  set<edge>
+  get_in_edges() const;
+
+  bool
+  adjacent( const reference refA, const reference refB ) const;
+  set<value_type>
+  neighbors( const reference ref ) const;
+
+  size_t
+  node_count() const;
+  size_t
+  edge_count() const;
+
+  void
+  clear() const;
 };
 
 #endif
