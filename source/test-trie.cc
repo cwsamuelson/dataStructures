23d7269a900c830b0c73b008768dce7f9cbd60cb
Chris <chris.sam55@gmail.com>
2018-10-31 15:21:43 -0400

    Created some starter tests
    Some basic tests that show the function of the prefix-tree

--- source/test-trie.cc
+++ source/test-trie.cc
@@ -1,9 +1,22 @@
+#include<string>
+
 #include<catch.hpp>
 
 #include<trie.hh>
 
-TEST_CASE( "", "" ){
-  SECTION( "" ){
-    REQUIRE( );
-  }
+using namespace std;
+using namespace gsw;
+
+TEST_CASE( "", "[trie]" ){
+  prefix_tree<string, int> tree;
+
+  tree.insert( "1", 2 );
+  REQUIRE( tree.contains( "1" ) );
+  REQUIRE( tree.find( "1" ).size() == 1 );
+
+  tree.insert( "12", 3 );
+  REQUIRE( tree.find( "1" ).size() == 2 );
+  REQUIRE( tree.find( "1" ) == set<int>{2, 3} );
+  REQUIRE( tree.contains( "1" ) );
+  REQUIRE( tree.contains( "12" ) );
 }
