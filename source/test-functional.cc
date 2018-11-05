3c9b031a691438fdc4e38377027b2268715666af
Chris <chris.sam55@gmail.com>
2018-11-05 12:51:16 -0500

    Reimplemented function to use type erasure
    Now gsw::function can use functors!

--- source/test-functional.cc
+++ source/test-functional.cc
@@ -6,19 +6,29 @@ int testfn(){
   return 5;
 }
 
+class functor{
+public:
+  int
+  operator()( int i ){
+    return i + 5;
+  }
+};
+
 TEST_CASE( "Function objects are used as normal functions.", "[function]" ){
   gsw::function<int( int )> fn;
+  gsw::function<int( int )> fn2;
   gsw::function<int( int, int )> nf;
   gsw::function<int()> tst;
-  
+
   fn = []( int x ){ return 1 + x; };
   nf = []( int x, int y ){ return x + y; };
   tst = testfn;
-  
+  fn2 = functor();
+
   SECTION( "Calling functions produce expected results." ){
     REQUIRE( fn( 3 ) == 4 );
     REQUIRE( nf( 1, 2 ) == 3 );
     REQUIRE( tst() == 5 );
+    REQUIRE( fn2( 5 ) == 10 );
   }
 }
-
