d6b82f5038bb2c6da6adb9f5b29dd57fe427e576
Chris <chris.sam55@gmail.com>
2018-11-07 12:38:15 -0500

    Changed how delayed and schedule were called
    Made the template parameter for the return type to be deducible, by specifying it differently.

--- header/timer.h
+++ header/timer.h
@@ -5,6 +5,9 @@
 #include<thread>
 #include<future>
 
+/*!
+ * see notes in cppreference async about multiple sequential async calls
+ */
 class timer{
 private:
   bool mCancel = false;
@@ -13,9 +16,9 @@ public:
   template<typename T>
   using callback = std::function<T()>;
 
-  template<typename Rep, typename Period, typename T>
+  template<typename Rep, typename Period>
   void
-  interval( callback<T> fn, const std::chrono::duration<Rep, Period>& duration ){
+  interval( callback<void> fn, const std::chrono::duration<Rep, Period>& duration ){
     mCancel = false;
 
     std::thread t( [=](){
@@ -30,9 +33,9 @@ public:
     t.detach();
   }
 
-  template<typename Rep, typename Period, typename T>
-  std::future<T>
-  delayed( callback<T> fn, const std::chrono::duration<Rep, Period>& duration ){
+  template<typename Rep, typename Period, typename callback_function>
+  auto
+  delayed( callback_function fn, const std::chrono::duration<Rep, Period>& duration )->decltype( std::async( std::launch::async, fn ) ){
     mCancel = false;
 
     return std::async( std::launch::async, [=](){
@@ -43,9 +46,9 @@ public:
     } );
   }
 
-  template<typename clock, typename duration, typename T>
-  std::future<T>
-  schedule( callback<T> fn, const std::chrono::time_point<clock, duration>& time ){
+  template<typename clock, typename duration, typename callback_function>
+  auto
+  schedule( callback_function fn, const std::chrono::time_point<clock, duration>& time )->decltype( std::async( std::launch::async, fn ) ){
     mCancel = false;
 
     return std::async( std::launch::async, [=](){
