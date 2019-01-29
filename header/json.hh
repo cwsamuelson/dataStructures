99501267dd87901340e6c8e35596c84fc431877a
Chris Samuelson <chris.sam55@gmail.com>
2019-01-29 08:56:21 -0500

    Changed json to use std::variant for internals

--- header/json.hh
+++ header/json.hh
@@ -5,6 +5,7 @@
 #include<vector>
 #include<map>
 #include<utility>
+#include<variant>
 
 // json supports int, string, array/vector, and object/map
 /*!
@@ -18,123 +19,7 @@ class json{
   using float_t = double;
   using bool_t = bool;
 
-  enum class json_type{
-    null,
-    object,
-    array,
-    string,
-    integer,
-    uinteger,
-    float_pt,
-    boolean,
-  }
-
-  union json_value{
-    object_t* object;
-    array_t* array;
-    string_t* string;
-    integer_t integer;
-    u_integer_t uinteger;
-    float_t float_pt;
-    bool_t boolean;
-
-    json_value() = default;
-
-    json_value( integer_t i )
-      : integer( i ){
-    }
-
-    json_value( u_integer_t ui )
-      : uinteger( ui ){
-    }
-
-    json_value( bool_t b )
-      : bool_t( b ){
-    }
-
-    json_value( const string_t& str )
-      : string( str ){
-    }
-
-    json_value( string_t&& str )
-      : string( std::move( str ) ){
-    }
-
-    json_value( const array_t& arr )
-      : string( arr ){
-    }
-
-    json_value( array_t&& arr )
-      : string( std::move( arr ) ){
-    }
-
-    json_value( const object_t& obj )
-      : string( obj ){
-    }
-
-    json_value( object_t&& obj )
-      : string( std::move( obj ) ){
-    }
-
-    json_value( json_type type ){
-      switch( type ){
-      case json_type::null:
-        object = nullptr;
-      break;
-
-      case json_type::object:
-        object = new object_t;
-      break;
-
-      case json_type::array:
-        array = new array_t;
-      break;
-
-      case json_type::string:
-        string = new string_t;
-      break;
-
-      case json_type::integer:
-        integer = integer_t( 0 );
-      break;
-
-      case json_type::uinteger:
-        uinteger = u_integer_t( 0 );
-      break;
-
-      case json_type::float_pt:
-        float_pt = float_t( 0.0 );
-      break;
-
-      case json_type::boolean:
-        boolean = bool_t( false );
-      break;
-      }
-    }
-
-    void
-    destroy( json_type type ){
-      switch( type ){
-      case json_type::object:
-        delete object;
-      break;
-
-      case json_type::array:
-        delete array;
-      break;
-
-      case json_type::string:
-        delete object;
-      break;
-
-      default:
-      break;
-      }
-    }
-  }
-
-  json_type mType;
-  json_value mValue;
+  std::variant<integer_t, u_integer_t, float_t, bool_t, string_t, array_t, object_t> mData;
 };
 
 #endif
