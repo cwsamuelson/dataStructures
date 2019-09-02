ccbded6aebcef3d4980b2ac600281ece5bbea2a3
Chris <chris.sam55@gmail.com>
2019-09-02 18:46:51 -0400

    Wrote some actual named_point tests

--- named_point/tests.cc
+++ named_point/tests.cc
@@ -11,10 +11,23 @@ TEST_CASE("Named point testing", "[named]")
 
   p1_1 = p1_2;
 
+  CHECK(p1_1 == gsw::point1(2.0));
+  CHECK(p1_1 == gsw::point1::x_type(2.0));
+  CHECK(p1_1[0] == 2.0);
+  CHECK(p1_1.x() == 2.0);
+
   gsw::point2 p2_1 = {0.0, 1.0};
   gsw::point2 p2_2{0.0, 2.0};
   gsw::point2 p2_3{{0.0, 3.0}};
 
-  CHECK(false);
+  p2_1 = p2_2;
+
+  CHECK(p2_1 == gsw::point2{0.0, 2.0});
+  CHECK(p2_1 == gsw::point2::x_type(0.0));
+  CHECK(p2_1 == gsw::point2::y_type(2.0));
+  CHECK(p2_1[0] == 0.0);
+  CHECK(p2_1[1] == 2.0);
+  CHECK(p2_1.x() == 0.0);
+  CHECK(p2_1.y() == 2.0);
 }
 
