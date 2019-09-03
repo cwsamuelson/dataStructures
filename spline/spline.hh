98daa272a258b17e1a7604b63e6a9dfb5374459a
Chris <chris.sam55@gmail.com>
2019-09-02 22:14:31 -0400

    Created initial spline definition

--- spline/spline.hh
+++ spline/spline.hh
@@ -5,15 +5,39 @@
 #ifndef GALACTICSTRUCTURES_SPLINE_HH
 #define GALACTICSTRUCTURES_SPLINE_HH
 
+#include <vector>
+#include <polynomial.hh>
+
+namespace gsw {
+
+struct point2d{
+  float x;
+  float y;
+};
+
 /** Catmull-Rom
+ * maybe take a polynomial as an argument?
  */
-template<bool LOOPED = true>
-class spline{
+ //@todo: looping?
+class spline
+{
 private:
-  constexpr bool isLooped = LOOPED;
+  std::vector<point2d> mPoints;
+  gsw::polynomial mPoly0;
+  gsw::polynomial mPoly1;
+  gsw::polynomial mPoly2;
+  gsw::polynomial mPoly3;
 
 public:
+  spline();
 
+  point2d
+  operator()(float p);
+
+  point2d
+  gradient(float p);
 };
+
+}
 
 #endif //GALACTICSTRUCTURES_SPLINE_HH
