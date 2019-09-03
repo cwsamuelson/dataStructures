ef6a1eab92ee9d34a2e6dc42667c4a44f76c2e55
Chris <chris.sam55@gmail.com>
2019-09-02 22:12:43 -0400

    Added some named_type tests

--- named_type/tests.cc
+++ named_type/tests.cc
@@ -4,6 +4,18 @@
 #include <named_type.hh>
 
 TEST_CASE("Named type testing", "[named]"){
-  CHECK(false);
+  gsw::named_type<int, struct foo> x;
+  CHECK(x == 0);
+  CHECK(x.get() == 0);
+
+  gsw::named_type<int, struct bar> y(1);
+  //x = y;//not possible
+  x = y.get();
+  CHECK(x == 1);
+  CHECK(y.get() == 1);
+
+  x = 5;
+  CHECK(x == 5);
+  CHECK(x.get() == 5);
 }
 
