9f6c162ff4ec2cf158b4b0400ebaefc185d24a41
Chris <chris.sam55@gmail.com>
2019-09-15 20:53:42 -0400

    Fixed unit test header for spline tests

--- spline/tests.cc
+++ spline/tests.cc
@@ -1,12 +1,13 @@
 #include<string>
 
-#include<catch.hpp>
+#define CATCH_CONFIG_MAIN
+#include<catch2/catch.hpp>
 
 #include<spline.hh>
 
 using namespace std;
 
-TEST_CASE( "", "" ){
+TEST_CASE("", ""){
   SECTION(""){
     CHECK(false);
   }
