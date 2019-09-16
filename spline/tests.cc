f935c20f80f1c86fd08072826fbd7078062cdc26
Chris <chris.sam55@gmail.com>
2019-09-16 18:35:30 -0400

    Wrote some spline test

--- spline/tests.cc
+++ spline/tests.cc
@@ -7,9 +7,20 @@
 
 using namespace std;
 
-TEST_CASE("", ""){
-  SECTION(""){
-    CHECK(false);
-  }
+TEST_CASE("Manual point insertion", "[spline]"){
+  gsw::spline s;
+
+  s.points.push_back({0, 0});
+  s.points.push_back({1, 0});
+  s.points.push_back({2, 0});
+  s.points.push_back({3, 0});
+
+  CHECK(s.point(1.5) == gsw::point2d{0.5f, 0.0f});
 }
 
+TEST_CASE("Initialized with points", "[spline]")
+{
+  gsw::spline s{{0, 0}, {1, 0}, {2, 0}};
+
+  CHECK(s.point(1.5) == gsw::point2d{0.5f, 0.0f});
+}
