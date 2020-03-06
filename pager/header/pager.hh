fcb6a6a69c03ec7d88de4adb09d10632e029b553
Chris <chris.sam55@gmail.com>
2020-03-05 21:50:03 -0500

    Implemented page saving and loading

--- pager/header/pager.hh
+++ pager/header/pager.hh
@@ -1,7 +1,7 @@
 #ifndef GALACTICSTRUCTURES_PAGER_HH
 #define GALACTICSTRUCTURES_PAGER_HH
 
-#include <fstream>
+#include <cstdio>
 #include <memory>
 #include <map>
 
@@ -28,19 +28,49 @@ private:
   size_t mPageCount;
   ptr_type mPageMemory;
   std::map<size_t, size_t> mMemoryMap; /**< map of item index to memory index */
+  std::map<size_t, std::FILE*> mFileMap;
 
   int victimize(){
     return mMemoryMap.begin()->first;
   }
 
-  void save_page(size_t){}
+  void save_page(size_t page_id){
+    std::FILE* f;
+    if(mFileMap.count(page_id) == 0){
+      mFileMap[page_id] = tmpfile();
+    }
 
-  void load_page(size_t){}
+    f = mFileMap[page_id];
+
+    std::rewind(f);
+
+    for(size_t i = 0; i < mPageSize; ++i){
+      char arr[sizeof(value_type)];
+      std::memcpy(arr, &mPageMemory[i], sizeof(value_type));
+
+      for(size_t j = 0; j < sizeof(value_type); ++j){
+        std::fputc(arr[j], f);
+      }
+    }
+  }
+
+  void load_page(size_t dst_page_id, size_t src_page_id){
+    if(mFileMap.count(src_page_id) == 0) return;
+
+    std::FILE* f = mFileMap.at(src_page_id);
+    auto offset = mMemoryMap.at(dst_page_id);
+
+    std::rewind(f);
+
+    for(size_t i = 0; i < sizeof(value_type) * mPageSize; ++i){
+      mPageMemory[i + offset] = char(std::fgetc(f));
+    }
+  }
 
   void page_fault(index_t page_id){
     auto victim_page_id = victimize();
     save_page(victim_page_id);
-    load_page(page_id);
+    load_page(victim_page_id, page_id);
 
     auto offset = mMemoryMap.at(victim_page_id);
     mMemoryMap.erase(victim_page_id);
