d83127805c01dcb2f03bb34768a555723e274fd3
Chris <chris.sam55@gmail.com>
2020-03-05 22:22:07 -0500

    more complete custom exception

--- error/header/errors.hh
+++ error/header/errors.hh
@@ -6,9 +6,41 @@
 
 namespace gsw{
 
-class exception : public std::exception{
+class exception : public std::runtime_error{
+private:
+  std::string mFile;
+  std::string mFunction;
+  size_t mLine;
+  std::string mMessage;
+
 public:
-  exception(std::string file, std::string function, size_t line, std::string message){}
+  exception(std::string file, std::string function, size_t line, std::string message)
+    : std::runtime_error(message + " on line: " + std::to_string(line) + " in function " + function + " and file " + file)
+    , mFile(std::move(file))
+    , mFunction(std::move(function))
+    , mLine(line)
+    , mMessage(std::move(message))
+  {}
+
+  [[nodiscard]]
+  auto file() const{
+    return mFile;
+  }
+
+  [[nodiscard]]
+  auto function() const{
+    return mFunction;
+  }
+
+  [[nodiscard]]
+  auto line() const{
+    return mLine;
+  }
+
+  [[nodiscard]]
+  auto message() const{
+    return mMessage;
+  }
 };
 
 #define GSW_WRAP(anything) do{anything;}while(false);
