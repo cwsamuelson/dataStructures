f1be9ee29b27cbd965898e7dda76fa277175e19a
Chris <chris.sam55@gmail.com>
2020-03-19 21:23:59 -0400

    stringify cleanup

--- stringify/header/catch_stringify_helper.hh
+++ stringify/header/catch_stringify_helper.hh
@@ -7,21 +7,12 @@
 #include <set>
 
 // define how to stringify a set for catch error printing
-namespace Catch {
-  /*template<typename T, typename Compare, typename Allocator>
-  struct StringMaker<std::set<T, Compare, Allocator>> {
-      static std::string convert(const std::set<T, Compare, Allocator>& s) {
-        return ::Catch::Detail::rangeToString(s.begin(), s.end());
-      }
-  };*/
-}
-
 template<typename OSTREAM, typename T, typename Compare, typename Allocator>
 OSTREAM& operator<<(OSTREAM& os, const std::set<T, Compare, Allocator>& s) {
   return os << ::Catch::Detail::rangeToString(s.begin(), s.end());
 }
 
-template<typename OSTREAM, typename T, typename Compare, typename Allocator, typename MEAS, typename SYSTEM, typename DBL, typename FACTOR>
+template<typename OSTREAM, typename MEAS, typename SYSTEM, typename DBL, typename FACTOR>
 OSTREAM& operator<<(OSTREAM& os, const gsw::unit<MEAS, SYSTEM, DBL, FACTOR>& u) {
   return os << u.getValue();
 }
