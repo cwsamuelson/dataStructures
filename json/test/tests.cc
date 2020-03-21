b047926c39c00e9bb534a9cf4e9e7236a1748992
Chris <chris.sam55@gmail.com>
2020-03-21 15:28:37 -0400

    setup some baseline type detection tests

--- json/test/tests.cc
+++ json/test/tests.cc
@@ -4,11 +4,106 @@
 
 #include <json.hh>
 
-TEST_CASE("", "") {
-  gsw::json j;
+TEST_CASE("", "[json]") {
+  SECTION("'uninit' type"){
+    gsw::json j;
 
-  SECTION("") {
-    CHECK(false);
+    CHECK(!j.is_object());
+    CHECK(!j.is_array());
+    CHECK(!j.is_string());
+    CHECK(!j.is_integer());
+    CHECK(!j.is_floating());
+    CHECK(!j.is_bool());
+    CHECK(!j.is_signed());
+    CHECK(!j.is_unsigned());
+  }
+
+  SECTION("'object' type") {
+    gsw::json j;
+  }
+
+  SECTION("'array' type") {
+    gsw::json j;
+  }
+
+  SECTION("'string' type") {
+    gsw::json j("");
+
+    CHECK(!j.is_object());
+    CHECK(!j.is_array());
+    CHECK( j.is_string());
+    CHECK(!j.is_integer());
+    CHECK(!j.is_floating());
+    CHECK(!j.is_bool());
+    CHECK(!j.is_signed());
+    CHECK(!j.is_unsigned());
+  }
+
+  SECTION("'signed integer' type") {
+    gsw::json j(5LL);
+
+    CHECK(!j.is_object());
+    CHECK(!j.is_array());
+    CHECK(!j.is_string());
+    CHECK( j.is_integer());
+    CHECK(!j.is_floating());
+    CHECK(!j.is_bool());
+    CHECK( j.is_signed());
+    CHECK(!j.is_unsigned());
+  }
+
+  SECTION("'unsigned integer' type") {
+    gsw::json j(5ULL);
+
+    CHECK(!j.is_object());
+    CHECK(!j.is_array());
+    CHECK(!j.is_string());
+    CHECK( j.is_integer());
+    CHECK(!j.is_floating());
+    CHECK(!j.is_bool());
+    CHECK(!j.is_signed());
+    CHECK( j.is_unsigned());
+  }
+
+  SECTION("'floating point' type") {
+    SECTION("float"){
+      gsw::json j(0.0f);
+
+      CHECK(!j.is_object());
+      CHECK(!j.is_array());
+      CHECK(!j.is_string());
+      CHECK(!j.is_integer());
+      CHECK( j.is_floating());
+      CHECK(!j.is_bool());
+      CHECK( j.is_signed());
+      CHECK(!j.is_unsigned());
+    }
+
+    SECTION("double"){
+      gsw::json j(0.0);
+
+      CHECK(!j.is_object());
+      CHECK(!j.is_array());
+      CHECK(!j.is_string());
+      CHECK(!j.is_integer());
+      CHECK( j.is_floating());
+      CHECK(!j.is_bool());
+      CHECK( j.is_signed());
+      CHECK(!j.is_unsigned());
+    }
+  }
+
+  SECTION("'boolean' type") {
+    gsw::json j(true);
+
+    CHECK(!j.is_object());
+    CHECK(!j.is_array());
+    CHECK(!j.is_string());
+    CHECK(!j.is_integer());
+    CHECK(!j.is_floating());
+    CHECK( j.is_bool());
+    CHECK(!j.is_signed());
+    CHECK(!j.is_unsigned());
   }
 }
 
