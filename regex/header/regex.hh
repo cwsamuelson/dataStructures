678b7220b63ea8219c01293f1644f2e9bc79a787
Chris <chris.sam55@gmail.com>
2020-04-12 22:20:10 -0400

    simple result ctor
    otherwise emplace doesn't work

--- regex/header/regex.hh
+++ regex/header/regex.hh
@@ -3,14 +3,21 @@
 
 #include <vector>
 #include <string>
+#include <string_view>
 
 namespace gsw {
 
 struct regex_result{
   std::string_view view;
   size_t index;
+
+  explicit regex_result(std::string_view vw, size_t idx)
+    : view(vw)
+    , index(idx)
+  {}
 };
 
+//! @TODO lazy evaluation
 class regex {
 private:
   std::string mExpression;
