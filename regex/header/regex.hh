f38419e16ec5bb56475436667033579cda069219
Chris <chris.sam55@gmail.com>
2020-04-12 21:56:54 -0400

    initial regex outline

--- regex/header/regex.hh
+++ regex/header/regex.hh
@@ -1,10 +1,25 @@
 #ifndef GALACTICSTRUCTURES_REGEX_HH
 #define GALACTICSTRUCTURES_REGEX_HH
 
+#include <vector>
+#include <string>
+
 namespace gsw {
 
-class regex {
+struct regex_result{
+  std::string_view view;
+  size_t index;
+};
 
+class regex {
+private:
+  std::string mExpression;
+
+public:
+  explicit regex(std::string expression);
+
+  [[nodiscard]]
+  std::vector<regex_result> operator()(const std::string& input);
 };
 
 }
--- regex/source/regex.cc
+++ regex/source/regex.cc
@@ -2,4 +2,11 @@
 
 namespace gsw{
 
+regex::regex(std::string expression)
+  : mExpression(std::move(expression)){
+}
+
+std::vector<regex_result> regex::operator()(const std::string& input) {
+  std::vector<regex_result> results;
+  return results;
 }
