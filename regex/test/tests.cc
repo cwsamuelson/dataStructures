a826a2df2d69f147614f1f4db3ca291eb6c431bb
Chris <chris.sam55@gmail.com>
2020-04-12 21:57:09 -0400

    simple starting test cases

--- regex/test/tests.cc
+++ regex/test/tests.cc
@@ -7,5 +7,34 @@
 using namespace gsw;
 
 TEST_CASE("Simple string", "[]"){
+  regex re("abcd");
 
+  SECTION("Exact match") {
+    re("abcd");
+  }
+
+  SECTION("No match"){
+    SECTION("No match exists") {
+      re("htns");
+    }
+
+    SECTION("Partial matches"){
+      re("ahtns");
+      re("abchtns");
+    }
+  }
+
+  SECTION("Full matches"){
+    SECTION("Exact"){
+      re("abcd");
+    }
+
+    SECTION("partway through"){
+      re("aoeuabcdhtns");
+    }
+
+    SECTION("end"){
+      re("aoeuabcd");
+    }
+  }
 }
