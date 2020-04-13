706427e6a63075ea5ee544c9e342f0f5ef710249
Chris <chris.sam55@gmail.com>
2020-04-12 22:20:38 -0400

    completed match testing implementation

--- regex/source/regex.cc
+++ regex/source/regex.cc
@@ -10,12 +10,18 @@ std::vector<regex_result> regex::operator()(const std::string& input) {
   std::vector<regex_result> results;
 
   for(size_t input_index = 0; input_index < input.size(); ++input_index){
-    for(size_t expression_index = 0; expression_index < mExpression.size(); ++expression_index){
-      if(!matches(input[input_index], mExpression[expression_index])) {
-        continue;
+    bool found_match = false;
+    for(size_t expression_index = 0, check_index = input_index; expression_index < mExpression.size() && check_index < input.size(); ++expression_index, ++check_index){
+      if(!matches(input[check_index], mExpression[expression_index])) {
+        found_match = false;
+        break;
       } else {
-
+        found_match = true;
+        continue;
       }
+    }
+    if(found_match){
+      results.emplace_back(std::string_view(&input[input_index], mExpression.size()), input_index);
     }
   }
 
