ca9efc5dfd20ad14f5965ae827b5d30a2687e868
Chris <chris.sam55@gmail.com>
2020-10-13 20:56:28 -0400

    Create convenience signal class
    can sorta be treated as a function, but allows connection to pre call and post call events!

---
+++ event_handler/header/signal.hh
@@ -0,0 +1,68 @@
+#ifndef GALACTICSTRUCTURES_SIGNAL_HH
+#define GALACTICSTRUCTURES_SIGNAL_HH
+
+#include <event_handler.hh>
+
+#include <functional>
+
+namespace gsw{
+
+template<typename T>
+class signal;
+
+template<typename R, typename ...Args>
+class signal<R(Args...)>{
+public:
+  using before_trigger_t = gsw::event_trigger<Args...>;
+  using after_trigger_t = gsw::event_trigger<Args...>;
+  using trigger_t = after_trigger_t;
+
+  using before_channel_t = typename before_trigger_t::channel_t;
+  using after_channel_t = typename after_trigger_t::channel_t;
+  using channel_t = after_channel_t;
+
+  using before_handler_t = typename before_channel_t::handler;
+  using after_handler_t = typename after_channel_t::handler;
+  using handler_t = after_handler_t;
+
+  using action_t = std::function<R(Args...)>;
+
+private:
+  before_trigger_t mPreTrigger;
+  after_trigger_t mPostTrigger;
+  action_t mAction;
+
+public:
+  signal(action_t action)
+    : mAction(std::move(action))
+  {}
+
+  //! @TODO should this be forwarded?
+  // reflex says yes, but which gets forwarded?
+  // optimally the action would be forwarded, but that could invalidate vars for
+  // the post operation.
+  //! @TODO should R be required to be copyable?
+  // it's pretty much treated as guaranteed copyable here, but maybe there's
+  // another solution to forward its result after the post trigger is fired
+  auto operator()(Args... args){
+    mPreTrigger.fire(args...);
+
+    auto r = mAction(args...);
+
+    mPostTrigger.fire(args...);
+
+    return r;
+  }
+
+  auto& pre(){
+    return *mPreTrigger.getChannel().lock();
+  }
+
+  auto& post(){
+    return *mPostTrigger.getChannel().lock();
+  }
+};
+
+}
+
+#endif //GALACTICSTRUCTURES_SIGNAL_HH
