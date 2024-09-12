5f9f983cbd47607de4c8854b5c755c4d10b809a9
Chris Samuelson <chris.sam55@gmail.com>
2024-09-12 09:21:52 -0400

    flesh out interfaces, and thinking about what they should be

--- context/header/context.hh
+++ context/header/context.hh
@@ -61,6 +61,10 @@ struct LoggerTrait {
     implementation = std::make_unique<Implementation<Logger>>(std::forward<Args>(args)...);
   }
 
+  // similar to contracts, we could have logger categories that are asked whether they're active
+  // and then an actual logger handles dispatching the results, but not necessarily controlling whether it's sent, or it's content?
+  // perhaps all 3 of these (allocator, logger, contracts) should have similar semantics
+
   void log(const LogMessage& message) {
     implementation->log(message);
   }
@@ -70,6 +74,8 @@ struct LoggerTrait {
   }
 
   // asynchronous???
+  //  - due to forking, a context may be cleaned up while a given scope continues to hold a handle to it
+  //  - to prevent this, a shared_ptr could be used.  I'd rather use the heap less, but this  may be unavoidable
   // co_await log?
   // co_spawn log? to make it less-blocking
 
@@ -84,11 +90,46 @@ private:
 struct AllocatorTrait {
   struct Interface {
     virtual ~Interface() noexcept = default;
+
+    // cpp std::allocators have only allocate + deallocate
+    // I think having a construct/create + destruct/destroy allows things like using an aligned buffer to manage
+    // allocation cpp2 allocators use 'new', and returns custom pointers I'm unsure where between these the solution
+    // lies
+
+    template<typename Type>
+    Type* allocate(this auto&& self, const size_t object_count) {
+      return self.allocate_impl(object_count);
+    }
+
+    template<typename Type>
+    void deallocate(this auto&& self, const Type* pointer, const size_t object_count) {
+      return self.deallocate_impl(pointer, object_count);
+    }
+
+    template<typename Type>
+    void construct(this auto&& self, const Type* pointer, const size_t object_count) {
+      return self.construct_impl(pointer, object_count);
+    }
+
+    template<typename Type>
+    void destroy(this auto&& self, const Type* pointer, const size_t object_count) {
+      return self.destroy_impl(pointer, object_count);
+    }
   };
 
   template<typename Type>
   struct Implementation : Interface {
-    Type logger;
+    Type allocator;
+
+    template<typename AllocationType>
+    AllocationType* allocate_impl(const size_t object_count) {
+      return allocator.allocate(object_count);
+    }
+
+    template<typename AllocationType>
+    void deallocate_impl(const AllocationType* pointer, const size_t object_count) {
+      allocator.deallocate_impl(pointer, object_count);
+    }
   };
 
   AllocatorTrait()                          = default;
@@ -116,35 +157,64 @@ struct AllocatorTrait {
     implementation = std::make_unique<Implementation<Logger>>(std::forward<Args>(args)...);
   }
 
+  template<typename AllocationType>
+  AllocationType* allocate(const size_t object_count) {
+    return implementation->allocate<AllocationType>(object_count);
+  }
+
+  template<typename AllocationType>
+  void deallocate(const AllocationType* pointer, const size_t object_count) {
+    implementation->deallocate(pointer, object_count);
+  }
+
 private:
   std::unique_ptr<Interface> implementation;
 };
 
-struct ErrorCategoryTrait {
+struct ErrorContractTrait {
   struct Interface {
     virtual ~Interface() noexcept = default;
+
+    [[nodiscard]]
+    virtual bool is_active() const
+      = 0;
+    virtual void report_violation() const                 = 0;
+    virtual void report_violation(std::string_view) const = 0;
   };
 
   template<typename Type>
   struct Implementation : Interface {
-    Type logger;
+    Type contract;
+
+    [[nodiscard]]
+    bool is_active() const override {
+      return contract.is_active();
+    }
+
+    void report_violation() const override {
+      contract.report_violation();
+    }
+
+    void report_violation(std::string_view message) const override {
+      contract.report_violation(message);
+    }
   };
 
-  ErrorCategoryTrait()                              = default;
-  ErrorCategoryTrait(const ErrorCategoryTrait&)     = delete;
-  ErrorCategoryTrait(ErrorCategoryTrait&&) noexcept = default;
+  ErrorContractTrait()                              = default;
+  ErrorContractTrait(const ErrorContractTrait&)     = delete;
+  ErrorContractTrait(ErrorContractTrait&&) noexcept = default;
 
-  ErrorCategoryTrait& operator=(const ErrorCategoryTrait&)     = delete;
-  ErrorCategoryTrait& operator=(ErrorCategoryTrait&&) noexcept = default;
+  ErrorContractTrait& operator=(const ErrorContractTrait&)     = delete;
+  ErrorContractTrait& operator=(ErrorContractTrait&&) noexcept = default;
 
-  ~ErrorCategoryTrait() = default;
+  ~ErrorContractTrait() = default;
 
   template<typename Type>
-  explicit ErrorCategoryTrait(Type&& logger)
+  explicit ErrorContractTrait(Type&& logger)
     : implementation(std::make_unique<Implementation<Type>>(std::forward<Type>(logger))) {}
 
   template<typename Type>
-  ErrorCategoryTrait& operator=(Type&& logger) {
+  ErrorContractTrait& operator=(Type&& logger) {
     implementation = std::make_unique<Implementation<Type>>(std::forward<Type>(logger));
 
     return *this;
@@ -153,6 +223,19 @@ struct ErrorCategoryTrait {
   template<typename Logger, typename... Args>
   void emplace(Args&&... args) {
     implementation = std::make_unique<Implementation<Logger>>(std::forward<Args>(args)...);
+  }
+
+  [[nodiscard]]
+  bool is_active() const {
+    return implementation->is_active();
+  }
+
+  void report_violation() const {
+    implementation->report_violation();
+  }
+
+  void report_violation(std::string_view message) const {
+    implementation->report_violation(message);
   }
 
 private:
@@ -165,7 +248,7 @@ private:
 struct Context {
   LoggerTrait        logger;
   AllocatorTrait     allocator;
-  ErrorCategoryTrait default_error_category;
+  ErrorContractTrait error_contract;
 };
 
 struct ScopedContext {
