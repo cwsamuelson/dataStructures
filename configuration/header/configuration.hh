338ddb480a21fdafe590591bd0c772ada9a7c3c4
Chris Samuelson <chris.sam55@gmail.com>
2024-10-05 09:42:23 -0400

    Add a starting point for configuration parsing from multiple sources

---
+++ configuration/header/configuration.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+// In addition to my own ideas for how configuration should be handled, this resource tries to do some similar things:
+// https://github.com/doom/nectarine
+// ultimately those ideas are very similar to mine, but they have a decent API, imo.
+
+namespace flp {
+
+} // namespace flp
---
+++ configuration/test/tests.cc
@@ -0,0 +1,9 @@
+#include <configuration.hh>
+
+#include <catch2/catch_all.hpp>
+
+using namespace flp;
+
+// this struct should maybe be put into a shared location?
+TEST_CASE("configuration") {
+}
