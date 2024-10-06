daa4e3660551487c1ae85fde8d06786f83971d60
Chris Samuelson <chris.sam55@gmail.com>
2024-10-05 22:18:58 -0400

    sparse_set implementation
    turns out it can't do too much, and isn't very complex

--- sparse_set/header/sparse_set.hh
+++ sparse_set/header/sparse_set.hh
@@ -1,15 +1,78 @@
 #pragma once
 
+#include <concepts>
+#include <vector>
+
+// https://programmingpraxis.com/2012/03/09/sparse-sets/
+// https://skypjack.github.io/2019-03-07-ecs-baf-part-2/
 
 namespace flp {
 
-template<typename Key, typename Value>
+template<std::unsigned_integral Type>
 struct SparseSet {
-  // sparse set requires 2 data structures
-  // real storage
-  // something to map ID to effective index in real storage
+  [[nodiscard]]
+  bool contains(const Type value) const noexcept {
+    return value < sparse_data.size() and sparse_data.at(value) < element_count
+       and dense_data.at(sparse_data.at(value)) == value;
+  }
 
-  // it possibly requires a way of tracking free list?
+  void insert(const Type value) {
+    ensure_size(value + 1);
+    dense_data.at(element_count) = value;
+    sparse_data.at(value)        = element_count;
+    ++element_count;
+  }
+
+  void erase(const Type value) {
+    // this could be optimized, I'm sure
+    if (contains(value)) {
+      // incrementing the value is cheap, and ensures they no longer match
+      ++dense_data.at(sparse_data.at(value));
+      --element_count;
+    }
+  }
+
+  void clear() noexcept {
+    element_count = 0;
+  }
+
+  [[nodiscard]]
+  size_t size() const noexcept {
+    return element_count;
+  }
+
+  [[nodiscard]]
+  bool empty() const noexcept {
+    return size() == 0;
+  }
+
+  void ensure_size(const size_t min_size) {
+    if (element_count < min_size) {
+      sparse_data.resize(min_size, 0);
+      dense_data.resize(min_size, 0);
+    }
+  }
+
+  auto begin() {
+    return dense_data.begin();
+  }
+
+  auto begin() const {
+    return dense_data.begin();
+  }
+
+  auto end() {
+    return dense_data.begin() + element_count;
+  }
+
+  auto end() const {
+    return dense_data.begin() + element_count;
+  }
+
+  std::vector<Type> sparse_data;
+  std::vector<Type> dense_data;
+
+  size_t element_count {};
 };
 
 } // namespace flp
--- sparse_set/test/tests.cc
+++ sparse_set/test/tests.cc
@@ -6,4 +6,46 @@ using namespace flp;
 
 // this struct should maybe be put into a shared location?
 TEST_CASE("sparse_set") {
+  SparseSet<size_t> sparse_set;
+
+  CHECK(sparse_set.size() == 0);
+  CHECK(sparse_set.empty());
+  CHECK(sparse_set.begin() == sparse_set.end());
+
+  sparse_set.clear();
+  CHECK(sparse_set.size() == 0);
+  CHECK(sparse_set.empty());
+  CHECK(sparse_set.begin() == sparse_set.end());
+
+  sparse_set.insert(1);
+  CHECK(sparse_set.size() == 1);
+  CHECK(not sparse_set.empty());
+  CHECK(sparse_set.begin() != sparse_set.end());
+  CHECK(sparse_set.contains(1));
+
+  CHECK(*sparse_set.begin() == 1);
+  CHECK(sparse_set.begin() != sparse_set.end());
+
+  sparse_set.clear();
+  CHECK(sparse_set.size() == 0);
+  CHECK(sparse_set.empty());
+  CHECK(sparse_set.begin() == sparse_set.end());
+
+  sparse_set.insert(1);
+  sparse_set.insert(2);
+
+  CHECK(sparse_set.size() == 2);
+  CHECK(not sparse_set.empty());
+  CHECK(sparse_set.begin() != sparse_set.end());
+  CHECK(sparse_set.contains(1));
+  CHECK(sparse_set.contains(2));
+
+  CHECK(*sparse_set.begin() == 1);
+  CHECK(*(++sparse_set.begin()) == 2);
+  CHECK(sparse_set.begin() != sparse_set.end());
+
+  sparse_set.clear();
+  CHECK(sparse_set.size() == 0);
+  CHECK(sparse_set.empty());
+  CHECK(sparse_set.begin() == sparse_set.end());
 }
