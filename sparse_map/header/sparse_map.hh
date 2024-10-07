bd6d14343b1f8dd052926b7bb79d1da623833d6b
Chris Samuelson <chris.sam55@gmail.com>
2024-10-06 21:08:42 -0400

    base a sparse map on the sparse set
    there's going to be a lot of common code here.  once I have a better idea what that is (more exactly), then I may create a shared base class or something

---
+++ sparse_map/header/sparse_map.hh
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <concepts>
+#include <vector>
+
+// https://programmingpraxis.com/2012/03/09/sparse-sets/
+// https://skypjack.github.io/2019-03-07-ecs-baf-part-2/
+
+namespace flp {
+
+template<std::unsigned_integral Key, typename Value = void>
+struct SparseMap {
+  [[nodiscard]]
+  bool contains(const Key value) const noexcept {
+    return value < sparse_data.size() and sparse_data.at(value) < element_count
+       and dense_data.at(sparse_data.at(value)) == value;
+  }
+
+  void insert(const Key value) {
+    ensure_size(value + 1);
+    dense_data.at(element_count) = value;
+    sparse_data.at(value)        = element_count;
+    ++element_count;
+  }
+
+  void erase(const Key value) {
+    // this could be optimized, I'm sure
+    if (contains(value)) {
+      // swapping this value with the last valid element ensures this one is invalidated, and maintains the dense
+      // invariant
+      const auto index = sparse_data.at(value);
+      std::swap(dense_data.at(index), dense_data.at(element_count - 1));
+      // then the sparse data indexes must be updated
+      sparse_data.at(std::get<0>(dense_data.at(index))) = index;
+
+      --element_count;
+    }
+  }
+
+  void clear() noexcept {
+    element_count = 0;
+  }
+
+  [[nodiscard]]
+  size_t size() const noexcept {
+    return element_count;
+  }
+
+  [[nodiscard]]
+  bool empty() const noexcept {
+    return size() == 0;
+  }
+
+  void ensure_size(const size_t min_size) {
+    if (element_count < min_size) {
+      sparse_data.resize(min_size, 0);
+      dense_data.resize(min_size, 0);
+    }
+  }
+
+  auto begin() {
+    return dense_data.begin();
+  }
+
+  auto begin() const {
+    return dense_data.begin();
+  }
+
+  auto end() {
+    return dense_data.begin() + element_count;
+  }
+
+  auto end() const {
+    return dense_data.begin() + element_count;
+  }
+
+  std::vector<Key>       sparse_data;
+  std::vector<Value> dense_data;
+
+  size_t element_count {};
+};
+
+} // namespace flp
---
+++ sparse_map/test/tests.cc
@@ -0,0 +1,9 @@
+#include <sparse_map.hh>
+
+#include <catch2/catch_all.hpp>
+
+using namespace flp;
+
+// this struct should maybe be put into a shared location?
+TEST_CASE("sparse map") {
+}
