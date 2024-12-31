75b59e6c78d475d4625b0f2c9ebaab40d3fe4bbb
Chris Samuelson <chris.sam55@gmail.com>
2024-12-31 13:56:28 -0500

    Improved  tests, and bug fix

--- try_wrap/header/try_wrap.hh
+++ try_wrap/header/try_wrap.hh
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <iostream>
+#include <exception>
+#include <functional>
 #include <optional>
+#include <tuple>
 #include <type_traits>
 
 namespace flp {
@@ -11,70 +13,72 @@ using Ref = std::conditional_t<std::is_reference_v<Type>, std::reference_wrapper
 
 template<typename Type>
 decltype(auto) Forward(Type& argument) {
-    return std::ref(argument);
+  return std::ref(argument);
 }
 
 template<typename Type>
 decltype(auto) Forward(Type&& argument) {
-    return std::move(argument);
+  return std::move(argument);
 }
 
 template<typename Functor>
 struct TryWrap {
-    Functor functor;
+  Functor functor;
 
-    template<typename F>
-    TryWrap(F&& func) noexcept
-        : functor(std::forward<F>(func))
-    {}
+  template<typename F>
+  TryWrap(F&& func) noexcept
+    : functor(std::forward<F>(func)) {}
 
-    TryWrap(Functor func) noexcept
-        : functor(std::move(func))
-    {}
+  TryWrap(Functor func) noexcept
+    : functor(std::move(func)) {}
 
-    // auto functor(Args...) noexcept
-    template<typename ...Args>
-        requires (std::is_nothrow_invocable_v<Functor, Args...>
-        and std::is_void_v<std::invoke_result_t<Functor, Args...>>)
-    void operator()(Args&& ...args) && noexcept {
-        std::move(functor)(std::forward<Args>(args)...);
+  // no return, no throws
+  // void functor(Args...) noexcept
+  template<typename... Args>
+    requires(std::is_nothrow_invocable_v<Functor, Args...> and std::is_void_v<std::invoke_result_t<Functor, Args...>>)
+  void operator()(Args&&... args) && noexcept {
+    std::invoke(std::move(functor), std::forward<Args>(args)...);
+  }
+
+  // no return, but throws
+  // void functor(Args...)
+  template<typename... Args>
+    requires((not std::is_nothrow_invocable_v<Functor, Args...>)
+             and std::is_void_v<std::invoke_result_t<Functor, Args...>>)
+  [[nodiscard]]
+  std::exception_ptr operator()(Args&&... args) && noexcept {
+    try {
+      std::invoke(std::move(functor), std::forward<Args>(args)...);
+      return nullptr;
+    } catch (...) {
+      return std::current_exception();
     }
+  }
 
-    // auto functor(Args...)
-    template<typename ...Args>
-        requires ((not std::is_nothrow_invocable_v<Functor, Args...>)
-        and std::is_void_v<std::invoke_result_t<Functor, Args...>>)
-        [[nodiscard]]
-    const std::exception_ptr operator()(Args&& ...args) && noexcept {
-        try {
-            std::move(functor)(std::forward<Args>(args)...);
-            return nullptr;
-        } catch(...) {
-            return std::current_exception();
-        }
-    }
+  // returns value, no throws
+  // auto functor(Args...) noexcept
+  template<typename... Args>
+    requires(std::is_nothrow_invocable_v<Functor, Args...>
+             and not std::is_void_v<std::invoke_result_t<Functor, Args...>>)
+  [[nodiscard]]
+  decltype(auto) operator()(Args&&... args) && noexcept {
+    return std::invoke(std::move(functor), std::forward<Args>(args)...);
+  }
 
-    // auto functor(Args...) noexcept
-    template<typename ...Args>
-        requires (std::is_nothrow_invocable_v<Functor, Args...>
-        and not std::is_void_v<std::invoke_result_t<Functor, Args...>>)
-        [[nodiscard]]
-    decltype(auto) operator()(Args&& ...args) && noexcept {
-        return std::move(functor)(std::forward<Args>(args)...);
+  // returns value, and can throw
+  // auto functor(Args...)
+  template<typename... Args>
+    requires((not std::is_nothrow_invocable_v<Functor, Args...>)
+             and not std::is_void_v<std::invoke_result_t<Functor, Args...>>)
+  [[nodiscard]]
+  std::tuple<std::optional<Ref<std::invoke_result_t<Functor, Args...>>>, const std::exception_ptr>
+  operator()(Args&&... args) && noexcept {
+    try {
+      return std::make_tuple(Forward(std::invoke(std::move(functor), std::forward<Args>(args)...)), nullptr);
+    } catch (...) {
+      return std::make_tuple(std::nullopt, std::current_exception());
     }
-
-    // auto functor(Args...)
-    template<typename ...Args>
-        requires ((not std::is_nothrow_invocable_v<Functor, Args...>)
-        and not std::is_void_v<std::invoke_result_t<Functor, Args...>>)
-        [[nodiscard]]
-    std::tuple<std::optional<Ref<std::invoke_result_t<Functor, Args...>>>, const std::exception_ptr> operator()(Args&& ...args) && noexcept {
-        try {
-            return std::make_tuple(Forward(std::move(functor)(std::forward<Args>(args)...)), nullptr);
-        } catch(...) {
-            return std::make_tuple(std::nullopt, std::current_exception());
-        }
-    }
+  }
 };
 
-}
+} // namespace flp
--- try_wrap/test/tests.cc
+++ try_wrap/test/tests.cc
@@ -2,41 +2,170 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <utility>
+
 using namespace flp;
 
-void nodata_nothrow() noexcept {
+void nodata_nothrow() noexcept {}
+
+void nodata_could_throw() {}
+
+void nodata_throws() {
+  throw std::runtime_error("nodata");
 }
 
-void nodata() {
+int withdata_nothrow() noexcept {
+  return 42;
 }
 
-int nothrow() noexcept {
-    return 42;
+int withdata_could_throw() {
+  return 1138;
 }
 
-int data() {
-    return 1138;
+int withdata_throws() {
+  throw std::runtime_error("with_data");
 }
 
-int& get_static() {
-    static int i = 42;
-    return i;
+int& static_data_could_throw() {
+  static int i = 42;
+  if (42 % 69 == 0) {
+    throw std::runtime_error("static_data");
+  }
+  return i;
 }
+
+template<bool Throwing, typename Return, typename... Args>
+struct Functor {
+  Return operator()(Args&&... args) noexcept(not Throwing) {
+    if constexpr (Throwing) {
+      throw std::runtime_error("Throwing Functor");
+    }
+    return {};
+  }
+
+  // noexcept etc variations?
+  static Return static_member() noexcept(not Throwing) {
+    if constexpr (Throwing) {
+      throw std::runtime_error("Throwing Static Functor");
+    }
+    return {};
+  }
+
+  Return member_function() noexcept(not Throwing) {
+    if constexpr (Throwing) {
+      throw std::runtime_error("Throwing Static Functor");
+    }
+    return {};
+  }
+};
 
 TEST_CASE("try_wrap") {
-    /*auto x = */TryWrap{nodata_nothrow}();
-    auto y = TryWrap{nothrow}();
-    auto z = TryWrap{nodata}();
-    auto [v, w] = TryWrap{data}();
-    auto&& [a, b] = TryWrap{get_static}();
+  SECTION("No exceptions thrown") {
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { nodata_nothrow })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { nodata_could_throw })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { nodata_throws })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { withdata_nothrow })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { withdata_could_throw })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { withdata_throws })>);
+    STATIC_CHECK(std::is_nothrow_invocable_v<decltype(TryWrap { static_data_could_throw })>);
 
-    std::cout << *a << '\n';
-    ++*a;
-    std::cout << *a << '\n';
+    CHECK_NOTHROW(TryWrap { nodata_nothrow }());
+    CHECK_NOTHROW(TryWrap { nodata_could_throw }());
+    CHECK_NOTHROW(TryWrap { nodata_throws }());
+    CHECK_NOTHROW(TryWrap { withdata_nothrow }());
+    CHECK_NOTHROW(TryWrap { withdata_could_throw }());
+    CHECK_NOTHROW(TryWrap { withdata_throws }());
+    CHECK_NOTHROW(TryWrap { static_data_could_throw }());
+  }
 
-    auto&& [c, d] = TryWrap{get_static}();
-    std::cout << *c << '\n';
-    ++*c;
-    std::cout << *c << '\n';
-    std::cout << *a << '\n';
+  SECTION("References forwarded") {
+    SECTION("LValue return value") {
+      STATIC_CHECK(not std::is_nothrow_invocable_v<decltype(static_data_could_throw)>);
+      STATIC_CHECK(not std::is_void_v<std::invoke_result_t<decltype(static_data_could_throw)>>);
+
+      {
+        auto [value, error] = TryWrap { static_data_could_throw }();
+        CHECK(error == nullptr);
+        REQUIRE(value.has_value());
+        CHECK(*value == 42);
+        ++*value;
+      }
+
+      {
+        auto&& [value, error] = TryWrap { static_data_could_throw }();
+        CHECK(error == nullptr);
+        REQUIRE(value.has_value());
+        CHECK(*value == 43);
+      }
+    }
+
+    SECTION("LValue ref argument") {
+      auto f = [](int& x) {
+        x = 42;
+      };
+      int x = 0;
+      TryWrap { f }(x);
+      CHECK(x == 42);
+    }
+
+    SECTION("RValue ref argument") {
+      const auto f = [](int&& x) {
+        x = 42;
+      };
+      int x = 0;
+      TryWrap { f }(std::move(x));
+      CHECK(x == 42);
+    }
+  }
+
+  SECTION("Handling thrown exceptions") {
+    {
+      auto&& [value, error] = TryWrap { withdata_could_throw }();
+      CHECK(error == nullptr);
+      REQUIRE(value.has_value());
+      CHECK(*value == 1138);
+    }
+
+    {
+      auto&& [value, error] = TryWrap { withdata_throws }();
+      CHECK(error != nullptr);
+      CHECK(not value.has_value());
+    }
+  }
+
+  CHECK(TryWrap { withdata_nothrow }() == 42);
+
+  SECTION("Functor") {
+    SECTION("no throw") {
+      Functor<false, int> functor;
+      CHECK_NOTHROW(TryWrap { functor }());
+    }
+
+    SECTION("throws") {
+      Functor<true, int> functor;
+      CHECK_NOTHROW(TryWrap { functor }());
+    }
+
+    SECTION("member function") {
+      SECTION("no throw") {
+        Functor<false, int> functor;
+        CHECK_NOTHROW(TryWrap { &Functor<false, int>::member_function }(functor));
+      }
+
+      SECTION("throws") {
+        Functor<true, int> functor;
+        CHECK_NOTHROW(TryWrap { &Functor<true, int>::member_function }(functor));
+      }
+    }
+
+    SECTION("static member") {
+      SECTION("no throw") {
+        CHECK_NOTHROW(TryWrap { Functor<false, int>::static_member }());
+      }
+
+      SECTION("throws") {
+        CHECK_NOTHROW(TryWrap { Functor<true, int>::static_member }());
+      }
+    }
+  }
 }
