942d02b14388fd95bf07cfa3578f6ac40b473a64
Chris Samuelson <chris.sam55@gmail.com>
2024-12-31 22:02:59 -0500

    Adding some more type traits
    Hoping these will make implementing units easier

--- core/header/core/traits.hh
+++ core/header/core/traits.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 
 namespace flp {
@@ -23,6 +24,7 @@ template<typename Type>
 using AddConst = const Type;
 
 // --- volatile
+
 template<typename Type>
 struct RemoveVolatileImpl {
   using type = Type;
@@ -40,6 +42,7 @@ template<typename Type>
 using AddVolatile = volatile Type;
 
 // --- CV
+
 template<typename Type>
 using RemoveCV = RemoveConst<RemoveVolatile<Type>>;
 
@@ -47,6 +50,7 @@ template<typename Type>
 using AddCV = AddConst<AddVolatile<Type>>;
 
 // --- reference
+
 template<typename Type>
 struct RemoveReferenceImpl {
   using type = Type;
@@ -72,6 +76,7 @@ template<typename Type>
 using AddRValueReference = Type&&;
 
 // --- pointer
+
 template<typename Type>
 struct RemovePointerImpl {
   using type = Type;
@@ -103,5 +108,106 @@ using RemovePointer = RemovePointerImpl<Type>::type;
 template<typename Type>
 using AddPointer = Type*;
 
-} // namespace flp
+// --- Integral Constants
 
+template<typename Type>
+concept Integral = std::integral<Type>;
+
+template<Integral Type, Type Value>
+struct IntegralConstant {
+  static constexpr Type value = Value;
+
+  [[nodiscard]]
+  constexpr operator Type() const noexcept {
+    return value;
+  }
+};
+
+template<bool Value>
+using BoolConstant = IntegralConstant<bool, Value>;
+using TrueType     = BoolConstant<true>;
+using FalseType    = BoolConstant<false>;
+
+// --- predicates
+
+template<auto Value>
+struct Identity {
+  static constexpr auto value = Value;
+
+  [[nodiscard]]
+  constexpr operator decltype(Value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct Addition {
+  static constexpr auto value = LHS + RHS;
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct Subtraction {
+  static constexpr auto value = LHS - RHS;
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct Multiplication {
+  static constexpr auto value = LHS * RHS;
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct Division {
+  static constexpr auto value = LHS / RHS;
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct Equality {
+  static constexpr BoolConstant<LHS == RHS> value {};
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct LessThan {
+  static constexpr BoolConstant < LHS<RHS> value {};
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+template<auto LHS, auto RHS>
+struct GreaterThan {
+  static constexpr BoolConstant<(LHS > RHS)> value {};
+
+  [[nodiscard]]
+  constexpr operator decltype(value)() const noexcept {
+    return value;
+  }
+};
+
+} // namespace flp
--- core/test/traits.cc
+++ core/test/traits.cc
@@ -1,112 +1,160 @@
-#include <catch2/catch_all.hpp>
-
 #include <core/traits.hh>
+
+#include <catch2/catch_all.hpp>
 
 // trying to be pretty comprehensive..
 TEST_CASE("Add/remove const") {
-  STATIC_CHECK(std::same_as<flp::AddConst<int>,       const int>);
+  STATIC_CHECK(std::same_as<flp::AddConst<int>, const int>);
   STATIC_CHECK(std::same_as<flp::AddConst<const int>, const int>);
 
-  STATIC_CHECK(std::same_as<flp::AddConst<volatile       int>, const volatile int>);
+  STATIC_CHECK(std::same_as<flp::AddConst<volatile int>, const volatile int>);
   STATIC_CHECK(std::same_as<flp::AddConst<volatile const int>, const volatile int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<int>,       int>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<int>, int>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<const int>, int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile       int>, volatile int>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile int>, volatile int>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<volatile const int>, volatile int>);
 
-  STATIC_CHECK(std::same_as<flp::AddConst<int*>,             int* const>);
+  STATIC_CHECK(std::same_as<flp::AddConst<int*>, int* const>);
   STATIC_CHECK(std::same_as<flp::AddConst<const int*>, const int* const>);
 
-  STATIC_CHECK(std::same_as<flp::AddConst<volatile       int*>, volatile       int* const>);
+  STATIC_CHECK(std::same_as<flp::AddConst<volatile int*>, volatile int* const>);
   STATIC_CHECK(std::same_as<flp::AddConst<volatile const int*>, volatile const int* const>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<int*>,             int*>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<int*>, int*>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<const int*>, const int*>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile       int*>, volatile int*>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile int*>, volatile int*>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<volatile const int*>, volatile const int*>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<int* const>,             int*>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<int* const>, int*>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<const int* const>, const int*>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile       int* const>, volatile int*>);
+  STATIC_CHECK(std::same_as<flp::RemoveConst<volatile int* const>, volatile int*>);
   STATIC_CHECK(std::same_as<flp::RemoveConst<volatile const int* const>, volatile const int*>);
 }
 
 TEST_CASE("Add/remove volatile") {
-  STATIC_CHECK(std::same_as<flp::AddVolatile<int>,          volatile int>);
+  STATIC_CHECK(std::same_as<flp::AddVolatile<int>, volatile int>);
   STATIC_CHECK(std::same_as<flp::AddVolatile<volatile int>, volatile int>);
 
-  STATIC_CHECK(std::same_as<flp::AddVolatile<const          int>, volatile const int>);
+  STATIC_CHECK(std::same_as<flp::AddVolatile<const int>, volatile const int>);
   STATIC_CHECK(std::same_as<flp::AddVolatile<const volatile int>, volatile const int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveVolatile<int>,          int>);
+  STATIC_CHECK(std::same_as<flp::RemoveVolatile<int>, int>);
   STATIC_CHECK(std::same_as<flp::RemoveVolatile<volatile int>, int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveVolatile<const int>,          const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveVolatile<const int>, const int>);
   STATIC_CHECK(std::same_as<flp::RemoveVolatile<const volatile int>, const int>);
 }
 
 TEST_CASE("Add/remove CV") {
-  STATIC_CHECK(std::same_as<flp::AddCV<int>,          const volatile int>);
+  STATIC_CHECK(std::same_as<flp::AddCV<int>, const volatile int>);
   STATIC_CHECK(std::same_as<flp::AddCV<volatile int>, const volatile int>);
 
-  STATIC_CHECK(std::same_as<flp::AddCV<const          int>, volatile const int>);
+  STATIC_CHECK(std::same_as<flp::AddCV<const int>, volatile const int>);
   STATIC_CHECK(std::same_as<flp::AddCV<const volatile int>, volatile const int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveCV<int>,          int>);
+  STATIC_CHECK(std::same_as<flp::RemoveCV<int>, int>);
   STATIC_CHECK(std::same_as<flp::RemoveCV<volatile int>, int>);
 
-  STATIC_CHECK(std::same_as<flp::RemoveCV<const int>,          int>);
+  STATIC_CHECK(std::same_as<flp::RemoveCV<const int>, int>);
   STATIC_CHECK(std::same_as<flp::RemoveCV<const volatile int>, int>);
 }
 
 TEST_CASE("Add/remove reference") {
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         int>,                        int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         int&>,                       int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         int&&>,                      int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         const int>,            const int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         const int&>,           const int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<         const int&&>,          const int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int>,         volatile       int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int&>,        volatile       int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int&&>,       volatile       int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile const int>,   volatile const int>);
-  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile const int&>,  volatile const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<int>, int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<int&>, int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<int&&>, int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<const int>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<const int&>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<const int&&>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int>, volatile int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int&>, volatile int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile int&&>, volatile int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile const int>, volatile const int>);
+  STATIC_CHECK(std::same_as<flp::RemoveReference<volatile const int&>, volatile const int>);
   STATIC_CHECK(std::same_as<flp::RemoveReference<volatile const int&&>, volatile const int>);
 
-  STATIC_CHECK(std::same_as<flp::AddLValueReference<               int>,                int&>);
-  STATIC_CHECK(std::same_as<flp::AddLValueReference<const          int>, const          int&>);
+  STATIC_CHECK(std::same_as<flp::AddLValueReference<int>, int&>);
+  STATIC_CHECK(std::same_as<flp::AddLValueReference<const int>, const int&>);
   STATIC_CHECK(std::same_as<flp::AddLValueReference<const volatile int>, const volatile int&>);
-  STATIC_CHECK(std::same_as<flp::AddRValueReference<               int>,                int&&>);
-  STATIC_CHECK(std::same_as<flp::AddRValueReference<const          int>, const          int&&>);
+  STATIC_CHECK(std::same_as<flp::AddRValueReference<int>, int&&>);
+  STATIC_CHECK(std::same_as<flp::AddRValueReference<const int>, const int&&>);
   STATIC_CHECK(std::same_as<flp::AddRValueReference<const volatile int>, const volatile int&&>);
 }
 
 TEST_CASE("Add/remove pointer") {
-  STATIC_CHECK(std::same_as<flp::RemovePointer<int>,   int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<int*>,  int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<int>, int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<int*>, int>);
   STATIC_CHECK(std::same_as<flp::RemovePointer<int**>, int*>);
 
-  STATIC_CHECK(std::same_as<flp::AddPointer<int>,   int*>);
-  STATIC_CHECK(std::same_as<flp::AddPointer<int*>,  int**>);
+  STATIC_CHECK(std::same_as<flp::AddPointer<int>, int*>);
+  STATIC_CHECK(std::same_as<flp::AddPointer<int*>, int**>);
   STATIC_CHECK(std::same_as<flp::AddPointer<int**>, int***>);
 
-  STATIC_CHECK(std::same_as<flp::RemovePointer<const int>,   const int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<const int*>,  const int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<const int>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<const int*>, const int>);
   STATIC_CHECK(std::same_as<flp::RemovePointer<const int**>, const int*>);
 
-  STATIC_CHECK(std::same_as<flp::AddPointer<const int>,   const int*>);
-  STATIC_CHECK(std::same_as<flp::AddPointer<const int*>,  const int**>);
+  STATIC_CHECK(std::same_as<flp::AddPointer<const int>, const int*>);
+  STATIC_CHECK(std::same_as<flp::AddPointer<const int*>, const int**>);
   STATIC_CHECK(std::same_as<flp::AddPointer<const int**>, const int***>);
 
-  STATIC_CHECK(std::same_as<flp::RemovePointer<      int* const>,           int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* const>,     const int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<      int* volatile>,        int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* volatile>,  const int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<      int* const volatile>,        int>);
-  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* const volatile>,  const int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<int* const>, int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* const>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<int* volatile>, int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* volatile>, const int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<int* const volatile>, int>);
+  STATIC_CHECK(std::same_as<flp::RemovePointer<const int* const volatile>, const int>);
 }
 
+TEST_CASE("Integral Constants") {
+  STATIC_CHECK(flp::IntegralConstant<size_t, 42> {} == 42);
+  STATIC_CHECK(flp::IntegralConstant<size_t, 1138> {} == 1138);
+  STATIC_CHECK(flp::IntegralConstant<size_t, 42>::value == 42);
+  STATIC_CHECK(flp::IntegralConstant<size_t, 1138>::value == 1138);
+
+  STATIC_CHECK(flp::BoolConstant<true> {});
+  STATIC_CHECK(not flp::BoolConstant<false> {});
+  STATIC_CHECK(flp::BoolConstant<true>::value);
+  STATIC_CHECK(not flp::BoolConstant<false>::value);
+
+  STATIC_CHECK(flp::TrueType {});
+  STATIC_CHECK(not flp::FalseType {});
+  STATIC_CHECK(flp::TrueType::value);
+  STATIC_CHECK(not flp::FalseType::value);
+}
+
+TEST_CASE("Predicates") {
+  SECTION("Identity") {
+    STATIC_CHECK(flp::Identity<true> {});
+    STATIC_CHECK(flp::Identity<true>::value);
+
+    STATIC_CHECK(flp::Identity<42> {} == 42);
+    STATIC_CHECK(flp::Identity<42>::value == 42);
+    STATIC_CHECK(flp::Identity<4.2> {} == 4.2);
+    STATIC_CHECK(flp::Identity<4.2>::value == 4.2);
+  }
+
+  SECTION("Addition") {
+    STATIC_CHECK(flp::Addition<4, 2> {} == 6);
+    STATIC_CHECK(flp::Addition<4, 2>::value == 6);
+  }
+
+  SECTION("Subtraction") {
+    STATIC_CHECK(flp::Subtraction<4, 2> {} == 2);
+    STATIC_CHECK(flp::Subtraction<4, 2>::value == 2);
+  }
+
+  SECTION("Multiplication") {
+    STATIC_CHECK(flp::Multiplication<4, 2> {} == 8);
+    STATIC_CHECK(flp::Multiplication<4, 2>::value == 8);
+  }
+
+  SECTION("Division") {
+    STATIC_CHECK(flp::Division<4, 2> {} == 2);
+    STATIC_CHECK(flp::Division<4, 2>::value == 2);
+  }
+}
