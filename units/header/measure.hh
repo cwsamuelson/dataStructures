5981f974a46327e2e91e545e79e7e13b3d1010bf
Chris Samuelson <chris.sam55@gmail.com>
2025-01-01 21:05:22 -0500

    Implementing and testing measures

--- units/header/measure.hh
+++ units/header/measure.hh
@@ -14,47 +14,35 @@ struct MeasureBase {
   template<typename Self, typename Other>
     requires std::same_as<Self, Other>
   [[nodiscard]]
-  constexpr Self operator+(this const Self& self, const Other& other) const {
+  constexpr Self operator+(this const Self& self, const Other& other) {
     return self.order + other.order;
   }
 
   template<typename Self, typename Other>
     requires std::same_as<Self, Other>
   [[nodiscard]]
-  constexpr Self operator-(this const Self& self, const Other& other) const {
+  constexpr Self operator-(this const Self& self, const Other& other) {
     return self.order - other.order;
   }
 };
 
-struct LengthBase : MeasureBase {};
-struct TimeBase : MeasureBase {};
-struct MassBase : MeasureBase {};
-struct CurrentBase : MeasureBase {};
-struct TemperatureBase : MeasureBase {};
-struct CandelaBase : MeasureBase {};
-struct MoneyBase : MeasureBase {};
-struct AngleBase : MeasureBase {};
-struct PercentageBase : MeasureBase {};
-struct CountBase : MeasureBase {};
-struct ByteBase : MeasureBase {};
-struct TickBase : MeasureBase {};
-
+// must derive from, but not be `MeasureBase`
 template<auto... Measures>
   requires(std::derived_from<decltype(Measures), MeasureBase> && ...)
       and ((not std::same_as<decltype(Measures), MeasureBase>) && ...)
 struct Measure {
-  static constexpr typename Rebind<TypeSet<decltype(Measures)...>, std::tuple>::type measures;
+  static constexpr typename Rebind<TypeSet<decltype(Measures)...>, std::tuple>::type measures {};
 
   template<auto... OtherMeasures>
   constexpr auto operator*(const Measure<OtherMeasures...>& other) {
-    auto f = []<typename Measure, typename Tuple>(const Measure& measure, const Tuple& tuple) {
+    constexpr auto f = []<typename Measure, typename Tuple>(const Measure& measure, const Tuple& tuple) {
       if constexpr (typename Rebind<Tuple, TypeSet>::type::template contains<Measure>()) {
         return measure + std::get<Measure>(tuple);
       } else {
-        return  measure;
+        return measure;
       }
     };
-    (f(std::get<Measures>(measures), other), ...);
+    return Measure<f(std::get<Measures>(measures), other)...> {};
   }
 };
 
--- units/test/tests.cc
+++ units/test/tests.cc
@@ -1,6 +1,10 @@
+#include <measure.hh>
+#include <standard_measures.hh>
 #include <units.hh>
 
 #include <catch2/catch_all.hpp>
+
+#include <concepts>
 
 using namespace flp;
 
@@ -11,7 +15,14 @@ TEST_CASE("Measures") {
     Measure<> measure1;
     Measure<> measure2;
 
-    auto measure3 = measure1 * measure2;
-    STATIC_CHECK(std::same_as<decltype(measure3), decltype(measure1)>);
+    STATIC_CHECK(std::same_as<decltype(measure1 * measure2), decltype(measure1)>);
+  }
+
+  SECTION("Single measures") {
+    Measure<LengthBase { 1 }> measure1;
+    Measure<>                 measure2;
+
+    // STATIC_CHECK(std::same_as<decltype(measure1 * measure2), decltype(measure1)>);
+    //  STATIC_CHECK(std::same_as < decltype(measure1 * measure1), Measure<LengthBase { 2 }>);
   }
 }
