b7066b7fe27680f67abb46205913d77df663da00
Chris Samuelson <chris.sam55@gmail.com>
2025-01-24 14:36:02 -0500

    couple more project ideas..

---
+++ ring_buffer/test/tests.cc
@@ -0,0 +1,9 @@
+#include <ring_buffer.hh>
+
+#include <catch2/catch_all.hpp>
+
+using namespace flp;
+
+TEST_CASE("ring_buffer") {
+}
+
---
+++ ring_view/test/tests.cc
@@ -0,0 +1,9 @@
+#include <ring_view.hh>
+
+#include <catch2/catch_all.hpp>
+
+using namespace flp;
+
+TEST_CASE("ring_view") {
+}
+
