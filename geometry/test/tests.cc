51ae48cd2fc7d27d3280d759122099713300bccd
Chris Samuelson <chris.sam55@gmail.com>
2025-03-07 14:56:30 -0500

    another data structure idea

---
+++ geometry/test/tests.cc
@@ -0,0 +1,11 @@
+#include <template.hh>
+
+#include <catch2/catch_all.hpp>
+
+using namespace flp;
+
+// the gif here I think demonstrates what I'm thinking about
+//https://github.com/skramm/homog2d
+TEST_CASE("template") {
+}
+
