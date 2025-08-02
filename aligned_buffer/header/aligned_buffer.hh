828055f744fbcb29cfd03db7d74391dea4844667
Chris Samuelson <chris.sam55@gmail.com>
2025-08-02 11:07:56 -0400

    update to use launder for UB/lifetime safety

--- aligned_buffer/header/aligned_buffer.hh
+++ aligned_buffer/header/aligned_buffer.hh
@@ -2,41 +2,20 @@
 
 #include <array>
 #include <cstddef>
+#include <new>
 
 namespace flp {
 
-// this must always be manually destructed
+// `destruct` must always manually be called.
 // there is no way to track the object lifetime without changing memory footprint
-// from the 'contained' type, and correspondingly change alignment
-// this means this is only a utility/facility class, because it cannot maintain its own invariants
+// from the 'contained' type and correspondingly changing alignment.
+// This means this is only a utility/facility class because it cannot maintain its own invariants.
 
 template<size_t AllocationSize, size_t AllocationAlignment>
 struct AlignedBuffer {
   static constexpr size_t Size = AllocationSize;
 
   alignas(AllocationAlignment) std::array<std::byte, Size> storage;
-
-  template<typename Type, typename... Args>
-    requires(sizeof(Type) == AllocationSize and alignof(Type) == AllocationAlignment)
-  void construct(Args&&... args) {
-    new (storage.data()) Type(std::forward<Args>(args)...);
-  }
-  template<typename Type>
-  void destruct() noexcept(std::is_nothrow_destructible_v<Type>) {
-    reinterpret_cast<Type*>(&storage)->~Type();
-  }
-
-  template<typename Type>
-  [[nodiscard]]
-  Type& get() noexcept {
-    return *reinterpret_cast<Type*>(&storage);
-  }
-
-  template<typename Type>
-  [[nodiscard]]
-  const Type& get() const noexcept {
-    return *reinterpret_cast<Type*>(&storage);
-  }
 };
 
 template<typename Type>
@@ -44,21 +23,22 @@ struct AlignedTypeBuffer : AlignedBuffer<sizeof(Type), alignof(Type)> {
   using Base = AlignedBuffer<sizeof(Type), alignof(Type)>;
 
   template<typename... Args>
-  void construct(Args&&... args) {
-    Base::template construct<Type>(std::forward<Args>(args)...);
+  Type* construct(Args&&... args) noexcept(std::is_nothrow_constructible_v<Type, Args...>) {
+    return new (Base::storage.data()) Type(std::forward<Args>(args)...);
   }
+
   void destruct() noexcept(std::is_nothrow_destructible_v<Type>) {
-    Base::template destruct<Type>();
+    std::launder<Type*>(&Base::storage)->~Type();
   }
 
   [[nodiscard]]
   Type& get() noexcept {
-    return Base::template get<Type>();
+    return *std::launder<Type*>(&Base::storage);
   }
 
   [[nodiscard]]
   const Type& get() const noexcept {
-    return Base::template get<Type>();
+    return *std::launder<Type*>(&Base::storage);
   }
 };
 
