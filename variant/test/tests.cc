619e707fae3314e985a24b2c1fc55812d3ab2de7
Chris Samuelson <chris.sam55@gmail.com>
2025-08-17 18:34:31 -0400

    update `Variant` tests to be something a little more accurate, and reliable

--- variant/test/tests.cc
+++ variant/test/tests.cc
@@ -12,12 +12,17 @@ struct Buffer {
 };
 
 TEST_CASE("`Variant`") {
-  STATIC_CHECK(sizeof(Variant<int>) == sizeof(int) + 4);
-  STATIC_CHECK(sizeof(Variant<int, int>) == sizeof(int) + 4);
-  STATIC_CHECK(sizeof(Variant<float, double>) == sizeof(double) + 4);
-  STATIC_CHECK(sizeof(Variant<Buffer<0>, Buffer<0>>) == 1) + 4;
-  STATIC_CHECK(sizeof(Variant<Buffer<1>, Buffer<0>>) == 1 + 4);
-  STATIC_CHECK(sizeof(Variant<Buffer<0>, Buffer<1>>) == 1 + 4);
-  STATIC_CHECK(sizeof(Variant<Buffer<1>, Buffer<1>>) == 1 + 4);
-  STATIC_CHECK(sizeof(Variant<Buffer<10>, Buffer<1>>) == 10 + 4);
+  // Padding and alignment overhead makes these tests not so straight forward
+  //STATIC_CHECK(sizeof(Variant<int>) == sizeof(int) + 4);
+  //STATIC_CHECK(sizeof(Variant<int, int>) == sizeof(int) + 4);
+  //STATIC_CHECK(sizeof(Variant<float, double>) == sizeof(double) + 4);
+  //STATIC_CHECK(sizeof(Variant<Buffer<0>, Buffer<0>>) == 1) + 4;
+  //STATIC_CHECK(sizeof(Variant<Buffer<1>, Buffer<0>>) == 1 + 4);
+  //STATIC_CHECK(sizeof(Variant<Buffer<0>, Buffer<1>>) == 1 + 4);
+  //STATIC_CHECK(sizeof(Variant<Buffer<1>, Buffer<1>>) == 1 + 4);
+  //STATIC_CHECK(sizeof(Variant<Buffer<10>, Buffer<1>>) == 10 + 4);
+
+  STATIC_CHECK(sizeof(Variant<int>) >= sizeof(int));
+  STATIC_CHECK(sizeof(Variant<int, int, int, int>) == sizeof(Variant<int>));
+  STATIC_CHECK(sizeof(Variant<uint8_t, double>) == sizeof(Variant<double>));
 }
