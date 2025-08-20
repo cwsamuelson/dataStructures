3bbcfd0cdf4c36126cf3b9edce710d0d3bad5657
Chris Samuelson <chris.sam55@gmail.com>
2025-08-19 21:59:07 -0400

    Expanding 'shape' tests
    not liking the name 'shape' right now...

--- shape/header/shapes/functions.hh
+++ shape/header/shapes/functions.hh
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "shapes/common.hh"
+#include "shapes/vec2.hh"
+#include "shapes/vec3.hh"
+#include "shapes/vec4.hh"
+
+#include <cmath>
 
 namespace flp {
 
@@ -22,6 +26,11 @@ vec2 sign(vec2 f) noexcept {
 constexpr
 vec3 sign(vec3 f) noexcept {
   return { sign(f.x), sign(f.y), sign(f.z) };
+}
+
+constexpr
+vec4 sign(vec4 f) noexcept {
+  return { sign(f.x), sign(f.y), sign(f.z), sign(f.w) };
 }
 
 constexpr
@@ -50,8 +59,13 @@ vec3 clamp(vec3 f, float l, float h) noexcept {
 }
 
 constexpr
-float sqrt(float f) noexcept {
-  return 0.F;
+vec4 clamp(vec4 f, vec4 l, vec4 h) noexcept {
+  return { clamp(f.x, l.x, h.x), clamp(f.y, l.y, h.y), clamp(f.z, l.z, h.z), clamp(f.w, l.w, h.w) };
+}
+
+constexpr
+vec4 clamp(vec4 f, float l, float h) noexcept {
+  return { clamp(f.x, l, h), clamp(f.y, l, h), clamp(f.z, l, h), clamp(f.w, l, h) };
 }
 
 constexpr
@@ -74,12 +88,22 @@ vec3 abs(vec3 v) noexcept {
 }
 
 constexpr
+vec4 abs(vec4 v) noexcept {
+  return { abs(v.x), abs(v.y), abs(v.z), abs(v.w) };
+}
+
+constexpr
 float dot(vec2, vec2) noexcept {
   return 0.F;
 }
 
 constexpr
 float dot(vec3, vec3) noexcept {
+  return 0.F;
+}
+
+constexpr
+float dot(vec4, vec4) noexcept {
   return 0.F;
 }
 
@@ -94,18 +118,28 @@ float dot2(vec3 v) noexcept {
 }
 
 constexpr
+float dot2(vec4 v) noexcept {
+  return dot(v, v);
+}
+
+constexpr
 float ndot(vec2 a, vec2 b) noexcept {
   return a.x * b.x - a.y * b.y;
 }
 
 constexpr
-float length(vec2) noexcept {
-  return 0.F;
+float length(const vec2& vector) noexcept {
+  return std::sqrt(vector.x * vector.x + vector.y * vector.y);
 }
 
 constexpr
-float length(vec3) noexcept {
-  return 0.F;
+float length(const vec3& vector) noexcept {
+  return std::sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+}
+
+constexpr
+float length(const vec4& vector) noexcept {
+  return std::sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z + vector.w * vector.w);
 }
 
 constexpr
@@ -129,6 +163,11 @@ vec3 min(vec3 a, vec3 b) noexcept {
 }
 
 constexpr
+vec4 min(vec4 a, vec4 b) noexcept {
+  return { min(a.x, b.x), min(a.y, b.y), min(a.z, b.z), min(a.w, b.w) };
+}
+
+constexpr
 vec2 max(vec2 a, vec2 b) noexcept {
   return { max(a.x, b.x), max(a.y, b.y) };
 }
@@ -136,6 +175,11 @@ vec2 max(vec2 a, vec2 b) noexcept {
 constexpr
 vec3 max(vec3 a, vec3 b) noexcept {
   return { max(a.x, b.x), max(a.y, b.y), max(a.z, b.z) };
+}
+
+constexpr
+vec4 max(vec4 a, vec4 b) noexcept {
+  return { max(a.x, b.x), max(a.y, b.y), max(a.z, b.z), max(a.w, b.w) };
 }
 
 }
--- shape/test/tests.cc
+++ shape/test/tests.cc
@@ -1,3 +1,4 @@
+#include <shapes/functions.hh>
 #include <shapes/vec2.hh>
 #include <shapes/vec3.hh>
 
@@ -7,34 +8,34 @@ using namespace flp;
 
 TEST_CASE("Shapes: `vec2`") {
   SECTION("Default ctor") {
-    const vec2 v;
+    constexpr vec2 v;
 
-    CHECK(v.x == 0.F);
-    CHECK(v.y == 0.F);
+    STATIC_CHECK(v.x == 0.F);
+    STATIC_CHECK(v.y == 0.F);
 
-    CHECK(v == vec2{});
-    CHECK(v == vec2{ 0.F });
-    CHECK(v == vec2{ 0.F, 0.F });
-    CHECK(2.F * v == v);
-    CHECK(v * 2.F == v);
+    STATIC_CHECK(v == vec2{});
+    STATIC_CHECK(v == vec2{ 0.F });
+    STATIC_CHECK(v == vec2{ 0.F, 0.F });
+    STATIC_CHECK(2.F * v == v);
+    STATIC_CHECK(v * 2.F == v);
   }
 
   SECTION("Single value ctor") {
-    const vec2 v(3.F);
+    constexpr vec2 v(3.F);
 
-    CHECK(v.x == 3.F);
-    CHECK(v.y == 3.F);
+    STATIC_CHECK(v.x == 3.F);
+    STATIC_CHECK(v.y == 3.F);
 
-    CHECK(v == vec2{ 3.F });
-    CHECK(v == vec2{ 3.F, 3.F });
-    CHECK(2.F * v == vec2{ 6.F });
-    CHECK(v * 2.F == vec2{ 6.F });
+    STATIC_CHECK(v == vec2{ 3.F });
+    STATIC_CHECK(v == vec2{ 3.F, 3.F });
+    STATIC_CHECK(2.F * v == vec2{ 6.F });
+    STATIC_CHECK(v * 2.F == vec2{ 6.F });
 
-    CHECK(v > vec2{});
-    CHECK(v >= vec2{});
-    CHECK(vec2{} < v);
-    CHECK(vec2{} <= v);
-    CHECK(v != vec2{});
+    STATIC_CHECK(v > vec2{});
+    STATIC_CHECK(v >= vec2{});
+    STATIC_CHECK(vec2{} < v);
+    STATIC_CHECK(vec2{} <= v);
+    STATIC_CHECK(v != vec2{});
   }
 }
 
@@ -44,6 +45,7 @@ TEST_CASE("Shapes: `vec3`") {
 
     STATIC_CHECK(v.x == 0.F);
     STATIC_CHECK(v.y == 0.F);
+    STATIC_CHECK(v.z == 0.F);
 
     STATIC_CHECK(v == vec3{});
     STATIC_CHECK(v == vec3{ 0.F });
@@ -57,6 +59,7 @@ TEST_CASE("Shapes: `vec3`") {
 
     STATIC_CHECK(v.x == 3.F);
     STATIC_CHECK(v.y == 3.F);
+    STATIC_CHECK(v.z == 3.F);
 
     STATIC_CHECK(v == vec3{ 3.F });
     STATIC_CHECK(v == vec3{ 3.F, 3.F, 3.F });
@@ -71,7 +74,146 @@ TEST_CASE("Shapes: `vec3`") {
   }
 }
 
+TEST_CASE("Shapes: `vec4`") {
+  SECTION("Default ctor") {
+    constexpr vec4 v;
+
+    STATIC_CHECK(v.x == 0.F);
+    STATIC_CHECK(v.y == 0.F);
+    STATIC_CHECK(v.z == 0.F);
+    STATIC_CHECK(v.w == 0.F);
+
+    STATIC_CHECK(v == vec4{});
+    STATIC_CHECK(v == vec4{ 0.F });
+    STATIC_CHECK(v == vec4{ 0.F, 0.F, 0.F, 0.F });
+    STATIC_CHECK(2.F * v == v);
+    STATIC_CHECK(v * 2.F == v);
+  }
+
+  SECTION("Single value ctor") {
+    constexpr vec4 v(3.F);
+
+    STATIC_CHECK(v.x == 3.F);
+    STATIC_CHECK(v.y == 3.F);
+    STATIC_CHECK(v.z == 3.F);
+    STATIC_CHECK(v.w == 3.F);
+
+    STATIC_CHECK(v == vec4{ 3.F });
+    STATIC_CHECK(v == vec4{ 3.F, 3.F, 3.F, 3.f });
+    STATIC_CHECK(2.F * v == vec4{ 6.F });
+    STATIC_CHECK(v * 2.F == vec4{ 6.F });
+
+    STATIC_CHECK(v > vec4{});
+    STATIC_CHECK(v >= vec4{});
+    STATIC_CHECK(vec4{} < v);
+    STATIC_CHECK(vec4{} <= v);
+    STATIC_CHECK(v != vec4{});
+  }
+}
+
 TEST_CASE("Shapes: functions") {
+  SECTION("Sign") {
+    //float sign(float f)
+    STATIC_CHECK(sign(0.f) == 0.f);
+    STATIC_CHECK(sign(1.f) == 1.f);
+    STATIC_CHECK(sign(-1.f) == -1.f);
+    STATIC_CHECK(sign(2.f) == 1.f);
+    STATIC_CHECK(sign(-2.f) == -1.f);
+    STATIC_CHECK(sign(3.14f) == 1.f);
+    STATIC_CHECK(sign(-3.14f) == -1.f);
+
+    //vec2 sign(vec2 f)
+    STATIC_CHECK(sign(vec2{ 0.f, 3.f }) == vec2{ 0.f, 1.f });
+    STATIC_CHECK(sign(vec2{ -1.f, 3.2f }) == vec2{ -1.f, 1.f });
+
+    //vec3 sign(vec3 f)
+    STATIC_CHECK(sign(vec3{ 0.f, 3.f, -2.46 }) == vec3{ 0.f, 1.f, -1.f });
+    STATIC_CHECK(sign(vec3{ -1.f, 3.2f, 0.f }) == vec3{ -1.f, 1.f, 0.f });
+
+    //vec4 sign(vec4 f)
+    STATIC_CHECK(sign(vec4{ 0.f, 3.f, -2.46, -0.f }) == vec4{ 0.f, 1.f, -1.f, 0.f });
+    STATIC_CHECK(sign(vec4{ -1.f, 3.2f, 0.f, 1.f }) == vec4{ -1.f, 1.f, 0.f, 1.f });
+  }
+
+  SECTION("Clamp") {
+    //float clamp(float f, float l, float h)
+    STATIC_CHECK(clamp(.5f, .0f, 1.f) == .5f);
+    STATIC_CHECK(clamp(.5f, 1.2f, 6.7f) == 1.2f);
+    STATIC_CHECK(clamp(.5f, -1.f, .3f) == .3f);
+
+    //vec2 clamp(vec2 f, vec2 l, vec2 h)
+    STATIC_CHECK(clamp(vec2{ .5f, .5f }, vec2{ .0f, 1.2f }, vec2{ 1.f, 6.7f }) == vec2{ .5f, 1.2f });
+    STATIC_CHECK(clamp(vec2{ .5f, 12.f }, vec2{ -1.f, 10.f }, vec2{ .3f, 11.f }) == vec2{ .3f, 11.f });
+
+    //vec2 clamp(vec2 f, float l, float h)
+    STATIC_CHECK(clamp(vec2{ .5f, 15.f }, 1.f, 10.f) == vec2{ 1.f, 10.f });
+    STATIC_CHECK(clamp(vec2{ .5f, 12.f }, .0f, .0f) == vec2{ .0f, .0f });
+
+    //vec3 clamp(vec3 f, vec3 l, vec3 h)
+    STATIC_CHECK(clamp(vec3{ .5f, .5f, .5f }, vec3{ .0f, 1.2f, -10.f }, vec3{ 1.f, 6.7f, -6.7f }) == vec3{ .5f, 1.2f, -6.7f });
+
+    //vec3 clamp(vec3 f, float l, float h)
+    STATIC_CHECK(clamp(vec3{ .5f, 3.f, 7.f }, 2.3f, 5.f) == vec3{ 2.3f, 3.f, 5.f });
+  }
+
+  SECTION("abs") {
+    //float abs(float f)
+    //vec2 abs(vec2 v)
+    //vec3 abs(vec3 v)
+    //vec4 abs(vec4 v)
+  }
+
+  SECTION("dot") {
+    //float dot(vec2, vec2)
+    //float dot(vec3, vec3)
+    //float dot(vec4, vec4)
+
+    //float dot2(vec2 v)
+    //float dot2(vec4 v)
+    //float ndot(vec2 a, vec2 b)
+  }
+
+  SECTION("length") {
+    //float length(const vec2& vector)
+    STATIC_CHECK(length(vec2{ 1.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec2{ 0.f, 1.f }) == 1.f);
+    STATIC_CHECK(length(vec2{ 3.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec2{ 0.f, 3.f }) == 3.f);
+    STATIC_CHECK(length(vec2{ 3.f, 4.f }) == 5.f);
+
+    //float length(const vec3& vector)
+    STATIC_CHECK(length(vec3{ 1.f, 0.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec3{ 0.f, 1.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec3{ 0.f, 0.f, 1.f }) == 1.f);
+    STATIC_CHECK(length(vec3{ 3.f, 0.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec3{ 0.f, 3.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec3{ 0.f, 0.f, 3.f }) == 3.f);
+
+    //float length(const vec4& vector)
+    STATIC_CHECK(length(vec4{ 1.f, 0.f, 0.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec4{ 0.f, 1.f, 0.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec4{ 0.f, 0.f, 1.f, 0.f }) == 1.f);
+    STATIC_CHECK(length(vec4{ 0.f, 0.f, 0.f, 1.f }) == 1.f);
+
+    STATIC_CHECK(length(vec4{ 3.f, 0.f, 0.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec4{ 0.f, 3.f, 0.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec4{ 0.f, 0.f, 3.f, 0.f }) == 3.f);
+    STATIC_CHECK(length(vec4{ 0.f, 0.f, 0.f, 3.f }) == 3.f);
+  }
+
+  SECTION("minmax") {
+    //float min(float a, float b)
+    //float max(float a, float b)
+
+    //vec2 min(vec2 a, vec2 b)
+    //vec2 max(vec2 a, vec2 b)
+
+    //vec3 min(vec3 a, vec3 b)
+    //vec3 max(vec3 a, vec3 b)
+
+    //vec4 min(vec4 a, vec4 b)
+    //vec4 max(vec4 a, vec4 b)
+  }
 }
 
 TEST_CASE("Shapes: swizzling") {
