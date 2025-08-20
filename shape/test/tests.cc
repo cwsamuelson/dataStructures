46cf813b7274777517802c75aa94a3f8c14379eb
Chris Samuelson <chris.sam55@gmail.com>
2025-08-19 22:39:11 -0400

    Swizzling assignment
    there's probably some edge cases I'll have to double check here

--- shape/test/tests.cc
+++ shape/test/tests.cc
@@ -225,6 +225,11 @@ TEST_CASE("Shapes: swizzling") {
     CHECK(vec.xy() == vec2{ 1.f, 2.f });
     CHECK(vec.yx() == vec2{ 2.f, 1.f });
     CHECK(vec.yy() == vec2{ 2.f, 2.f });
+
+    vec.xx() = vec2{ 3.f, 4.f };
+    CHECK(vec == vec2{ 4.f, 2.f });
+    CHECK(vec.x == 4.f);
+    CHECK(vec.y == 2.f);
   }
 
   SECTION("vec3") {
