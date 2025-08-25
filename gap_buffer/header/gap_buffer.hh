a5d9d9b103d032f1585825a87dcd3ea9e91878a0
Chris Samuelson <chris.sam55@gmail.com>
2025-08-25 14:43:35 -0400

    make `GapBuffer` iterable.

--- gap_buffer/header/gap_buffer.hh
+++ gap_buffer/header/gap_buffer.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <core/maybe_const.hh>
+
 #include <algorithm>
 #include <cstddef>
 #include <string>
@@ -11,13 +13,127 @@ namespace flp {
  */
 template<typename Type>
 struct GapBuffer {
+  template<bool IsConst>
+  struct Iterator;
+
+  using iterator = Iterator<false>;
+  using const_iterator = Iterator<true>;
+
+  template<bool IsConst>
+  struct Iterator {
+    using value_type = Type::value_type;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using pointer = MaybeConst<value_type, IsConst>*;
+    using reference = MaybeConst<value_type, IsConst>&;
+
+    using Container = MaybeConst<GapBuffer, IsConst>;
+    using BlockIterator = decltype(std::declval<std::remove_reference_t<Container>>().buffer_sequence.begin());
+
+    Container* buffer = nullptr;
+    BlockIterator block_it;
+    size_t index{};
+
+    Iterator(Container* container, BlockIterator iter, size_t idx)
+      : buffer(container)
+      , block_it(iter)
+      , index(idx)
+    {}
+
+    Iterator() noexcept = default;
+    Iterator(const Iterator&) = default;
+    Iterator(Iterator&&) noexcept = default;
+    Iterator& operator=(const Iterator&) = default;
+    Iterator& operator=(Iterator&&) noexcept = default;
+    ~Iterator() noexcept = default;
+
+    Iterator(const Iterator<false>& other)
+      requires (IsConst) // otherwise it's a duplicate definition
+      : buffer(other.buffer)
+      , block_it(other.block_it)
+      , index(other.index)
+    {}
+
+    Iterator(Iterator<false>&& other) noexcept
+      requires (IsConst) // otherwise it's a duplicate definition
+      : buffer(other.buffer)
+      , block_it(std::move(other.block_it))
+      , index(other.index)
+    {}
+
+    Iterator& operator=(const Iterator<false>& other)
+      requires (IsConst) {
+      buffer = other.buffer;
+      block_it = other.block_it;
+      index = other.index;
+      return *this;
+    }
+
+    Iterator& operator=(Iterator<false>&& other) noexcept
+      requires (IsConst) {
+      buffer = other.buffer;
+      block_it = std::move(other.block_it);
+      index = other.index;
+      return *this;
+    }
+
+    friend bool operator==(const Iterator& lhs, const Iterator& rhs) noexcept = default;
+    friend auto operator<=>(const Iterator& lhs, const Iterator& rhs) noexcept = default;
+
+    reference operator*(this auto&& self) noexcept {
+      return self.block_it->at(self.index);
+    }
+
+    pointer operator->(this auto&& self) noexcept {
+      return &self.block_it->at(self.index);
+    }
+
+    Iterator& operator++() noexcept {
+      if (block_it == buffer->buffer_sequence.end()) {
+        return *this;
+      }
+
+      ++index;
+
+      if (index == block_it->size()) {
+        ++block_it;
+        index = 0;
+      }
+
+      return *this;
+    }
+
+    Iterator operator++(int) noexcept {
+      Iterator copy = *this;
+      ++(*this);
+      return copy;
+    }
+
+    Iterator& operator--() noexcept {
+      if (index == 0) {
+        --block_it;
+        return *this;
+      }
+
+      --index;
+
+      return *this;
+    }
+
+    Iterator operator--(int) noexcept {
+      Iterator copy = *this;
+      --(*this);
+      return copy;
+    }
+  };
+
   constexpr
   GapBuffer() noexcept = default;
 
   constexpr
-  GapBuffer(Type string) {
+  GapBuffer(Type string)
+    : buffer_sequence(1, std::move(string)) {
     buffer_sequence.front().reserve(block_size);
-    buffer_sequence.front() = std::move(string);
   }
 
   constexpr
@@ -104,9 +220,93 @@ struct GapBuffer {
     return string;
   }
 
-  friend auto operator<=>(const GapBuffer&, const GapBuffer&) noexcept = default;
-  //friend std::strong_ordering operator<=>(const GapBuffer& buffer, std::string& string) noexcept {
-  //}
+  friend auto operator<=>(const GapBuffer& lhs, const GapBuffer& rhs) noexcept {
+    auto lhs_iter = lhs.begin();
+    auto rhs_iter = rhs.begin();
+
+    while (lhs_iter != lhs.end() and rhs_iter != rhs.end()) {
+      if (*lhs_iter != *rhs_iter) {
+        return *lhs_iter <=> *rhs_iter;
+      }
+      ++lhs_iter;
+      ++rhs_iter;
+    }
+
+    if (lhs_iter == lhs.end()) {
+      return std::strong_ordering::less;
+    }
+
+    if (rhs_iter == rhs.end()) {
+      return std::strong_ordering::greater;
+    }
+
+    return std::strong_ordering::equal;
+  }
+
+  friend
+  std::strong_ordering operator<=>(
+    const GapBuffer& buffer,
+    const char* string
+  ) noexcept {
+    return buffer <=> std::string_view(string);
+  }
+
+  friend
+  std::strong_ordering operator<=>(
+    const GapBuffer& buffer,
+    const std::string_view& string
+  ) noexcept {
+    auto buf_iter = buffer.begin();
+    auto str_iter = string.begin();
+
+    while (buf_iter != buffer.end() and str_iter != string.end()) {
+      if (*buf_iter != *str_iter) {
+        return *buf_iter <=> *str_iter;
+      }
+      ++buf_iter;
+      ++str_iter;
+    }
+
+    if (buf_iter == buffer.end()) {
+      return std::strong_ordering::less;
+    }
+
+    if (str_iter == string.end()) {
+      return std::strong_ordering::greater;
+    }
+
+    return std::strong_ordering::equal;
+  }
+
+  [[nodiscard]]
+  friend
+  bool operator==(const GapBuffer& buffer, const std::string_view& string) {
+    auto buf_iter = buffer.begin();
+    auto str_iter = string.begin();
+
+    while (buf_iter != buffer.end() and str_iter != string.end()) {
+      if (*buf_iter != *str_iter) {
+        return false;
+      }
+      ++buf_iter;
+      ++str_iter;
+    }
+
+    return true;
+  }
+
+  template<typename Self>
+  auto begin(this Self&& self) noexcept {
+    return Iterator<std::is_const_v<std::remove_reference_t<Self>>>(&self, self.buffer_sequence.begin(), 0);
+  }
+
+  template<typename Self>
+  auto end(this Self&& self) noexcept {
+    return Iterator<std::is_const_v<std::remove_reference_t<Self>>>(&self, self.buffer_sequence.end(), 0);
+  }
+
+  const_iterator cbegin() const;
+  const_iterator cend() const;
 
   // block_size represents how much memory to allocate per block by default
   static constexpr size_t block_size{2048};
--- gap_buffer/test/tests.cc
+++ gap_buffer/test/tests.cc
@@ -9,20 +9,37 @@ TEST_CASE("`GapBuffer`: `GapString`") {
   CHECK(string.size() == 0);
   CHECK(string.capacity() == 0);
   CHECK(string.empty());
+  CHECK(string == "");
+  CHECK(string == std::string(""));
 
   string = "";
   CHECK(string.size() == 0);
   // even on an empty string, a buffer will be allocated to accomodate
   CHECK(string.capacity() != 0);
   CHECK(string.empty());
+  CHECK(string == "");
+  CHECK(string == std::string(""));
 
   string = "TEST TEXT";
   CHECK(string.size() == 9);
   CHECK(string.capacity() >= 10);
   CHECK(not string.empty());
+  CHECK(string == "TEST TEXT");
+  CHECK(string == std::string("TEST TEXT"));
 
   string = std::string("TEST TEXT");
   CHECK(string.size() == 9);
   CHECK(string.capacity() >= 10);
   CHECK(not string.empty());
+  CHECK(string == "TEST TEXT");
+  CHECK(string == std::string("TEST TEXT"));
+
+  string = "xxx";
+  CHECK(string < "xxxx");
+  CHECK(string > "xx");
+  CHECK(string < GapString("xxxx"));
+  CHECK(string > GapString("xx"));
+  string = "A";
+  CHECK(string < "B");
+  CHECK(string < GapString("B"));
 }
