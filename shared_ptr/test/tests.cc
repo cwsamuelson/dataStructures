ac9b33d5dc00f9cafb545b97144a6170200ee692
Chris Samuelson <chris.sam55@gmail.com>
2025-08-25 12:22:55 -0400

    Remove historical debug print statements

--- shared_ptr/test/tests.cc
+++ shared_ptr/test/tests.cc
@@ -6,7 +6,7 @@
 
 using namespace flp;
 
-TEST_CASE("SharedPointer") {
+TEST_CASE("`SharedPointer`") {
   SharedPointer<int> pointer1(new int);
 
   CHECK(pointer1.use_count() == 1);
@@ -28,47 +28,29 @@ TEST_CASE("SharedPointer") {
     CHECK(*pointer2 == 1138);
     CHECK(pointer2);
 
-    std::println("Pre");
-
     SECTION("reset pointer1") {
-      std::println("A1");
       pointer1.reset();
 
-      std::println("A2");
       CHECK(pointer1.use_count() == 0);
-      std::println("A3");
       CHECK(pointer2.use_count() == 1);
 
-      std::println("A4");
       CHECK(pointer1.get() == nullptr);
 
-      std::println("A5");
       CHECK(not pointer1);
-      std::println("A6");
       CHECK(pointer2);
     }
 
-    std::println("Interstitium");
-
     SECTION("reset pointer2") {
-      std::println("B1");
       pointer2.reset();
 
-      std::println("B2");
       CHECK(pointer2.use_count() == 0);
-      std::println("B3");
       CHECK(pointer1.use_count() == 1);
 
-      std::println("B4");
       CHECK(pointer2.get() == nullptr);
 
-      std::println("B5");
       CHECK(pointer1);
-      std::println("B6");
       CHECK(not pointer2);
     }
-
-    std::println("Post");
   }
 
   SECTION("Scoped Copy") {
@@ -86,4 +68,3 @@ TEST_CASE("SharedPointer") {
     CHECK(*pointer1 == 1138);
   }
 }
-
