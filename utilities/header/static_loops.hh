de9a5e55c7bc6572f10b1438cc9b4cf9903577c9
Chris Samuelson <chris.sam55@gmail.com>
2025-08-30 12:11:18 -0400

    lowercass names

--- utilities/header/static_loops.hh
+++ utilities/header/static_loops.hh
@@ -7,18 +7,18 @@
 namespace flp {
 
 template<typename Function, typename ...Args>
-void StaticLoop(Function&& function, Args&& ...args) {
-    (function(args), ...);
+void static_loop(Function&& function, Args&& ...args) {
+  (function(args), ...);
 }
 
 template<typename Function, typename T, typename ...Args, T ...Ts>
-void TupleLoopImpl(Function&& function, const std::tuple<Args...>& tuple, std::integer_sequence<T, Ts...>) {
-    (function(std::get<Ts>(tuple)), ...);
+void tuple_loop_impl(Function&& function, const std::tuple<Args...>& tuple, std::integer_sequence<T, Ts...>) {
+  (function(std::get<Ts>(tuple)), ...);
 }
 
 template<typename Function, typename ...Args>
-void TupleLoop(Function&& function, const std::tuple<Args...>& tuple) {
-    TupleLoopImpl(function, tuple, std::make_index_sequence<sizeof...(Args)>());
+void tuple_loop(Function&& function, const std::tuple<Args...>& tuple) {
+  tuple_loop_impl(function, tuple, std::make_index_sequence<sizeof...(Args)>());
 }
 
 /*int main() {
