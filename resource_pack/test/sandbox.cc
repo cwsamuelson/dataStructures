aa4b3004e038e65e34c8edc22bcfd9ca4e6b3712
Chris Samuelson <chris.sam55@gmail.com>
2025-09-28 19:55:20 -0400

    Fix-up tests for reading/writing resource pack

--- resource_pack/test/sandbox.cc
+++ resource_pack/test/sandbox.cc
@@ -1,6 +1,7 @@
 #include <catch2/catch_all.hpp>
 
 #include <filesystem>
+#include <fstream>
 #include <map>
 #include <spanstream>
 #include <string>
@@ -26,31 +27,24 @@ struct ResourcePack {
     data.clear();
   }
 
-  void operator[](std::string_view key) {
+  decltype(auto) operator[](this auto&& self, std::string_view key) {
+    return self.data.at[key];
   }
 
   static
   void save(const ResourcePack& pack, const std::filesystem::path destination) {
-    struct Index {
-      struct Entry {
-        struct Key {
-          size_t length{};
-          std::string text;
-        };
+    std::ofstream file_stream(destination);
+    // VERIFY(file_stream, "Couldn't open file({})", destination.string());
 
-        Key key;
-        // offset into data section of data associated with key
-        size_t offset{};
-        // size of the blob data associated with the key
-        size_t size{};
-      };
-
-      std::vector<Entry> entries;
-    };
+    write(pack, file_stream);
   }
 
   static
-  void load(const std::filesystem::path source) {
+  ResourcePack load(const std::filesystem::path source) {
+    std::ifstream file_stream(source);
+    // VERIFY(file_stream, "Couldn't open file({})", destination.string());
+
+    return read(file_stream);
   }
 
   static
@@ -69,49 +63,55 @@ struct ResourcePack {
     Blob blob;
     blob.resize(blob_size);
 
-    write(pack, blob);
+    write(pack, std::span{ blob.data(), blob.size() });
     return blob;
+  }
+
+  template<typename OSTREAM>
+  static
+  void write(const ResourcePack& pack, OSTREAM& ostream) {
+    const auto index_size = pack.data.size();
+    ostream.write(reinterpret_cast<const OSTREAM::char_type*>(&index_size), 8);
+
+    // write index; keys and offsets
+    for (size_t offset{}; const auto& [key, blob] : pack.data) {
+      // <key length>"key string"<data offset><data length>
+      const auto key_length = key.size();
+      ostream.write(reinterpret_cast<const OSTREAM::char_type*>(&key_length), 8);
+      ostream.write(reinterpret_cast<const OSTREAM::char_type*>(key.data()), key.size());
+      ostream.write(reinterpret_cast<const OSTREAM::char_type*>(&offset), 8);
+
+      const auto blob_size = blob.size();
+      ostream.write(reinterpret_cast<const OSTREAM::char_type*>(&blob_size), 8);
+
+      offset += blob_size;
+    }
+
+    // write blob section; binary data
+    for (const auto& [key, blob] : pack.data) {
+      ostream.write(reinterpret_cast<const OSTREAM::char_type*>(blob.data()), blob.size());
+    }
   }
 
   static
   void write(const ResourcePack& pack, const std::span<std::byte> span) {
     std::basic_spanstream blob_stream(span);
 
-    const auto index_size = pack.data.size();
-    blob_stream.write(reinterpret_cast<const std::byte*>(&index_size), 8);
-
-    for (size_t offset{}; const auto& [key, blob] : pack.data) {
-      // <key length>"key string"<data offset><data length>
-      const auto key_length = key.size();
-      blob_stream.write(reinterpret_cast<const std::byte*>(&key_length), 8);
-      blob_stream.write(reinterpret_cast<const std::byte*>(key.data()), key.size());
-      blob_stream.write(reinterpret_cast<const std::byte*>(&offset), 8);
-      const auto blob_size = blob.size();
-      blob_stream.write(reinterpret_cast<const std::byte*>(&blob_size), 8);
-
-      offset += blob_size;
-    }
+    write(pack, blob_stream);
   }
 
-  // this should be able to be `std::span<const std::byte>`, but it fails to
-  // build.  It seems that `std::basic_spanstream` erroneously requires its type
-  // to be non-const.  Even `ispanstream` seems to have this issue, even though
-  // especially the input stream would have no need for the type to be non-const.
+  template<typename ISTREAM>
+    requires requires(ISTREAM istream, ISTREAM::char_type* data, const size_t count) {
+      istream.read(data, count);
+    }
   static
-  ResourcePack read(const std::span<std::byte> span) {
-    std::basic_ispanstream blob_stream(span);
-
+  ResourcePack read(ISTREAM& istream) {
     size_t entries{};
-    blob_stream.read(reinterpret_cast<std::byte*>(&entries), 8);
+    istream.read(reinterpret_cast<ISTREAM::char_type*>(&entries), 8);
 
     struct Index {
       struct Entry {
-        struct Key {
-          size_t length{};
-          std::string text;
-        };
-
-        Key key;
+        std::string key;
         // offset into data section of data associated with key
         size_t offset{};
         // size of the blob data associated with the key
@@ -123,41 +123,54 @@ struct ResourcePack {
 
     Index index;
 
+    // read index data
     for (size_t i{}; i < entries; ++i) {
       size_t key_length{};
-      blob_stream.read(reinterpret_cast<std::byte*>(&key_length), 8);
+      istream.read(reinterpret_cast<ISTREAM::char_type*>(&key_length), 8);
 
-      std::string key_text;
-      key_text.resize(key_length);
-      blob_stream.read(reinterpret_cast<std::byte*>(key_text.data()), key_length);
+      std::string key;
+      key.resize(key_length);
+      istream.read(reinterpret_cast<ISTREAM::char_type*>(key.data()), key_length);
 
       size_t data_offset{};
-      blob_stream.read(reinterpret_cast<std::byte*>(&data_offset), 8);
+      istream.read(reinterpret_cast<ISTREAM::char_type*>(&data_offset), 8);
 
       size_t data_size{};
-      blob_stream.read(reinterpret_cast<std::byte*>(&data_size), 8);
+      istream.read(reinterpret_cast<ISTREAM::char_type*>(&data_size), 8);
 
-      const Index::Entry::Key key{ key_length, key_text };
+      // VERIFY(key.size() == key_length, "Inconsistent key string sizes.");
 
       index.entries.emplace_back(key, data_offset, data_size);
     }
 
+    // read blob data
     ResourcePack pack;
     for (const auto& entry : index.entries) {
       std::vector<std::byte> blob;
       blob.resize(entry.size);
-      blob_stream.read(reinterpret_cast<std::byte*>(blob.data()), entry.size);
-      pack.data[entry.key.text] = std::move(blob);
+      istream.read(reinterpret_cast<ISTREAM::char_type*>(blob.data()), entry.size);
+      pack.data[entry.key] = std::move(blob);
     }
 
     return pack;
+  }
+
+  // this should be able to be `std::span<const std::byte>`, but it fails to
+  // build.  It seems that `std::basic_spanstream` erroneously requires its type
+  // to be non-const.  Even `ispanstream` seems to have this issue, even though
+  // especially the input stream would have no need for the type to be non-const.
+  static
+  ResourcePack read(const std::span<std::byte> span) {
+    std::basic_ispanstream blob_stream(span);
+
+    return read(blob_stream);
   }
 
   std::map<std::string, Blob, std::less<>> data;
 };
 
 SCENARIO("`ResourcePack`: sandbox") {
-  {
+  SECTION("Empty pack properties") {
     const ResourcePack empty_pack;
 
     auto blob = ResourcePack::write(empty_pack);
@@ -168,10 +181,11 @@ SCENARIO("`ResourcePack`: sandbox") {
     CHECK(pack.data.empty());
   }
 
-  {
+  SECTION("Pack filled with data") {
     ResourcePack filled_pack;
 
-    filled_pack.data["this is the key, it's weirdly long"] = {};
+    const auto long_key = "this is the key, it's weirdly long";
+    filled_pack.data[long_key] = {};
     filled_pack.data["shrt"] = { static_cast<std::byte>(0xFF), static_cast<std::byte>(0x40) };
 
     auto blob = ResourcePack::write(filled_pack);
@@ -182,9 +196,11 @@ SCENARIO("`ResourcePack`: sandbox") {
     CHECK(pack.data.size() == 2);
 
     CHECK(pack.data.contains("shrt"));
-    CHECK(pack.data.contains("this is the key, it's weirdly long"));
+    CHECK(pack.data.contains(long_key));
 
-    CHECK(pack.data.at("shrt").empty());
-    CHECK(pack.data.at("this is the key, it's weirdly long").size() == 2);
+    CHECK(pack.data.at(long_key).empty());
+    CHECK(pack.data.at("shrt").size() == 2);
+    CHECK(pack.data.at("shrt").at(0) == static_cast<std::byte>(0xFF));
+    CHECK(pack.data.at("shrt").at(1) == static_cast<std::byte>(0x40));
   }
 }
