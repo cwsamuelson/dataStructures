a0dfd6794cad3cd644dd1fcd8fd42988ca805b29
Chris Samuelson <chris.sam55@gmail.com>
2025-10-04 11:31:51 -0400

    Implementation/tests for basic assignment/construction
    Aggregated types (array/map) don't work yet

--- abstract/header/abstract/values.hh
+++ abstract/header/abstract/values.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <cstdint>
 #include <map>
 #include <string>
@@ -31,6 +32,69 @@ struct Value {
 
   template<typename Type>
   using Allocator = AllocatorType<Type>;
+
+  Value() = default;
+
+  Value(std::nullptr_t)
+    : Value()
+  {}
+
+  template<typename Type>
+  Value(Type object)
+    : value(std::move(object))
+  {}
+
+  Value& operator=(std::nullptr_t) {
+    value = {};
+    return *this;
+  }
+
+  template<typename Type>
+  Value& operator=(Type object) {
+    value = std::move(object);
+    return *this;
+  }
+
+  // Value& operator=(const Boolean object) {
+  //   value = object;
+  //   return *this;
+  // }
+
+  // template<std::signed_integral SInt>
+  //   requires (not std::same_as<SInt, Boolean>)
+  // Value& operator=(const SInt& sivalue) {
+  //   value = static_cast<SignedInteger>(sivalue);
+  //   return *this;
+  // }
+
+  // template<std::unsigned_integral UInt>
+  //   requires (not std::same_as<UInt, Boolean>)
+  // Value& operator=(const UInt& uivalue) {
+  //   value = static_cast<UnsignedInteger>(uivalue);
+  //   return *this;
+  // }
+
+  // template<std::floating_point FloatPt>
+  //   requires (
+  //     not std::signed_integral<FloatPt>
+  // and not std::unsigned_integral<FloatPt>)
+  // Value& operator=(const FloatPt& fpvalue) {
+  //   value = static_cast<Float>(fpvalue);
+  //   return *this;
+  // }
+
+  // template<typename Type>
+  // Value& operator=(const ArrayType<Type>& array) {
+  //   Array new_value;
+
+  //   for (const auto& element : array) {
+  //     new_value.push_back(element);
+  //   }
+
+  //   value = std::move(new_value);
+
+  //   return *this;
+  // }
 
   std::variant<
     std::monostate,
--- abstract/test/values-tests.cc
+++ abstract/test/values-tests.cc
@@ -2,8 +2,72 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <map>
+#include <string>
+#include <vector>
+
 using namespace flp;
 
 TEST_CASE("`Abstract`::`Values`") {
-  Abstract::Value value;
+  {
+    Abstract::Value value;
+
+    value = "";
+    value = true;
+    value = 1;
+    value = -1;
+    // value = std::vector<int>{};
+    // value = std::map<std::string, int>{};
+    value = nullptr;
+  }
+
+  {
+    Abstract::Value value{""};
+  }
+
+  {
+    Abstract::Value value{true};
+  }
+
+  {
+    Abstract::Value value{1};
+  }
+
+  {
+    Abstract::Value value{-1};
+  }
+
+  // {
+  //   {
+  //     Abstract::Value value{std::vector<int>{}};
+  //   }
+  //   {
+  //     Abstract::Value value{std::vector<unsigned int>{}};
+  //   }
+  //   {
+  //     Abstract::Value value{std::vector<double>{}};
+  //   }
+  //   {
+  //     Abstract::Value value{std::vector<std::string>{}};
+  //   }
+  // }
+
+  // {
+  //   {
+  //     Abstract::Value value{std::map<std::string, int>};
+  //   }
+  //   {
+  //     Abstract::Value value{std::map<std::string, unsigned int>};
+  //   }
+  //   {
+  //     Abstract::Value value{std::map<std::string, double>};
+  //   }
+  //   {
+  //     Abstract::Value value{std::map<std::string, std::string>};
+  //   }
+  // }
+
+  {
+    Abstract::Value value{nullptr};
+  }
 }
