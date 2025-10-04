73c4f3ae1ffbd19133e2ed1df624928176391966
Chris Samuelson <chris.sam55@gmail.com>
2025-10-03 21:17:11 -0400

    Test/Implement filter range

--- ranges/header/ranges/filter.hh
+++ ranges/header/ranges/filter.hh
@@ -2,18 +2,27 @@
 
 #include "ranges/traits.hh"
 
-namespace flp::ranges {
+namespace flp::views {
 
-template<typename Container>
+template<typename Container, typename Functor>
 struct FilterView {
   constexpr
-  FilterView(Container& cntnr)
+  FilterView(Container& cntnr, Functor&& functor)
     : container(cntnr)
+    , predicate(std::forward<Functor>(functor))
   {}
 
   struct Iterator {
     using Iter = decltype(std::begin(std::declval<Container>()));
     Iter iterator;
+    FilterView* view;
+
+    constexpr
+    Iterator(Iter iter, FilterView* vw)
+      : iterator(iter)
+      , view(vw) {
+      while (iterator != std::end(view->container) and not view->predicate(*++iterator)) {}
+    }
 
     constexpr
     decltype(auto) operator*(this auto&& self) {
@@ -27,13 +36,13 @@ struct FilterView {
 
     constexpr
     Iterator& operator++(this auto&& self) {
-      while (predicate(*++self.iterator)) {}
+      while (self.iterator != std::end(self.view->container) and not self.view->predicate(*++self.iterator)) {}
       return self;
     }
 
     constexpr
     Iterator operator++(this auto&& self, int) {
-      while (predicate(++self.iterator)) {}
+      while (self.iterator != std::end(self.view->container) and not self.view->predicate(++self.iterator)) {}
       return {self.iterator++};
     }
 
@@ -71,22 +80,32 @@ struct FilterView {
 
   constexpr
   Iterator begin(this auto&& self) {
-    return {std::begin(self.container)};
+    return {std::begin(self.container), &self};
   }
 
   constexpr
   Iterator end(this auto&& self) {
-    return {std::end(self.container)};
+    return {std::end(self.container), &self};
   }
 
   Container& container;
+  Functor predicate;
 };
 
-struct Filter_t {};
+template<typename Functor>
+struct FilterAdaptor {
+  Functor functor;
+};
 
-template<Range Container>
-FilterView<Container> operator|(Container&& container, const Filter_t&) {
-  return {std::forward<Container>(container)};
+template<Range Container, typename Functor>
+constexpr
+FilterView<Container, Functor> operator|(Container&& container, FilterAdaptor<Functor>&& adaptor) {
+  return {std::forward<Container>(container), std::forward<Functor>(adaptor.functor)};
+}
+
+template<typename Functor>
+FilterAdaptor<Functor> filter(Functor&& functor) {
+  return {std::forward<Functor>(functor)};
 }
 
 }
--- ranges/test/filter_tests.cc
+++ ranges/test/filter_tests.cc
@@ -2,7 +2,24 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <print>
+#include <vector>
+
 using namespace flp;
 
 TEST_CASE("`Ranges`::`FilterView`") {
+  std::vector<int> vector{ 0, 0, 1, 2, 3, 4, 0, 0, 5, 6, 7 };
+
+  for (const auto& element : vector | views::filter([](const auto& element) {
+    return element == 0;
+  })) {
+    CHECK(element == 0);
+  }
+
+  for (const auto& element : vector | views::filter([](const auto& element) {
+    return element != 0;
+  })) {
+    std::println("{}", element);
+    CHECK(element != 0);
+  }
 }
