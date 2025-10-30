8b671d5ce58373ac563c2403ef683198fe5fa9fa
Chris Samuelson <chris.sam55@gmail.com>
2025-10-30 17:24:35 -0400

    Implement menu constructor

--- menu/header/menu.hh
+++ menu/header/menu.hh
@@ -1,11 +1,51 @@
 #pragma once
 
+#include <ranges>
+#include <string>
 #include <vector>
+#include <map>
 
 namespace flp {
 
 struct Menu {
-  std::vector<Menu> sub_menus;
+  Menu(const std::string_view prmp)
+    : prompt(prmp)
+  {}
+
+  Menu(const std::string_view prmp, std::map<size_t, Menu> menus)
+    : prompt(prmp)
+    , sub_menus(std::move(menus))
+  {}
+
+  Menu(const std::string_view prmp, std::vector<Menu> menus)
+    : prompt(prmp)
+    , sub_menus(std::from_range, menus | std::views::enumerate)
+  {}
+
+  std::string prompt;
+  std::map<size_t, Menu> sub_menus;
+};
+
+struct MenuManager {
+  Menu* current{nullptr};
+
+  void select(const size_t selection) {
+    current = &current->sub_menus.at(selection);
+  }
+
+private:
+  friend struct Menu;
+
+  MenuManager() = delete;
+  MenuManager(const MenuManager&) noexcept = default;
+  MenuManager(MenuManager&&) noexcept = default;
+  MenuManager& operator=(const MenuManager&) = default;
+  MenuManager& operator=(MenuManager&&) noexcept = default;
+  ~MenuManager() noexcept = default;
+
+  MenuManager(Menu& menu)
+    : current(&menu)
+  {}
 };
 
 } // namespace flp
--- menu/test/tests.cc
+++ menu/test/tests.cc
@@ -2,8 +2,40 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <vector>
+
 using namespace flp;
 
-TEST_CASE("`Template`") {
-  Menu menu;
+TEST_CASE("`Menu`::Construction") {
+  const std::vector<Menu> v;
+  Menu a("");
+
+  CHECK(a.sub_menus.empty());
+
+  Menu b("", v);
+  Menu c("", {{""}});
+  Menu d("", {{""}, {"", {{""}}}});
+
+  CHECK(b.sub_menus.empty());
+  CHECK(c.sub_menus.size() == 1);
+  CHECK(d.sub_menus.size() == 2);
+
+  const std::map<size_t, Menu> m {
+    {0, {""}},
+    {1, {""}},
+    {2, {""}},
+  };
+
+  Menu e("", m);
+  Menu f("", {{0, {""}}});
+  Menu g("", {
+    {0, {""}},
+    {1, {"", {
+      {""}
+    }}}
+  });
+
+  CHECK(e.sub_menus.size() == 3);
+  CHECK(f.sub_menus.size() == 1);
+  CHECK(g.sub_menus.size() == 2);
 }
