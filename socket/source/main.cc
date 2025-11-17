2b33c3849f44dd58c64ce2976d4db0b23f3dde09
Chris Samuelson <chris.sam55@gmail.com>
2025-11-17 16:31:42 -0500

    remove socket pieces that are not implemented yet

--- socket/source/main.cc
+++ socket/source/main.cc
@@ -4,8 +4,8 @@ int main() {
   const auto conn_params = flp::Net::getaddrinfo("www.google.com", "80");
   const auto& params = conn_params.front();
 
-  const auto s = socket((int)params.family, (int)params.type, (int)params.protocol);
-  const auto conn_res = connect(s, server_info->ai_addr, server_info->ai_addrlen);
+  // const auto s = socket((int)params.family, (int)params.type, (int)params.protocol);
+  // const auto conn_res = connect(s, server_info->ai_addr, server_info->ai_addrlen);
 
-  close(s);
+  // close(s);
 }
