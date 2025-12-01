842bbdf601b9af366dfe1d3eb56808c4c0c38749
Chris Samuelson <chris.sam55@gmail.com>
2025-12-01 17:34:46 -0500

    Expanding on Complex implementation

--- utilities/header/complex.hh
+++ utilities/header/complex.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <compare>
 #include <format>
 
@@ -7,36 +8,178 @@ namespace flp {
 
 template<typename Type>
 struct Complex {
-  constexpr
-  auto operator++();
-  constexpr
-  auto operator++(int);
-  constexpr
-  auto operator--();
-  constexpr
-  auto operator--(int);
+  // constexpr
+  // auto operator++();
+  // constexpr
+  // auto operator++(int);
+  // constexpr
+  // auto operator--();
+  // constexpr
+  // auto operator--(int);
 
   constexpr
-  auto operator+(Complex);
-  constexpr
-  auto operator-(Complex);
-  constexpr
-  auto operator*(Complex);
-  constexpr
-  auto operator/(Complex);
-  constexpr
-  auto operator%(Complex);// ?
+  Complex operator+() const noexcept {
+    return {real, imaginary};
+  }
 
   constexpr
-  auto operator+(Type);
+  Complex operator-() const noexcept {
+    return {-real, -imaginary};
+  }
+
   constexpr
-  auto operator-(Type);
+  Complex& operator+=(const Complex& other) noexcept {
+    real += other.real;
+    imaginary += other.imaginary;
+    return *this;
+  }
+
   constexpr
-  auto operator*(Type);
+  Complex& operator-=(const Complex& other) noexcept {
+    real -= other.real;
+    imaginary -= other.imaginary;
+    return *this;
+  }
+
   constexpr
-  auto operator/(Type);
+  Complex& operator*=(const Complex& other) noexcept {
+    const Complex complex(*this);
+    *this = complex * other;
+    return *this;
+  }
+
   constexpr
-  auto operator%(Type);// ?
+  Complex& operator/=(const Complex& other) noexcept {
+    const Complex complex(*this);
+    *this = complex * other;
+    return *this;
+  }
+
+  // ?
+  constexpr
+  Complex& operator%=(const Complex& other) noexcept {
+    return *this;
+  }
+
+  constexpr
+  Complex operator+(const Complex& other) const noexcept {
+    Complex value(*this);
+    value += other;
+    return value;
+  }
+
+  constexpr
+  Complex operator-(const Complex& other) const noexcept {
+    Complex value(*this);
+    value -= other;
+    return value;
+  }
+
+  constexpr
+  Complex operator*(const Complex& other) const noexcept {
+    return {
+      real * other.real - imaginary * other.imaginary,
+      real * other.imaginary + imaginary * other.real
+    };
+  }
+
+  constexpr
+  Complex operator/(const Complex& other) const noexcept {
+    const Type denom = other.real * other.real + other.imaginary * other.imaginary;
+    return {
+      (real * other.real + imaginary * other.imaginary) / denom,
+      (imaginary * other.real - real * other.imaginary) / denom
+    };
+  }
+
+  // ?
+  constexpr
+  Complex operator%(const Complex& other) const noexcept {
+    Complex value(*this);
+    value %= other;
+    return value;
+  }
+
+  constexpr
+  Complex operator+(const Type& value) const noexcept {
+    return (*this) + Complex{value, 0};
+  }
+
+  constexpr
+  Complex operator-(const Type& value) const noexcept {
+    return (*this) - Complex{value, 0};
+  }
+
+  constexpr
+  Complex operator*(const Type& value) const noexcept {
+    return (*this) * Complex{value, 0};
+  }
+
+  constexpr
+  Complex operator/(const Type& value) const noexcept {
+    return (*this) / Complex{value, 0};
+  }
+
+  // ?
+  constexpr
+  Complex operator%(const Type& value) const noexcept {
+    return (*this) % Complex{value, 0};
+  }
+
+  constexpr
+  friend
+  Complex operator+(const Type& value, const Complex& complex) noexcept {
+    return complex + value;
+  }
+
+  constexpr
+  friend
+  Complex operator-(const Type& value, const Complex& complex) noexcept {
+    return complex - value;
+  }
+
+  constexpr
+  friend
+  Complex operator*(const Type& value, const Complex& complex) noexcept {
+    return complex * value;
+  }
+
+  constexpr
+  friend
+  Complex operator/(const Type& value, const Complex& complex) noexcept {
+    return complex / value;
+  }
+
+  // ?
+  constexpr
+  friend
+  Complex operator%(const Type& value, const Complex& complex) noexcept {
+    return complex % value;
+  }
+
+  constexpr
+  Complex conjugate() const noexcept {
+    return {real, -imaginary};
+  }
+
+  constexpr
+  auto magnitude() const noexcept {
+    if constexpr(std::integral<Type>) {
+      return (float)std::sqrt((float)(real * real) + (float)(imaginary * imaginary));
+    } else {
+      return std::sqrt(real * real + imaginary * imaginary);
+    }
+  }
+
+  constexpr
+  Type r() const noexcept {
+    return real;
+  }
+
+  constexpr
+  Type i() const noexcept {
+    return imaginary;
+  }
 
   friend
   constexpr
@@ -56,6 +199,11 @@ template<typename Type>
 struct std::formatter<flp::Complex<Type>> : std::formatter<uint64_t> {
   constexpr
   auto format(const flp::Complex<Type>& number, std::format_context& context) const {
-    return std::format_to(context.out(), "{} + {}i", number.real, number.imaginary);
+    return std::format_to(context.out(), "{} {} i{}", number.real, number.imaginary < 0.f ? '-' : '+', std::abs(number.imaginary));
   }
 };
+
+template<typename OStream, typename Type>
+OStream& operator<<(OStream& ostream, const flp::Complex<Type>& complex) noexcept {
+  return ostream << std::format("{}", complex);
+}
--- utilities/test/complex.cc
+++ utilities/test/complex.cc
@@ -7,5 +7,77 @@
 using namespace flp;
 
 TEST_CASE("`Complex`") {
-  Complex<int> value;
+  rc::prop("Initialization", [](const float r, const float i) {
+    const Complex<float> value{r, i};
+
+    RC_ASSERT(value.r() == r);
+    RC_ASSERT(value.i() == i);
+    RC_ASSERT(value.conjugate() == (Complex<float>{r, -i}));
+  });
+
+  rc::prop("Math", [](const float r0, const float i0, const float r1, const float i1) {
+    const Complex<float> value0{r0, i0};
+    const Complex<float> value1{r1, i1};
+
+    RC_ASSERT(value0 + value1 == (Complex<float>{r0 + r1, i0 + i1}));
+    RC_ASSERT(value0 - value1 == (Complex<float>{r0 - r1, i0 - i1}));
+    // RC_ASSERT(value0 * value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+    // RC_ASSERT(value0 / value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+  });
+
+  SECTION("Golden math") {
+    SECTION("0") {
+      const Complex<float> value0{-3.f, -5.f};
+      const Complex<float> value1{0.f, 2.f};
+
+      // CHECK(value0 * value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+      CHECK(value0 / value1 == (Complex<float>{-5.f / 2.f, 3.f / 2.f}));
+    }
+
+    SECTION("1") {
+      const Complex<float> value0{-1.f, 0.f};
+      const Complex<float> value1{3.f, 2.f};
+
+      // CHECK(value0 * value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+      CHECK(value0 / value1 == (Complex<float>{-3.f / 13.f, 2.f / 13.f}));
+    }
+
+    SECTION("2") {
+      const Complex<float> value0{1.f, 0.f};
+      const Complex<float> value1{3.f, 2.f};
+
+      // CHECK(value0 * value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+      CHECK(value0 / value1 == (Complex<float>{3.f / 13.f, -2.f / 13.f}));
+    }
+
+    SECTION("3") {
+      const Complex<float> value0{5.f, std::sqrt(2.f)};
+      const Complex<float> value1{1.f, -std::sqrt(2.f)};
+
+      // CHECK(value0 * value1 == (Complex<float>{r0 ? r1, i0 ? i1}));
+      CHECK(value0 / value1 == (Complex<float>{1.f, 2 * std::sqrt(2.f)}));
+    }
+  }
+
+  rc::prop("Math with 'normal' numbers", [](const float r, const float i, const float f) {
+    const Complex<float> value{r, i};
+
+    RC_ASSERT(value + f == (Complex<float>{r + f, i}));
+    RC_ASSERT(f + value == (Complex<float>{f + r, i}));
+    RC_ASSERT(value - f == (Complex<float>{r - f, i}));
+    RC_ASSERT(f - value == (Complex<float>{f - r, i}));
+
+    RC_ASSERT(value * f == (Complex<float>{r * f, i * f}));
+    RC_ASSERT(f * value == (Complex<float>{f * r, f * i}));
+    RC_ASSERT(value / f == (Complex<float>{r / f, i / f}));
+    RC_ASSERT(f / value == (Complex<float>{f / r, f / i}));
+  });
+
+  rc::prop("Magnitude", [](const float f) {
+    const Complex<float> value_r{f, 0.f};
+    const Complex<float> value_i{0.f, f};
+
+    RC_ASSERT(value_r.magnitude() == std::abs(f));
+    RC_ASSERT(value_i.magnitude() == std::abs(f));
+  });
 }
