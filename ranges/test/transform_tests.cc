cda2a7f2681344e9d5f187d0a859a676210d59d0
Chris Samuelson <chris.sam55@gmail.com>
2026-08-03 20:31:12 -0400

    Update transform view

--- ranges/test/transform_tests.cc
+++ ranges/test/transform_tests.cc
@@ -1,21 +1,30 @@
 #include <ranges/transform.hh>
 
 #include <catch2/catch_all.hpp>
+#include <rapidcheck/catch.h>
+#include <rapidcheck/Gen.h>
+#include <rapidcheck/gen/Numeric.h>
 
 using namespace flp;
 
 TEST_CASE("`Ranges`::`TransformView`") {
-  std::vector<int> vector{ 0, 0, 1, 2, 3, 4, 0, 0, 5, 6, 7 };
+  rc::prop("Change to a single value of the same type",
+    [](const std::vector<int>& container) {
+      for (const auto& element : container | views::transform([](const auto& element) {
+        return 1138;
+      })) {
+        RC_ASSERT(element == 1138);
+      }
+    }
+  );
 
-  for (const auto& element : vector | views::transform([](const auto& element) {
-    return 1138;
-  })) {
-    CHECK(element == 1138);
-  }
-
-  for (const auto& element : vector | views::transform([](const auto& element) {
-    return 11.38;
-  })) {
-    CHECK(element == 11.38);
-  }
+  rc::prop("Change to a single value a different type",
+    [](const std::vector<int>& container) {
+      for (const auto& element : container | views::transform([](const auto& element) {
+        return 11.38;
+      })) {
+        RC_ASSERT(element == 11.38);
+      }
+    }
+  );
 }
