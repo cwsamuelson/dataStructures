f35b51a1cff0f068f3388b7f6878315e871325ea
Chris Samuelson <chris.sam55@gmail.com>
2026-08-25 14:26:31 -0400

    spline impl things

--- spline/header/spline/catmull-rom.hh
+++ spline/header/spline/catmull-rom.hh
@@ -1,12 +1,138 @@
 #pragma once
 
+#include <error_help.hh>
+
+#include <cmath>
+#include <vector>
+
 // https://www.cs.cmu.edu/~fp/courses/graphics/asst5/catmullRom.pdf
 // https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
 // https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull%E2%80%93Rom_spline
 
 namespace flp::Spline {
 
-struct CatmullRom {
+template<typename Type>
+concept Container = requires(Type container) {
+  // value_type?
+  // ADL this?.. =/
+  // using std::begin;
+  // using std::end;
+  std::begin(container);
+  std::end(container);
 };
+
+template<typename Type>
+concept RandomAccessContainer = Container<Type> and requires(Type container, size_t index) {
+  container[index]/*->std::convertible_to<Type::value_type>*/;
+};
+
+template<typename Point, RandomAccessContainer Container = std::vector<Point>>
+struct CatmullRom {
+  using value_type = Point::value_type;
+  // using value_type = ContainerTraits<Point>::value_type;
+
+  enum Ending {
+    Closed,
+    Open,
+
+    Looped = Closed,
+    Unlooped = Open,
+    Straight = Open,
+
+    Default = Open,
+  };
+
+  CatmullRom(Container&& container/*, value_type alpha*/, Ending closing = Ending::Default)
+    : points(std::move(container)) {
+    const value_type alpha = (value_type)1 / (value_type)2;
+
+    // VERIFY(points.size() < 4, "Catmull-Rom spline requires at least 4 points ({} given)", points.size());
+    // VERIFY(alpha >= 0 and alpha <= 1, "Catmull-Rom parameterization alpha must be [0, 1]");
+
+    // { A, B, C, D, ..., Y, Z
+
+    const auto num_points = points.size();
+    points.resize(num_points + 3);
+
+    // { A, B, C, D, ..., Y, Z, 0, 0, 0}
+
+    points[num_points + 1] = points[0];
+    points[num_points + 2] = points[1];
+
+    // { A, B, C, D, ..., Y, Z, 0, A, B}
+
+    const auto temp = points[num_points - 1];
+    for (size_t i = num_points; i > 0; --i) {
+      points[i] = points[i - 1];
+    }
+    // { A, A, B, C, D, ..., Y, Z, A, B}
+
+    points[0] = temp;
+    // { Z, A, B, C, D, ..., Y, Z, A, B}
+
+    parameters.resize(points.size());
+
+    parameters[0] = -alpha_distance(points[0], points[1], alpha);
+    // After the 'rotation' above, p1 is the intended beginning; therefore,
+    // set its parameter to '0', as a starting reference point
+    parameters[1] = 0;
+
+    for (size_t i{2}; i < parameters.size(); ++i) {
+      const auto d = alpha_distance(points[i], points[i - 1], alpha);
+      parameters[i] = parameters[i - 1] + d;
+    }
+
+    if (closing == Ending::Closed) {
+      max_param = parameters[num_points + 1];
+    } else {
+      max_param = parameters[num_points];
+    }
+  }
+
+  // splines are 'parameterized'.  This is that parameter.
+  // idk, they're sorta reverse-parameterized or de-parameterized, but this works anyway
+  Point operator()(const value_type parameter) const {
+    VERIFY(parameter >= 0 and parameter <= max_param, "Catmull-Rom interpolation parameter must be [0, {}]", max_param);
+
+    // parameters are ordered, per how they're set
+    const auto iterator = std::upper_bound(parameters.begin(), parameters.end(), parameter);
+    // *iterator >= parameter;
+    // Our objective is i: parameters[i] <= parameter < parameters[i + 1]
+    const auto i = std::distance(parameters.begin(), iterator - 1);
+
+    // now do lots of math
+
+    return {};
+  }
+
+  value_type parameter(const size_t index) const {
+    return parameters.at(index);
+  }
+
+  value_type max_parameter() const {
+    return max_param;
+  }
+
+private:
+  static value_type alpha_distance(const Point& p0, const Point& p1, const value_type alpha) {
+    // boost uses ADL for things like 'size', 'pow', etc; not necessary yet
+    value_type dsq = 0;
+
+    for (size_t i{}; i < p1.size(); ++i) {
+      const auto dx = p0[i] - p1[i];
+      dsq += dx * dx;
+    }
+
+    // sqrt
+    return std::pow(dsq, alpha / 2);
+  }
+
+  Container points;
+  std::vector<value_type> parameters;
+  value_type max_param;
+};
+
+template<RandomAccessContainer Container>
+CatmullRom(Container&&) -> CatmullRom<typename Container::value_type, Container>;
 
 } // namespace flp
--- spline/test/tests.cc
+++ spline/test/tests.cc
@@ -2,15 +2,48 @@
 
 #include <catch2/catch_all.hpp>
 
+#include <array>
+#include <vector>
+
 using namespace flp;
 
-TEST_CASE("`Spline`") {
-  Spline::CatmullRom spline;
+TEST_CASE("`Spline`::Catmull-Rom") {
+  using Point = std::array<double, 2>;
+  const Spline::CatmullRom spline(std::vector{Point{0, 0}, Point{1, 0}, Point{2, 0}, Point{3, 0}});
+
+  CHECK(spline.max_parameter() == 3);
+  const auto p0 = spline(0);
+  CHECK(p0[0] == 0.);
+  CHECK(p0[1] == 0.);
+  const auto p1 = spline(1);
+  CHECK(p1[0] == 1.);
+  CHECK(p1[1] == 0.);
+  const auto p2 = spline(2);
+  CHECK(p2[0] == 2.);
+  CHECK(p2[1] == 0.);
+  const auto p3 = spline(3);
+  CHECK(p3[0] == 3.);
+  CHECK(p3[1] == 0.);
+
+  const auto s0 = spline.parameter(0);
+  CHECK(s0 == 0.);
+  const auto s1 = spline.parameter(1);
+  CHECK(s1 == 0.);
+  const auto s2 = spline.parameter(2);
+  CHECK(s2 == 0.);
+  const auto s3 = spline.parameter(3);
+  CHECK(s3 == 0.);
+
+  for (double s{1}; s < 2; s += 0.01) {
+    const auto point = spline(s);
+    CHECK(point[0] == s);
+    CHECK(point[1] == 0);
+  }
 }
 
 // def deBoor(k: int, x: int, t, c, p: int):
 //     """Evaluates S(x).
-// 
+//
 //     Arguments
 //     ---------
 //     k: Index of knot interval that contains x.
@@ -19,16 +52,16 @@ TEST_CASE("`Spline`") {
 //     c: Array of control points.
 //     p: Degree of B-spline.
 //     """
-//     d = [c[j + k - p] for j in range(0, p + 1)] 
-// 
+//     d = [c[j + k - p] for j in range(0, p + 1)]
+//
 //     for r in range(1, p + 1):
 //         for j in range(p, r - 1, -1):
-//             alpha = (x - t[j + k - p]) / (t[j + 1 + k - r] - t[j + k - p]) 
+//             alpha = (x - t[j + k - p]) / (t[j + 1 + k - r] - t[j + k - p])
 //             d[j] = (1.0 - alpha) * d[j - 1] + alpha * d[j]
-// 
+//
 //     return d[p]
 
-auto de_boor(int k, int x, const std::vector<Point>& t, const std::vector<Point>& c, int p) {
+/*auto de_boor(int k, int x, const std::vector<Point>& t, const std::vector<Point>& c, int p) {
   std::vector<Point> d;
 
   for (int j = 0; j < p + 1; ++j) {
@@ -43,7 +76,7 @@ auto de_boor(int k, int x, const std::vector<Point>& t, const std::vector<Point>
   }
 
   return d[p];
-}
+}*/
 
 TEST_CASE("`Spline`::sandbox") {
 }
