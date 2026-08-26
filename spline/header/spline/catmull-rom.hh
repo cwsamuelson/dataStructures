a2dcbf55b7975091ca4fdf81da4cff4ab76c8fc6
Chris Samuelson <chris.sam55@gmail.com>
2026-08-26 14:07:47 -0400

    Found the spline bug!
    some parameters were swapped

--- spline/header/spline/catmull-rom.hh
+++ spline/header/spline/catmull-rom.hh
@@ -49,6 +49,8 @@ struct CatmullRom {
     VERIFY(points.size() >= 4, "Catmull-Rom spline requires at least 4 points ({} given)", points.size());
     VERIFY(alpha >= 0 and alpha <= 1, "Catmull-Rom parameterization alpha must be [0, 1]");
 
+    // shift and rotate to make a closed loop easier
+
     // { A, B, C, D, ..., Y, Z
 
     const auto num_points = points.size();
@@ -94,53 +96,56 @@ struct CatmullRom {
   Point operator()(const value_type parameter) const {
     VERIFY(parameter >= 0 and parameter <= max_param, "Catmull-Rom interpolation parameter must be [0, {}]", max_param);
 
-    // parameters are ordered, per how they're set
+    // parameters are ordered, per how they're set -> upper_bound will produce a valid result
     const auto iterator = std::upper_bound(parameters.begin(), parameters.end(), parameter);
-    // *iterator >= parameter;
+    // *iterator > parameter;
     // Our objective is index: parameters[index] <= parameter < parameters[index + 1]
     const auto index = std::distance(parameters.begin(), iterator - 1);
 
     // now use Barry-Goldman pyramidial algorithm
-
-    // Start with 4 points P0, P1, P2, P3
-    // use those to blend into A0, A1, A2
-    // Then blend those to B0, B2
-    // Finally blend these into the final C
-    // P0  P1  P2  P3
-    //   A0  A1  A2
-    //     B0  B1
-    //       C
+    // Start with 4 points -> P0  P1  P2  P3
+    // Blend those         ->   A0  A1  A2
+    // Blend again         ->     B0  B1
+    // Blend those into    ->       C
+    //   the final result
     // blend(P0, P1) -> A0
-    // blend(P1, P2) -> A1
-    // blend(P2, P3) -> A2
+    //     blend(P1, P2) -> A1
+    //         blend(P2, P3) -> A2
     // blend(A0, A1) -> B0
-    // blend(A1, A2) -> B1
+    //     blend(A1, A2) -> B1
     // blend(B0, B1) -> C
     // -> C
 
-    const size_t index3 = parameters.size() == index + 2 ? 0 : index + 2;
-    const Point P0 = points[index - 1];
-    const Point P1 = points[index + 0];
-    const Point P2 = points[index + 1];
-    const Point P3 = points[index3];
+    // get indices
+    const auto i0 = index - 1;
+    const auto i1 = index + 0;
+    const auto i2 = index + 1;
+    // account for wrapping
+    const auto i3 = index + 2 == parameters.size() ? 0 : index + 2;
 
-    const value_type p0 = parameters[index - 1];
-    const value_type p1 = parameters[index + 0];
-    const value_type p2 = parameters[index + 1];
-    const value_type p3 = parameters[index3];
+    // get points
+    const Point P0 = points[i0];
+    const Point P1 = points[i1];
+    const Point P2 = points[i2];
+    const Point P3 = points[i3];
 
+    // get 'parameters' for those points
+    const value_type p0 = parameters[i0];
+    const value_type p1 = parameters[i1];
+    const value_type p2 = parameters[i2];
+    const value_type p3 = parameters[i3];
+
+    // get actual scale parameters
     const value_type s0s = p0 - parameter;
     const value_type s1s = p1 - parameter;
     const value_type s2s = p2 - parameter;
     const value_type s3s = p3 - parameter;
 
-    const value_type inverse_ds21 = 1 / (p2 - p1);
-
-    const auto blendP = [&](
+    const auto blend = [&](
       const auto& P0, const auto& P1,
       const auto& p0, const auto& p1,
       const auto& ds0, const auto& ds1
-    ){
+    ) {
       Point P;
       const value_type inverse = 1 / (p1 - p0);
       for (size_t axis{}; axis < P0.size(); ++axis) {
@@ -149,47 +154,14 @@ struct CatmullRom {
       return P;
     };
 
-    const Point A0 = blendP(P0, P1, p0, p1, s0s, s1s);
+    const Point A0 = blend(P0, P1, p0, p1, s0s, s1s);
+    const Point A1 = blend(P1, P2, p1, p2, s1s, s2s);
+    const Point A2 = blend(P2, P3, p2, p3, s2s, s3s);
 
-    // Point A0;
-    // const value_type inverse_ds10 = 1 / (P1 - P0);
-    // for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-    //   A0[axis] = inverse_ds10 * (s1s * P0[axis] - s0s * P1[axis]);
-    // }
+    const Point B0 = blend(A0, A1, p0, p2, s0s, s2s);
+    const Point B1 = blend(A2, A1, p1, p3, s1s, s3s);
 
-    const Point A1 = blendP(P1, P2, p1, p2, s2s, s1s);
-
-    // Point A1;
-    // for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-    //   A1[axis] = inverse_ds21 * (s2s * P1[axis] - s1s * P2[axis]);
-    // }
-
-    const Point A2 = blendP(P2, P3, p2, p3, s2s, s3s);
-
-    // Point A2;
-    // const value_type inverse_ds32 = 1 / (P3 - P2);
-    // for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-    //   A2[axis] = inverse_ds32 * (s3s * P2[axis] - s2s * P3[axis]);
-    // }
-
-    Point B0;
-    const value_type inverse_ds20 = 1 / (p2 - p0);
-    for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-      B0[axis] = inverse_ds20 * (s2s * A0[axis] - s0s * A1[axis]);
-    }
-
-    Point B1;
-    const value_type inverse_ds31 = 1 / (p3 - p1);
-    for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-      B1[axis] = inverse_ds31 * (s3s * A1[axis] - s1s * A2[axis]);
-    }
-
-    Point C;
-    for (size_t axis{}; axis < points.at(index).size(); ++axis) {
-      C[axis] = inverse_ds21 * (s2s * B0[axis] - s1s * B1[axis]);
-    }
-
-    return C;
+    return blend(B0, B1, p1, p2, s1s, s2s);
   }
 
   value_type parameter(const size_t index) const {
